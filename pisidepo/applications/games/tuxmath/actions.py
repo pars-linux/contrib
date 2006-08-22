ad522766e2d92891a8d0cecc3f76bd4329969fd3
Murat Senel <msenel@gmail.com>
2006-08-22 20:40:35 +0000

    tuxmath eklendi

---
+++ pisidepo/applications/games/tuxmath/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir="tuxmath"
+
+def setup():
+    pass
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dodir("/usr/share/tuxmax")
+    pisitools.dodir("/usr/bin")
+    autotools.install()
+    pisitools.dodoc("docs/*.txt")
+
