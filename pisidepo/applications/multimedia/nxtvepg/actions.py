5b6bb381300508a6bfb12421334637ddee183c02
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 21:21:48 +0000

    nxtvepg: eklendi

---
+++ pisidepo/applications/multimedia/nxtvepg/actions.py
@@ -0,0 +1,21 @@
+#
+#Pardox pardox2006 at hotmail dot com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+
+def setup():
+    pass
+
+def build():
+    autotools.make("prefix=/usr")
+
+def install():
+    autotools.rawInstall("prefix=%s/usr" % get.installDIR())
+    pisitools.domove("/usr/man", "/usr/share/man")
+    pisitools.dohtml("manual*.html")
+    pisitools.dodir("/var/tmp/nxtvdb")
+    pisitools.dodoc("CHANGES", "COPYRIGHT", "README*", "TODO")
