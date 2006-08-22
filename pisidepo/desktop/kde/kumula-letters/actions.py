dd3d17b3f58d1c6845a11f6b3a149cc8c26030f8
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 21:02:33 +0000

    kumula-letters: eklendi

---
+++ pisidepo/desktop/kde/kumula-letters/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+#Ertugrul Erata ertugrulerata at gmail.com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="kumula"
+
+def setup():
+    pass
+
+def build():
+    pass
+
+
+def install():
+    pisitools.dobin("bin/*", "opt/kumula/bin")
+    pisitools.insinto("/opt/kumula/lib","lib/*")
+    pisitools.insinto("/opt/kumula/graphics","graphics/*")
