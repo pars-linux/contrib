ad1eaa68fc8f42d093208ceb9e6ee0ef9d0ac444
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 20:39:49 +0000

    freemind: eklendi

---
+++ pisidepo/applications/editors/freemind/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Pardox pardox2006 at hotmail dot com
+# Uğur Çetin <jnmbk@users.sourceforge.net>
+#
+
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "."
+
+def install():
+    shelltools.chmod("freemind.sh", 0755)
+    pisitools.insinto("/opt/freemind", "*")
+    pisitools.dosym("/opt/freemind/freemind.sh", "/usr/bin/freemind")
+    pisitools.remove("/opt/freemind/freemind.bat")
+    pisitools.remove("/opt/freemind/Freemind.exe")
+    pisitools.remove("/opt/freemind/pisiBuildState")
+    #Turkish translation
+    shelltools.cd("%s/opt/freemind/lib" % get.installDIR())
+    shelltools.system("zip freemind.jar Resources_tr.properties")
+    pisitools.remove("/opt/freemind/lib/Resources_tr.properties")
