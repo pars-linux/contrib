b776dd6ad297279ec9edb4dac0eb6db41b6b1f91
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 21:06:55 +0000

    tellico: eklendi

---
+++ pisidepo/desktop/kde/tellico/comar/package.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Eren Türkay <turkay.eren@gmail.com>
+
+import os
+
+def postInstall():
+    os.system("/usr/bin/xmlcatalog --noout --add \"delegatePublic\" \
+               \"-//Robby Stephenson/DTD Tellico V9.0//EN\" \
+               \"file:///usr/share/apps/tellico/tellico.dtd\" \
+               /etc/xml/catalog")
+
+    os.system("/usr/bin/xmlcatalog --noout --add \"delegateSystem\" \
+               \"http://www.periapsis.org/tellico/dtd/v9/tellico.dtd\" \
+               \"file:///usr/share/apps/tellico/tellico.dtd\" \
+               /etc/xml/catalog")
+
+    os.system("/usr/bin/xmlcatalog --noout --add \"delegateURI\" \
+               \"http://www.periapsis.org/tellico/dtd/v9/tellico.dtd\" \
+               \"file:///usr/share/apps/tellico/tellico.dtd\" \
+               /etc/xml/catalog")
+
+def preRemove():
+    os.system("/usr/bin/xmlcatalog --noout --del \
+               \"file:///usr/share/apps/tellico/tellico.dtd\" \
+               /etc/xml/catalog")
