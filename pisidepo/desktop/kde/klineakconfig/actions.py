2285d00828e913c4fe5c587e02d77c621f0ab0c4
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 21:01:16 +0000

    klineakconfig: eklendi

---
+++ pisidepo/desktop/kde/klineakconfig/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Uğur Çetin <jnmbk@users.sourceforge.net>
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import pisitools
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+    pisitools.domo("po/tr.po", "tr", "klineakconfig.mo")
+    pisitools.dodir("/usr/kde/3.5/share/locale/tr/LC_MESSAGES")
+    pisitools.domove("/usr/share/locale/tr/LC_MESSAGES/klineakconfig.mo", "/usr/kde/3.5/share/locale/tr/LC_MESSAGES")
