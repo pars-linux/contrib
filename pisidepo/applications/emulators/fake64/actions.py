cf0aef5b8433d7c952408c04bd98afca4ff9238f
Murat Senel <msenel@gmail.com>
2006-08-22 19:59:41 +0000

    fake64 eklendi

---
+++ pisidepo/applications/emulators/fake64/actions.py
@@ -0,0 +1,20 @@
+#
+#Pardox pardox2006 at hotmail dot com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+#   autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+#   pisitools.domove("/usr/fake64/", "/usr/share/")
+#   pisitools.dosym("/usr/share/fake64/bin/romloader", "/usr/bin/fake64")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README", "TODO")
