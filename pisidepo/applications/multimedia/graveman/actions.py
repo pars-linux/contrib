18753fc021119aeaf8c5b98d0564b91f6b4b55c6
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 21:15:31 +0000

    graveman: eklendi

---
+++ pisidepo/applications/multimedia/graveman/actions.py
@@ -0,0 +1,19 @@
+#
+#Pardox pardox2006 at hotmail dot com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "graveman-0.3.12-5"
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README", "README.fr", "THANKS", "TRANSLATE-HOWTO.fr")
