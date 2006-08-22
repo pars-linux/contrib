cb6bf1852d3e2621f6afbbbd72a98c9eac0c1447
Murat Senel <msenel@gmail.com>
2006-08-22 19:51:14 +0000

    amaya eklendi

---
+++ pisidepo/applications/editors/amaya/actions.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#Murat Şenel <> muratasenel at gmail.com <>
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="Amaya"
+
+def setup():
+    shelltools.makedirs("%s/Amaya/obj" % get.workDIR())
+    shelltools.cd("%s/Amaya/obj" % get.workDIR())
+
+    shelltools.system("ln -s ../configure")
+    autotools.configure("--prefix=/usr/share")
+
+def build():
+    shelltools.cd("%s/Amaya/obj" % get.workDIR())
+    autotools.make()
+
+def install():
+    shelltools.cd("%s/Amaya/obj" % get.workDIR())
+
+    pisitools.dodir("/usr/share")
+
+    #run the scripts to install the files to the installation dir
+    shelltools.system("./script_install %s/Amaya/obj/bin %s/usr/share" % (get.workDIR(), get.installDIR()))
+    shelltools.system("./script_install_gnomekde %s/Amaya/obj/bin %s/usr/share" % (get.workDIR(), get.installDIR()))
+
+    #make symbolic link for executable files
+    pisitools.dosym("/usr/share/Amaya-9.51/wx/bin/amaya", "/usr/bin/amaya")
+    pisitools.dosym("/usr/share/Amaya-9.51/wx/bin/print", "/usr/bin/print")
+
+    #remove redundant dirs and files
+    pisitools.removeDir("/usr/share/bin")
+
+
+
+
+
