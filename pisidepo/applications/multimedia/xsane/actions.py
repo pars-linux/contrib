b525efca9eefe9826b7eb9671794fce2705fb350
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 21:29:19 +0000

    xsane: eklendi

---
+++ pisidepo/applications/multimedia/xsane/actions.py
@@ -0,0 +1,22 @@
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "xsane-0.991"
+
+def setup():
+    autotools.configure("--enable-gtk2 --enable-nls --enable-jpeg --enable-png --enable-tiff --enable-gimp")
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+    pisitools.dodoc("xsane.[A-Z]*")
+    pisitools.dohtml("doc/*")
+    pisitools.dosym("/usr/bin/xsane", "/usr/lib/gimp/2.0/plug-ins/xsane")
