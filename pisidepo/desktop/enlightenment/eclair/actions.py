f58c0c9df4e973ef743cc47f0431ef95ebe3744b
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 19:37:13 +0000

    eclair: eklendi

---
+++ pisidepo/desktop/enlightenment/eclair/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="eclair"
+
+def setup():
+    autotools.system("./autogen.sh --prefix=/usr \
+                                   --enable-xine \
+                                   --disable-libtool-lock \
+                                   --with-evas=/usr/share/evas \
+                                   --with-emotion=/usr/lib/emotion \
+                                   --with-gnu-ld \
+                                   --with-pic")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
