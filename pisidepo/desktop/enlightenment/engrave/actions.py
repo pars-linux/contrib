e4ab61085724dad97710e6d028a99b612df41e26
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 20:46:18 +0000

    engrave: eklendi

---
+++ pisidepo/desktop/enlightenment/engrave/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="engrave"
+
+def setup():
+    autotools.system("./autogen.sh --prefix=/usr \
+                                    --enable-dmalloc \
+                                    --enable-ecore \
+                                    --enable-ecore")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "Changelog", "COPYING*", "NEWS", "README", "TODO")
+
