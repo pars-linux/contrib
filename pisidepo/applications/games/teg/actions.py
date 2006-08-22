1386d90190be1b2fe7fbb752e41fb020ebfec6d1
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 20:39:13 +0000

    teg eklendi

---
+++ pisidepo/applications/games/teg/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#
+# murattsenell@gmail.com
+#######################
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "teg-0.11.1"
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "INSTALL", "README", "Changelog", "COPYING" "NEWS", "PEOPLE", "HACKING", "ABOUT-NLS")
