bfcb69af31bbeefa86eedf6cde1cd5c85d523db5
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-22 20:51:03 +0000

    enlightenment: eklendi

---
+++ pisidepo/desktop/enlightenment/enlightenment/actions.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="enlightenment-0.16.999.027"
+
+def setup():
+    autotools.configure("--enable-nls \
+                         --disable-rpath \
+                         --enable-x \
+                         --with-gnu-ld \
+                         --enable-libiconv \
+                         --enable-libintl \
+                         --enable-esd-sound \
+                         --enable-xinerama \
+                         --enable-xrandr \
+                         --enable-upgrade \
+                         --enable-hints-ewmh \
+                         --enable-fsstd \
+                         --enable-zoom \
+                         --with-imlib2=/usr/share/imlib2 \
+                         --enable-evas-config \
+                         --enable-ecore-config \
+                         --enable-edje_cc \
+                         --enable-edje \
+                         --enable-eet-config \
+                         --enable-embryo-config \
+                         --enable-profile")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.domo("po/tr.po","tr","enlightenment.mo")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "INSTALL", "NEWS", "README*", "docs/README*")
+
