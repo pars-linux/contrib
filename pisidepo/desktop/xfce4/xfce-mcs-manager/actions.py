7acbe1394653317a221b27925d48a0686aea6569
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-23 07:34:37 +0000

    xfce-mcs-manager: eklendi

---
+++ pisidepo/desktop/xfce4/xfce-mcs-manager/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+#Ertugrul Erata ertugrulerata at gmail.com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+    pisitools.removeDir("/usr/share/locale")
+    pisitools.removeDir("/usr/share/xfce4/doc/fr")
+    pisitools.removeDir("/usr/share/xfce4/doc/he")
+    pisitools.domo("po/tr.po","tr","xfce-mcs-manager.mo")
