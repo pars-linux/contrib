113afe411e70365c2be2290b073d73312eef6fc3
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-23 16:13:28 +0000

    d4x eklendi

---
+++ pisidepo/applications/network/d4x/actions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir="d4x-2.5.7.1.orig"
+
+def setup():
+    autotools.configure("--enable-release \
+                         --disable-esd \
+                         --disable-oss \
+                         --enable-libao \
+                         --disable-openssl \
+                         --enable-nls \
+                         --disable-rpath \
+                         --disable-aotest \
+                         --disable-esdtest \
+                         --disable-glibtest \
+                         --disable-gtktest \
+                         --with-gnu-ld \
+                         --with-included-gettext \
+                         --with-boost \
+                         --with-x")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.insinto("/usr/share/applications", "share/nt.desktop")
+    pisitools.insinto("/usr/share/pixmaps", "share/*.png")
+    pisitools.insinto("/usr/share/pixmaps", "share/*.xpm")
+    pisitools.domo("po/tr.po","tr","d4x.mo")
+    pisitools.remove("/usr/share/locale/locale.alias")
+    pisitools.dodoc("ABOUT-NLS", "AUTHORS", "ChangeLog*", "COPYING", "NEWS", "PLANS", "README", "TODO")
+
