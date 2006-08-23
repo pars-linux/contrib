f7ecb39b3b33b610f4bacf1fc806834aed587492
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-23 07:38:17 +0000

    xfce-utils: eklendi

---
+++ pisidepo/desktop/xfce4/xfce-utils/actions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#
+#Ertugrul Erata ertugrulerata at gmail.com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+    pisitools.removeDir("/usr/share/locale")
+    pisitools.remove("/usr/share/xfce4/*.az")
+    pisitools.remove("/usr/share/xfce4/*.ca")
+    pisitools.remove("/usr/share/xfce4/*.de")
+    pisitools.remove("/usr/share/xfce4/*.es")
+    pisitools.remove("/usr/share/xfce4/*.eu")
+    pisitools.remove("/usr/share/xfce4/*.fi")
+    pisitools.remove("/usr/share/xfce4/*.fr")
+    pisitools.remove("/usr/share/xfce4/*.ja")
+    pisitools.remove("/usr/share/xfce4/*.lt")
+    pisitools.remove("/usr/share/xfce4/*.ro")
+    pisitools.remove("/usr/share/xfce4/*.ru")
+    pisitools.remove("/usr/share/xfce4/*.sk")
+    pisitools.remove("/usr/share/xfce4/*.vi")
+    pisitools.remove("/usr/share/xfce4/*.zh_TW")
+    pisitools.remove("/usr/share/xfce4/*.he")
+    pisitools.remove("/usr/share/xfce4/*.it")
+    pisitools.remove("/usr/share/xfce4/*.uk")
+    pisitools.removeDir("/usr/share/xfce4/doc/fr")
+    pisitools.removeDir("/usr/share/xfce4/doc/he")
+    pisitools.domo("po/tr.po","tr","xfce-utils.mo")
+    pisitools.insinto("/etc/xdg/xfce4","scripts/xinitrc")
