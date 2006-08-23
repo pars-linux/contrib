b1e69c753890f810c9b67b5902c16415ea18026c
Ertuğrul Erata <ertugrulerata@gmail.com>
2006-08-23 09:05:33 +0000

    blufish web editör pyPdf ve Reportlab pdf araçları

---
+++ pisidepo/applications/doc/bluefish/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#Murat Şenel <> muratasenel at gmail.com <>
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
---
+++ pisidepo/applications/doc/pyPdf/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+# Ertugrul Erata
+
+from pisi.actionsapi import pythonmodules
+
+
+def install():
+    pythonmodules.install()
