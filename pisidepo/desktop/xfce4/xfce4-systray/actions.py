cad17012cc2e050430c2db66b1e9d4939d989881
Eren Türkay <turkay.eren@gmail.com>
2006-08-23 07:42:41 +0000

    xfce4-systray: eklendi

---
+++ pisidepo/desktop/xfce4/xfce4-systray/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#
+#Ertugrul Erata ertugrulerata at gmail.com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
