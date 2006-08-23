b19cc8c4cb96327e3b6c5bbeaf34f3267b0b8313
Murat Senel <msenel@gmail.com>
2006-08-23 17:23:25 +0000

    ksimus-floatingpoint: eklendi

---
+++ pisidepo/applications/science/ksimus-floatingpoint/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import kde
+
+WorkDir = "ksimus-floatingpoint-0.3.6"
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+
+def install():
+    kde.install()
