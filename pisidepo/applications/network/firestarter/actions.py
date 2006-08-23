19a4dfbdb4eec6eab076aedcc64a272a1ee8e97c
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-23 16:14:59 +0000

    firestarter: eklendi

---
+++ pisidepo/applications/network/firestarter/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+#
+# turkay.eren@gmail.com
+#######################
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS")
