5d9966f9e4355e9aee7693909ee1d08a941a65e2
Murat Senel <msenel@gmail.com>
2006-08-23 17:23:11 +0000

    ksimus-datarecorder: eklendi

---
+++ pisidepo/applications/science/ksimus-datarecorder/actions.py
@@ -0,0 +1,19 @@
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+WorkDir = "ksimus-datarecorder-0.3.6"
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+
+def install():
+    kde.install()
