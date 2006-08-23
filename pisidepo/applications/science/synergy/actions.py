915d4e2abc579d508d57596e8db94dfb04623cf6
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-23 17:23:43 +0000

    synergy: eklendi

---
+++ pisidepo/applications/science/synergy/actions.py
@@ -0,0 +1,16 @@
+#
+#Pardox pardox2006 at hotmail dot com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "weber"
+NoStrip = "/"
+
+def install():
+    pisitools.insinto("/usr/weber", "*")
+    pisitools.insinto("/usr/share/applications", "Synergy.desktop")
+    pisitools.dosym("/usr/weber/bin/synergy", "/usr/bin/synergy")
