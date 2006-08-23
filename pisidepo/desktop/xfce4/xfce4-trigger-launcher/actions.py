5e40a0927665e71c751eb9d7b68d1760f531124b
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-23 07:48:27 +0000

    xfce4-trigger-launcher: eklendi

---
+++ pisidepo/desktop/xfce4/xfce4-trigger-launcher/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#
+#Ertugrul Erata ertugrulerata at gmail.com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+    pisitools.removeDir("/usr/share/locale")
+    pisitools.domo("po/tr.po","tr","xfce4-trigger-launcher.mo")
