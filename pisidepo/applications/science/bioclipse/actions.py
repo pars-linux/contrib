ac579025c856f68f4222103d4a8fd0aceac69602
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-23 17:21:19 +0000

    bioclipse: eklendi

---
+++ pisidepo/applications/science/bioclipse/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#Murat Şenel <> muratasenel at gmail.com <>
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = ""
+
+def install():
+
+    pisitools.dodir("/opt/bioclipse")
+    shelltools.system("unzip bioclipse-0.9.2.linux.gtk.x86.zip")
+    shelltools.cd("bioclipse")
+    pisitools.insinto("/opt/bioclipse", "*")
+
+
