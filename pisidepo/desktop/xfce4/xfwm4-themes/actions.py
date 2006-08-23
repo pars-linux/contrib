c3c8a5770464252e26c097c2610185e6744a8a3c
Eren Türkay <turkay.eren@gmail.com>
2006-08-23 07:49:04 +0000

    xfwm4-themes: eklendi

---
+++ pisidepo/desktop/xfce4/xfwm4-themes/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+#Ertugrul Erata ertugrulerata at gmail.com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
