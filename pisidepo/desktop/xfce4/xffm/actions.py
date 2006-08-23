c813b1700abcda561c830feeb6e52ad1b1771535
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-23 07:43:07 +0000

    xffm: eklendi

---
+++ pisidepo/desktop/xfce4/xffm/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+#Ertugrul Erata ertugrulerata at gmail.com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+    pisitools.removeDir("/usr/share/locale")
+    pisitools.domo("po/tr.po","tr","xffm.mo")
+    pisitools.removeDir("/usr/share/xfce4/doc/he")
+    pisitools.removeDir("/usr/share/xfce4/doc/fr")
