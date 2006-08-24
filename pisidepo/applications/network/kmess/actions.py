6ade1ad371f20b13229bdda9ff2bec3a63c8a1cc
Eren Türkay <turkay.eren@gmail.com>
2006-08-24 08:11:22 +0000

    kmess: eklendi

---
+++ pisidepo/applications/network/kmess/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Uğur Çetin <jnmbk@users.sourceforge.net>
+
+from pisi.actionsapi import kde
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
