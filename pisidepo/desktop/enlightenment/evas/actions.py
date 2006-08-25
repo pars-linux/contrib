36b7a00209fc2f7e21d54337dff41ac78bb13eaf
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-25 18:24:01 +0000

    version bump

--- pisidepo/desktop/enlightenment/evas/actions.py
+++ pisidepo/desktop/enlightenment/evas/actions.py
@@ -5,39 +5,25 @@
 #
 #murattsenell@gmail.com
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="evas-0.9.9.027"
+WorkDir="evas-0.9.9.032"
 
 def setup():
-    autotools.configure("--prefix=/usr \
-                         --enable-x \
-                         --enable-directfb \
+    shelltools.export("EET_CONFIG", "")
+    shelltools.export("EDB_CONFIG", "")
+    autotools.configure("--enable-fontconfig \
                          --enable-software-x11 \
-                         --enable-software-xcb \
-                         --enable-software-qtopia \
-                         --enable-fbcon-fb \
-                         --enable-buffer \
-                         --enable-opengl \
-                         --enable-xrender \
-                         --enable-png-image-loader-png \
                          --enable-image-loader-gif \
-                         --enable-jpeg-image-loader-jpeg \
+                         --enable-image-loader-png \
+                         --enable-image-loader-jpeg \
                          --enable-image-loader-eet \
                          --enable-font-loader-eet \
                          --enable-image-loader-edb \
                          --enable-image-loader-tiff \
-                         --enable-cpu-mmx \
-                         --enable-cpu-sse \
-                         --enable-altivec-cpu-altivec \
-                         --with-enable-cairo \
-                         --enable-cpu-c \
-                         --enable-scale-sample \
-                         --enable-scale-smooth \
-                         --enable-convert-yuv \
-                         --enable-small-dither-mask \
                          --enable-convert-8-rgb-332 \
                          --enable-convert-8-rgb-666 \
                          --enable-convert-8-rgb-232 \
@@ -48,6 +34,7 @@ def setup():
                          --enable-convert-16-rgb-565 \
                          --enable-convert-16-rgb-555 \
                          --enable-convert-16-rgb-444 \
+                         --enable-convert-16-rgb-ipq \
                          --enable-convert-16-rgb-rot-0 \
                          --enable-convert-16-rgb-rot-270 \
                          --enable-convert-16-rgb-rot-90 \
@@ -59,7 +46,8 @@ def setup():
                          --enable-convert-32-bgrx-8888 \
                          --enable-convert-32-rgb-rot-0 \
                          --enable-convert-32-rgb-rot-270 \
-                         --enable-convert-32-rgb-rot-90")
+                         --enable-convert-32-rgb-rot-90 \
+                         --with-x")
 
 def build():
     autotools.make()
