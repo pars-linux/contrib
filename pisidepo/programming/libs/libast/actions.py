6865f41ca5cd7a6cbd3bab90fb0b1fc293dd0661
Murat Senel <msenel@gmail.com>
2006-08-25 16:47:15 +0000

    libast taşındı

---
+++ pisidepo/programming/libs/libast/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="libast-0.7"
+
+def setup():
+    autotools.configure("--with-x \
+                         --with-imlib \
+                         --enable-mmx \
+                         --with-regexp \
+                         --enable-pcre")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("Changelog", "DESIGN", "README")
+
