51788132ba159940877aedb6473d01b2a3dd6543
Murat Senel <msenel@gmail.com>
2006-08-25 18:30:50 +0000

    version bump

--- pisidepo/desktop/enlightenment/edje/actions.py
+++ pisidepo/desktop/enlightenment/edje/actions.py
@@ -10,15 +10,19 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
-WorkDir="edje-0.5.0.027"
+WorkDir="edje-0.5.0.032"
 
 def setup():
-    autotools.configure("--prefix=/usr \
-                         --with-pic \
+    autotools.configure("--enable-edje-cc \
                          --with-eet \
+                         --with-eet-exec \
                          --with-evas \
-                         --with-ecore=/usr/lib \
-                         --with-embryo")
+                         --with-evas-exec \
+                         --with-ecore \
+                         --with-ecore-exec \
+                         --with-embryo \
+                         --with-embryo-exec \
+                         --with-vim")
 
 def build():
     autotools.make()
