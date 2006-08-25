1133b34a7015739935d85bd7acaa271995d3ee81
Uğur Çetin <ugur@ugurcetin.com.tr>
2006-08-25 07:28:05 +0000

    openttd: actions.py kısaltıldı.

--- pisidepo/applications/games/openttd/actions.py
+++ pisidepo/applications/games/openttd/actions.py
@@ -13,13 +13,9 @@ def build():
     autotools.make()
 
 def install():
-    pisitools.dodir("/usr/share/openttd")
-    pisitools.dodir("/usr/share/openttd/data")
-    pisitools.dodir("/usr/share/openttd/lang")
-    pisitools.dodir("/usr/share/pixmaps/")
+    pisitools.insinto("/usr/share/openttd/data", "data/*")
+    pisitools.insinto("/usr/share/openttd/lang", "lang/*.lng")
+    pisitools.insinto("/usr/share/pixmaps", "media/openttd.128.png")
     pisitools.doexe("openttd", "/usr/share/openttd")
     pisitools.dodoc("docs/*")
     pisitools.dodoc("*.txt")
-    pisitools.dobin("data/*", "/usr/share/openttd/data")
-    pisitools.dobin("lang/*.lng", "/usr/share/openttd/lang")
-    pisitools.dobin("media/openttd.128.png", "/usr/share/pixmaps")
