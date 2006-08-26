4437431526d182b650686b82082bc3904570c219
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-26 19:42:29 +0000

    hadi bakalım

---
+++ pisidepo/programming/libs/ogdi/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "ogdi-3.1.5"
+
+def setup():
+    shelltools.export("TOPDIR", "%s/%s" % (get.workDIR(),get.srcDIR()))
+    autotools.configure("--with-zlib \
+                         --with-proj \
+                         --without-expat")
+
+def build():
+    shelltools.export("LD_LIBRARY_PATH", "%s/%s/bin/linux" % (get.workDIR(),get.srcDIR()))
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("ChangeLog", "LICENSE", "NEWS", "README", "VERSION")
+
+
