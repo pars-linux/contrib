1e8c898ee73e6bfe041b87500b56db47ffbb1abd
Murat Senel <msenel@gmail.com>
2006-08-26 19:42:58 +0000

    bir sonraki Grass, ama bir az bekleyin

---
+++ pisidepo/programming/libs/proj/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir = "proj-4.4.9"
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("AUTHOR", "ChangeLog", "COPYING", "NEWS", "README")
+
+
