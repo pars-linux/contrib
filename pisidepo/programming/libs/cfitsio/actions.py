5bfce38baf518335f6c1757733145c9a58611b6f
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-26 19:39:41 +0000

    Grass'e giden yolda ilk bağımlılık... devamı az sonra...

---
+++ pisidepo/programming/libs/cfitsio/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "cfitsio"
+
+def setup():
+    autotools.configure("--prefix=%s/usr" % get.installDIR())
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dodir("/usr")
+    autotools.install()
+    pisitools.dodoc("*.ps", "*.txt", "README")
+
