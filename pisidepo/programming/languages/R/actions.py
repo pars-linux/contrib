f80c8db93399821c432a9ff8d5d4174bf94dd154
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-26 05:56:29 +0000

    R A language and environment for statistical computing and graphics.

---
+++ pisidepo/programming/languages/R/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#
+#Ertugrul Erata ertugrulerata at gmail.com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+
+def setup():
+    shelltools.export("R_HOME_DIR", "/usr/lib/R")
+    autotools.configure("--enable-R-profiling --enable-R-shlib --enable-shared --with-tcltk --with-tcl-config=/usr/lib/tclConfig.sh --with-tk-config=/usr/lib/tkConfig.sh --with-system-pcre --with-system-zlib --with-system-bzlib --with-atlas")
+
+def build():
+    autotools.make()
+
+
+
+def install():
+    autotools.install()
+    pisitools.removeDir("/usr/bin")
+    pisitools.remove("/usr/lib/R/bin/R")
+    pisitools.dosym("/usr/lib/R/bin/R","/usr/bin/R")
