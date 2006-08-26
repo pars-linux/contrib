f67e6b05a7f77b591d7afbc9e36c2c1338b4aaf9
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-26 04:55:31 +0000

    kstreamripper: simgesi düzeltildi

--- pisidepo/desktop/kde/kstreamripper/actions.py
+++ pisidepo/desktop/kde/kstreamripper/actions.py
@@ -16,3 +16,5 @@ def build():
 
 def install():
     scons.install()
+    pisitools.insinto("%s/share/icons/hicolor/32x32/apps" % get.kdeDIR(), "src/hi32-app-kstreamripper.png", "kstreamripper.png")
+    pisitools.insinto("%s/share/icons/hicolor/16x16/apps" % get.kdeDIR(), "src/hi16-app-kstreamripper.png", "kstreamripper.png")
