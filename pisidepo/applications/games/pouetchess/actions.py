6d800f94a4e5ee358d142d4ccecbd2235d03070c
Uğur Çetin <ugur@ugurcetin.com.tr>
2006-08-26 12:12:23 +0000

    pouetchess: scons'u daha doğru kullan

--- pisidepo/applications/games/pouetchess/actions.py
+++ pisidepo/applications/games/pouetchess/actions.py
@@ -6,21 +6,24 @@
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import scons
 
 WorkDir="pouetchess_src_0.2.0"
 
 def setup():
-    shelltools.system("/usr/bin/scons configure prefix=/usr datadir=/usr/share/pouetchess")
+    shelltools.system("scons configure prefix=/usr datadir=/usr/share/pouetchess")
 
 def build():
-    shelltools.system("/usr/bin/scons")
+    scons.make()
 
 def install():
-    shelltools.system("/usr/bin/scons install")
+    scons.install()
 
     shelltools.system("rm /usr/bin/.sconsign")
     shelltools.system("find /usr/share/pouetchess/ -name .sconsign -exec rm {} \;")
     pisitools.dodir("/usr/bin")
     pisitools.dodir("/usr/share")
+
+    #FIXME: We should patch this some time, game tries to look into installDIR for data files if we give it a prefix in scons configure
     shelltools.move("/usr/bin/pouetChess", "%s/usr/bin/" % get.installDIR())
     shelltools.move("/usr/share/pouetchess", "%s/usr/share/pouetchess" % get.installDIR())
