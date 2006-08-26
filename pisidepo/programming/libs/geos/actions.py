b09b363cf9a7ee6080aea7df15bf1591bf8e15b2
Murat Senel <msenel@gmail.com>
2006-08-26 19:41:02 +0000

    olmazsa olmaz bir dep daha

---
+++ pisidepo/programming/libs/geos/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import libtools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "geos-2.2.3"
+
+def setup():
+    shelltools.export("WANT_AUTOCONF", "2.5")
+    libtools.libtoolize("--copy --force")
+    autotools.configure("--with-pic --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTOHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
