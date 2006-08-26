8b18380d956b804776bc5c610e1d5870d1e80923
Murat Senel <msenel@gmail.com>
2006-08-26 19:40:33 +0000

    bu da gerekli

---
+++ pisidepo/programming/libs/gdal/actions.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import libtools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "gdal-1.3.2"
+
+def setup():
+    autotools.configure("--with-libz \
+                         --with-grass=no \
+                         --with-libtool \
+                         --without-ld-shared \
+                         --enable-static=no \
+                         --enable-shared=yes \
+                         --with-pic \
+                         --with-libgrass=no \
+                         --with-cfitsio=yes \
+                         --with-pcraster=internal \
+                         --with-netcdf \
+                         --with-png \
+                         --with-libtiff \
+                         --with-geotiff=/usr/lib \
+                         --with-jpeg \
+                         --with-gif \
+                         --with-ogdi \
+                         --without-hdf5 \
+                         --with-jasper \
+                         --with-mrsid=no \
+                         --with-jp2mrsid=yes \
+                         --without-bsb \
+                         --with-mysql=no \
+                         --with-xerces \
+                         --with-odbc \
+                         --with-sqlite \
+                         --with-pg \
+                         --without-ogr \
+                         --with-static-proj4 \
+                         --with-geos \
+                         --with-php \
+                         --with-perl \
+                         --with-ruby \
+                         --with-python")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("COMMITERS", "Doxyfile", "HOWTO-RELEASE", "NEWS", "VERSION")
+    pisitools.doman("man/man1/*")
+
+    #prevent file conflict
+    pisitools.removeDir("/usr/lib/perl5")
+
+
