d99c612b7a195650a792e22fef3843a06b6314d8
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-27 07:13:04 +0000

    gnome-applets: tamamdır (test edilmesi lazım)

--- pisidepo/desktop/gnome/gnome-applets/actions.py
+++ pisidepo/desktop/gnome/gnome-applets/actions.py
@@ -4,15 +4,17 @@
 # Uğur Çetin <jnmbk@users.sourceforge.net>
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--with-x \
+                         --disable-scrollkeeper")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    #TODO: make docs for all applets
     pisitools.dodoc("ChangeLog", "AUTHORS", "INSTALL*", "NEWS", "README*")
