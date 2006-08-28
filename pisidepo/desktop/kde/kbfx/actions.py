8a0d2710891bcdc65ab75bb7fe904b304d12f106
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-28 06:10:18 +0000

    kbfx: güncellendi

--- pisidepo/desktop/kde/kbfx/actions.py
+++ pisidepo/desktop/kde/kbfx/actions.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #
 # Uğur Çetin <jnmbk@users.sourceforge.net>
 
 from pisi.actionsapi import kde
-from pisi.actionsapi import shelltools
+
+WorkDir = "kbfx-0.4.9.2rc1"
 
 def setup():
-    shelltools.system("sed -i \"s:\$(LIB_KIO):-L${KDEDIR}/lib -lkio:\" kbfxconfigapp/Makefile.am && make -f Makefile.cvs")
     kde.configure()
 
 def build():
