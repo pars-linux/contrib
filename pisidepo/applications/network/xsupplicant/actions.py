c62b0a97e90748b7d1f8b7a4f5e61cfe09695266
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-28 07:32:36 +0000

    xsupplicant: güncellendi

--- pisidepo/applications/network/xsupplicant/actions.py
+++ pisidepo/applications/network/xsupplicant/actions.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #
 # Uğur Çetin <jnmbk@users.sourceforge.net>
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--enable-aka-test \
+                         --with-madwifi-path=/usr/include/madwifi/include/")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+    pisitools.dodoc("README", "CHANGELOG", "COPYING", "AUTHORS", "etc/*.conf")
