105f5b94d01994fd98bf8d681971e78d90091ec5
Ertuğrul Erata <ertugrulerata@gmail.com>
2006-08-28 08:22:15 +0000

    herkes yerli yerine

---
+++ pisidepo/desktop/xfce4/base/libxfce4util/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+#Ertugrul Erata ertugrulerata at gmail.com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+
+def install():
+    autotools.install()
+    pisitools.removeDir("/usr/share/gtk-doc")
