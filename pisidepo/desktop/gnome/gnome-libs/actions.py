6ad5ad7f3c3824c5c8fdd16a6f828eef58e3e916
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-29 18:12:57 +0000

    gnome-libs: tertemis :)

--- pisidepo/desktop/gnome/gnome-libs/actions.py
+++ pisidepo/desktop/gnome/gnome-libs/actions.py
@@ -7,30 +7,25 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import libtools
 from pisi.actionsapi import get
 
 def setup():
     autotools.autoconf()
-    shelltools.export("CFLAGS", "%s -I/usr/include/db1" % get.CFLAGS())
     libtools.libtoolize("--force")
     autotools.rawConfigure("--host=%s  \
                             --prefix=/usr \
-			    --libdir=/usr/lib \
+                            --libdir=/usr/lib \
                             --mandir=/usr/share/man \
-			    --infodir=/usr/share/info \
-			    --sysconfdir=/etc \
-			    --localstatedir=/var/lib \
-			    --with-kde-datadir=%s/share \
+                            --infodir=/usr/share/info \
+                            --sysconfdir=/etc \
+                            --localstatedir=/var/lib \
+                            --with-kde-datadir=%s/share \
                             --disable-gtk-doc  \
-			    --enable-prefer-db1 \
-			    --enable-compat185" % (get.HOST(), get.kdeDIR()))
-                           
+                            --enable-prefer-db1 \
+                            --enable-compat185" % (get.HOST(), get.kdeDIR()))
+
 def build():
-    #db yüzünden düştüğümüz hallere bak :)
-    pisitools.dosed("libgnome/gnome-dump.c", "<db1/db.h>", "\"%s/%s/db.h\"" % (get.workDIR(), get.srcDIR()))
-    pisitools.dosed("libgnome/gnome-metadata.c", "<db1/db.h>", "\"%s/%s/db.h\"" % (get.workDIR(), get.srcDIR()))
     autotools.make("-j1")
 
 def install():
