28fcef8fd269d57148ce35cc980af8d3408884b0
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-08-29 07:33:28 +0000

    Terminal Thunar son halinde aterm go

---
+++ pisidepo/desktop/xfce4/Terminal/actions.py
@@ -0,0 +1,14 @@
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir="Terminal-0.2.5.4beta2"
+
+def setup():
+    autotools.configure("--enable-dbus")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
