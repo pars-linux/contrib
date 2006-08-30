05887c25cacc7b1f245663f88ac6c304209b74f0
Uğur Çetin <ugur@ugurcetin.com.tr>
2006-08-30 14:33:55 +0000

    system-tools-backends: gnome-applets bağımlılığı eklendi

---
+++ pisidepo/desktop/gnome/system-tools-backends/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Uğur Çetin <jnmbk@users.sourceforge.net>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "HACKING", "NEWS", "README", "TODO")
