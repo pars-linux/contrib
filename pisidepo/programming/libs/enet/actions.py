1eb0a5f6b9c5220ac2940bd4c70183075cfc6906
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-01 17:58:00 +0000

    enet: paketlendi

---
+++ pisidepo/programming/libs/enet/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Uğur Çetin <jnmbk@users.sourceforge.net>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir="enet"
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("docs/*")
