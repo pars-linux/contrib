be9459536be9a2109dc16683144631fd7c0e57fa
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-02 14:43:20 +0000

    gcursor: paketlendi

---
+++ pisidepo/desktop/gnome/gcursor/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Uğur Çetin <jnmbk@users.sourceforge.net>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("ChangeLog", "AUTHORS", "COPYING", "INSTALL", "TODO")
+
