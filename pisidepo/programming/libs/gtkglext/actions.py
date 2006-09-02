97c1d127f9544820e4acbbdf39f850163115c134
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-02 04:34:49 +0000

    gtkglext: eklendi

---
+++ pisidepo/programming/libs/gtkglext/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Murat Şenel
+#
+#murattsenell@gmail.com
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-glibtest \
+                         --disable-gtktest \
+                         --with-gdktarget=x11 \
+                         --with-x \
+                         --with-gl=/usr/lib/opengl/xorg-x11/lib \
+                         --with-gl-includedir=/usr/lib/opengl/include")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("README", "AUTHORS", "ChangeLog*", "COPYING*", "TODO")
