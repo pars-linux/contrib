315eb5b7c5683453728fe5b6240bba152affffab
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-03 15:32:21 +0000

    enchant: eklendi

---
+++ pisidepo/desktop/gnome/enchant/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Murat Şenel <muratasenel@gmail.com>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "HACKING", "MAINTAINERS", "NEWS", "README", "TODO")
+
