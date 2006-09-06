cb319174c7b8d931b9ccd5f9ca529e2d3892f5c1
Murat Senel <msenel@gmail.com>
2006-09-06 18:58:53 +0000

    AmyEdit eklendi

---
+++ pisidepo/applications/editors/amyedit/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Murat Şenel <muratasenel@gmail.com>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("ChangeLog", "AUTHORS", "INSTALL", "COPYING", "NEWS", "README", "TODO")
+
