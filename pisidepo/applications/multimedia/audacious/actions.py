8f1ff0072614af6ab09af9bbd9c261dcb8d58e1a
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-06 21:21:13 +0000

    audacious eklendi

---
+++ pisidepo/applications/multimedia/audacious/actions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Murat Şenel <muratasenel@gmail.com>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="audacious-1.1.2"
+
+def setup():
+    autotools.configure("--enable-ipv6 \
+                         --enable-chardet \
+                         --enable-gconf \
+                         --enable-gnome-vfs \
+                         --enable-esd \
+                         --enable-mp3 \
+                         --enable-lirc \
+                         --enable-adplug \
+                         --enable-vorbis \
+                         --enable-aac \
+                         --enable-sndfile \
+                         --enable-modplug \
+                         --enable-flac \
+                         --enable-wma \
+                         --enable-oss \
+                         --enable-jack \
+                         --enable-arts \
+                         --enable-sid \
+                         --enable-musepack \
+                         --enable-alsa \
+                         --enable-amidiplug \
+                         --enable-timidity \
+                         --with-dev-dsp=/dev/sound/dsp \
+                         --with-dev-mixer=/dev/sound/mixer")
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("ABOUT-NLS", "ChangeLog", "COPYING", "AUTHORS", "INSTALL", "NEWS", "README")
+
