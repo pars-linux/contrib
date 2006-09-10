c4cb0bf5f7c70adcc1eae8e9f0d9bab48fcd2f7b
pardox <pardox@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-10 16:46:14 +0000

    docbook paketi

---
+++ pisidepo/desktop/gnome/docbook-sgml-utils/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Copyright 2005 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "docbook-utils-0.6.14"
+
+def setup():
+#    autotools.autoconf()
+    autotools.configure("--disable-tetex")
+
+def build():
+    pass
+
+def install():
+    autotools.install()
+#"-e -f --makefile=Makefile --prefix=%s/usr" % get.installDIR())
+                    #htmldir=/usr/share/doc/%s/html" % (get.installDIR(), get.srcTAG()))
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "TODO")
