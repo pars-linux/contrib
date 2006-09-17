4b0cb85f303866cd61e33f80bf3323a91e4f1671
Uğur Çetin <ugur@ugurcetin.com.tr>
2006-09-17 06:19:46 +0000

    rkward: biraz daha modüler olsun, türkçe dosyası doğru yere gitsin

--- pisidepo/desktop/kde/rkward/actions.py
+++ pisidepo/desktop/kde/rkward/actions.py
@@ -6,17 +6,17 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import kde
-
+from pisi.actionsapi import get
 
 def setup():
-    kde.configure("--with-r-home=/usr/lib/R --with-r-includes=/usr/lib/R/include")
+    kde.configure("--with-r-home=/usr/lib/R \
+                   --with-r-includes=/usr/lib/R/include")
 
 def build():
     kde.make()
 
-
 def install():
     kde.install()
-    pisitools.remove("/usr/kde/3.5/share/apps/katepart/syntax/r.xml")
+    pisitools.remove("%s/share/apps/katepart/syntax/r.xml" % get.kdeDIR())
     pisitools.domo("po/tr.po","tr","rkward.mo")
-    pisitools.domove("/usr/share/locale/tr/LC_MESSAGES/rkward.po","/usr/kde/3.5/share/locale/tr/LC_MESSAGES/rkward.po")
+    pisitools.domove("/usr/share/locale/tr/LC_MESSAGES/rkward.mo","%s/share/locale/tr/LC_MESSAGES/" % get.kdeDIR())
