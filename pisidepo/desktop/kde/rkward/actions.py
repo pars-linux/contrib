c7ee0de20a0b62800927728eef5840ef7cc7566a
Uğur Çetin <ugur@ugurcetin.com.tr>
2006-09-18 02:09:31 +0000

    rkward: sürüm yükseltildi

--- pisidepo/desktop/kde/rkward/actions.py
+++ pisidepo/desktop/kde/rkward/actions.py
@@ -3,10 +3,9 @@
 #Ertugrul Erata ertugrulerata at gmail.com
 #
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde
 from pisi.actionsapi import get
+from pisi.actionsapi import kde
+from pisi.actionsapi import pisitools
 
 def setup():
     kde.configure("--with-r-home=/usr/lib/R \
@@ -17,6 +16,7 @@ def build():
 
 def install():
     kde.install()
+    #TODO: this one seems better than the one in kdelibs
     pisitools.remove("%s/share/apps/katepart/syntax/r.xml" % get.kdeDIR())
-    pisitools.domo("po/tr.po","tr","rkward.mo")
-    pisitools.domove("/usr/share/locale/tr/LC_MESSAGES/rkward.mo","%s/share/locale/tr/LC_MESSAGES/" % get.kdeDIR())
+    #fix conflict with R
+    pisitools.remove("/usr/lib/R/library/R.css")
