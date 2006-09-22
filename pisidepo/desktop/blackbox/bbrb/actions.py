1ebf78e7f158f9268879e2e9efee5ca6e958f9f3
pardox <pardox@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-22 14:27:51 +0000

    Background manager for blackbox/fluxbox/openbox

---
+++ pisidepo/desktop/blackbox/bbrb/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#Pardox pardox2006 at hotmail dot com
+#
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir = "bbrb-0.4.1"
+
+def setup():
+    autotools.configure()
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README", "THANKS", "TODO")
