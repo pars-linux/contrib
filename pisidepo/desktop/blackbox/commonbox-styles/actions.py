2817a998bf9eb95cdfa7b9a0569f511172b1780a
pardox <pardox@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-22 14:29:58 +0000

    Common styles for fluxbox, blackbox, and openbox.

---
+++ pisidepo/desktop/blackbox/commonbox-styles/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2006  TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir = "commonbox-styles-0.6"
+
+def install():
+    pisitools.insinto("/usr/share/commonbox/backgrounds", "backgrounds/*")
+    pisitools.insinto("/usr/share/commonbox/styles", "styles/*")
+    pisitools.dodoc("README.commonbox-styles", "COPYING", "STYLES.authors")
