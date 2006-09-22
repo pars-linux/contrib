1f28cd33ce5ae1be60848b926fc42713e4417a7a
pardox <pardox@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-22 14:08:56 +0000

    blackbox

---
+++ pisidepo/desktop/blackbox/blackbox/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--sysconfdir=/etc/X11/blackbox")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+#   shelltools.system("echo "/usr/bin/blackbox" > %s/etc/X11/Sessions/blackbox" % get.installDIR())
+    pisitools.dodoc("README*", "AUTHORS", "ChangeLog*", "COMPLIANCE", "INSTALL", "LICENSE", "RELNOTES", "TODO")
