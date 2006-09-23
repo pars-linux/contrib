7756ad41a9bfb9bbe64c48476716b946e12f900c
Murat Senel <msenel@gmail.com>
2006-09-23 23:28:10 +0000

    version bump

--- pisidepo/applications/multimedia/audacious/actions.py
+++ pisidepo/applications/multimedia/audacious/actions.py
@@ -7,7 +7,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="audacious-1.1.2"
+WorkDir="audacious-1.2.0-rc1"
 
 def setup():
     autotools.configure("--enable-ipv6 \
