511933e95a4d197c9be785240ce181083d8acb60
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-09-23 23:29:21 +0000

    plugins for audacious

---
+++ pisidepo/applications/multimedia/audacious-plugins/actions.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Murat Şenel <muratasenel@gmail.com>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="audacious-plugins-1.2.0"
+
+def setup():
+    autotools.configure("--enable-ipv6 \
+                         --enable-chardet \
+                         --enable-gconf \
+                         --enable-gnome-vfs \
+                         --enable-esd \
+                         --enable-mp3 \
+                         --enable-lirc \
+                         --enable-adplug \
+                         --enable-vorbis \
+                         --enable-aac \
+                         --enable-notify \
+                         --enable-sndfile \
+                         --enable-modplug \
+                         --enable-flac \
+                         --enable-wma \
+                         --enable-oss \
+                         --enable-jack \
+                         --enable-arts \
+                         --enable-sid \
+                         --enable-musepack \
+                         --enable-alsa \
+                         --enable-paranormal \
+                         --enable-amidiplug \
+                         --enable-amidiplug-alsa \
+                         --enable-amidiplug-flysn \
+                         --enable-amidiplug-dummy \
+                         --enable-timidity \
+                         --enable-xspf \
+                         --disable-xmltest")
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("ChangeLog", "COPYING", "AUTHORS", "INSTALL", "NEWS")
+
