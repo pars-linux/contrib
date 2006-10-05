a5f1408532d8cbf76502ac1a6abdc96e6adaf0aa
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-10-05 17:24:43 +0000

    Enable sound and localization fix

--- pisidepo/applications/network/d4x/actions.py
+++ pisidepo/applications/network/d4x/actions.py
@@ -8,14 +8,14 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
-WorkDir="d4x-2.5.7.1.orig"
+WorkDir="d4x-2.5.7.1"
 
 def setup():
     autotools.configure("--enable-release \
-                         --disable-esd \
-                         --disable-oss \
+                         --enable-esd \
+                         --enable-alsa \
                          --enable-libao \
-                         --disable-openssl \
+                         --enable-openssl \
                          --enable-nls \
                          --disable-rpath \
                          --disable-aotest \
@@ -37,5 +37,6 @@ def install():
     pisitools.insinto("/usr/share/pixmaps", "share/*.xpm")
     pisitools.domo("po/tr.po","tr","d4x.mo")
     pisitools.remove("/usr/share/locale/locale.alias")
-    pisitools.dodoc("ABOUT-NLS", "AUTHORS", "ChangeLog*", "COPYING", "NEWS", "PLANS", "README", "TODO")
+    pisitools.doman("DOC/nt.1")
+    pisitools.dodoc("ABOUT-NLS", "AUTHORS", "ChangeLog*", "COPYING", "NEWS", "PLANS", "README", "TODO", "DOC/FAQ", "DOC/LICENSE", "DOC/THANKS", "DOC/TROUBLES")
 
