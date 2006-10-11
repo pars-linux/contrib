07b8d0a18893eee0c4f882dc15764f0677c1e58e
Uğur Çetin <ugur@ugurcetin.com.tr>
2006-10-11 17:26:26 +0000

    kmess: sürüm yükseltildi

--- pisidepo/applications/network/kmess/actions.py
+++ pisidepo/applications/network/kmess/actions.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #
 # Uğur Çetin <jnmbk@users.sourceforge.net>
 
 from pisi.actionsapi import kde
+
+WorkDir="kmess-1.5pre1"
 
 def setup():
     kde.configure()
