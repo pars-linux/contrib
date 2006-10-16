0ee9a0a38c5eba752add1ce3bad7b3a409e487db
Ertuğrul Erata <ertugrulerata@gmail.com>
2006-10-16 06:15:41 +0000

    screencast ve bağımlıkları için fix ve deps düzenlemesi

--- pisidepo/programming/libs/ftgl/actions.py
+++ pisidepo/programming/libs/ftgl/actions.py
@@ -1,11 +1,13 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 WorkDir="FTGL/unix"
 
 def setup():
-    autotools.configure("--enable-shared \
-                         --with-x")
+    shelltools.system("/usr/bin/autoreconf")
+    shelltools.system("/usr/bin/libtoolize --copy --force")
+    autotools.configure("--with-gl-lib=/usr/lib --enable-shared")
 
 def build():
     autotools.make()
--- pisidepo/programming/libs/libinstrudeo/actions.py
+++ pisidepo/programming/libs/libinstrudeo/actions.py
@@ -4,13 +4,14 @@
 #
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
 
 def setup():
     shelltools.export("libxmlpp_CFLAGS","-I/usr/include/libxml++-1.0 -I/usr/lib/libxml++-1.0/include")
     shelltools.export("libxmlpp_LIBS","-L/usr/lib -lxml++-1.0")
+    shelltools.system("/usr/bin/autoreconf")
+    shelltools.system("/usr/bin/libtoolize --copy --force")
     autotools.configure()
 
 def build():
