8713b58ae75983e52e76ef2797a71d29d3117979
Murat Senel <msenel@gmail.com>
2006-10-16 16:54:11 +0000

    "up"ınız...

--- pisidepo/applications/games/gcompris/actions.py
+++ pisidepo/applications/games/gcompris/actions.py
@@ -9,13 +9,14 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "gcompris-8.0BETA6"
+WorkDir = "gcompris-8.1"
 
 def setup():
     autotools.configure("--disable-sdltest \
                          --disable-glibtest \
                          --disable-xf86vidmode \
                          --enable-sqlite \
+                         --disable-cairo \
                          --with-python=/usr/bin/python \
                          --with-x")
 
