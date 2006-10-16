5a9f5f1642cee52a22dbb00424bea1310d07caf3
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-10-16 17:44:24 +0000

    az kaldı

---
+++ pisidepo/programming/languages/python/zopeinterface/actions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Murat Şenel <muratasenel@gmail.com>
+
+from pisi.actionsapi import pythonmodules
+
+WorkDir="ZopeInterface-3.1.0c1"
+
+def install():
+    pythonmodules.install()
