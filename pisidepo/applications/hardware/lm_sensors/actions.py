4fa01f9081d343b8b5aa84bee881ace8bff8114f
Eren Türkay <turkay.eren@gmail.com>
2006-10-19 19:41:25 +0000

    added

---
+++ pisidepo/applications/hardware/lm_sensors/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Eren Türkay <turkay.eren@gmail.com>
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pass
+
+def build():
+    shelltools.system("make user")
+
+def install():
+    shelltools.system("make DESTDIR=%s PREFIX=/usr MANDIR=%s user_install" % (get.installDIR(),get.manDIR()))
+    pisitools.dodoc("doc/lm_sensors-FAQ.html")
