92152542cec6353fd862d42e77cf8fdccb6ed994
Murat Senel <msenel@gmail.com>
2006-10-20 23:36:37 +0000

    version bump

--- pisidepo/desktop/enlightenment/base/ecore/actions.py
+++ pisidepo/desktop/enlightenment/base/ecore/actions.py
@@ -9,24 +9,32 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="ecore-0.9.9.032"
+WorkDir="ecore-0.9.9.035"
 
 def setup():
     autotools.configure("--enable-ecore-txt \
                          --enable-ecore-x \
                          --enable-ecore-job \
+                         --enable-ecore-dfb \
+                         --enable-ecore-fb \
                          --enable-ecore-evas \
                          --enable-ecore-evas-gl \
                          --enable-ecore-evas-buffer \
+                         --enable-ecore-evas-xrender \
+                         --enable-ecore-evas-dfb \
+                         --enable-ecore-evas-fb \
                          --enable-ecore-con \
                          --enable-openssl \
+                         --enable-ecore-ipc \
                          --enable-ecore-dbus \
                          --enable-ecore-config \
                          --enable-ecore-file \
                          --enable-inotify \
                          --enable-poll \
+                         --enable-ecore-desktop \
                          --enable-curl \
-                         --enable-pthreads")
+                         --enable-pthreads \
+                         --with-x")
 
 def build():
     autotools.make()
