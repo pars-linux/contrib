71e7332a5328b28b8be6e0506b906569ada2b46e
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-10-21 05:25:30 +0000

    Limits the CPU usage of a process

---
+++ pisidepo/applications/util/cpulimit/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Uğur Çetin <jnmbk@users.sourceforge.net>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    pass
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dobin("cpulimit")
