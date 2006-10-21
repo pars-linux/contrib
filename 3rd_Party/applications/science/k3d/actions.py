7befea06cfdb42032df31a96cde413172c9bae6d
fasici <fasici@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-10-21 13:09:38 +0000

    A free 3D modeling, animation, and rendering system

---
+++ 3rd_Party/applications/science/k3d/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--without-gnome --with-nls")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README*", "NEWS")
