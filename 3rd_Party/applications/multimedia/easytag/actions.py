ea7a20c2c93d7f615e2a05b3be333c51fac5aafe
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2006-10-22 17:22:50 +0000

    Easytag'in mp4 desteği şimdilik kapalı. libmp4v2 gerekli. Depoda faad2'den gelenle derlenmiyor. Depoda olmayan mpeg4ip'den gelenle derlenebiliyor. mpeg4ip'den gelen de depodaki faad2'den gelen çakıştığı için paketini yapmadım.

---
+++ 3rd_Party/applications/multimedia/easytag/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-mp4")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%(DESTINATION)s \
+                          INSTALLDIR=%(DESTINATION)s/usr/bin \
+                          MANDIR=%(DESTINATION)s/usr/share/man/man1 \
+                          INCLUDEDIR=%(DESTINATION)s/usr/include \
+                          LOCALEDIR=%(DESTINATION)s/usr/share/locale \
+                          PKGCONFIGDIR=%(DESTINATION)s/usr/lib/pkgconfig" % {'DESTINATION': get.installDIR()})
+
+    pisitools.dodoc("TODO", "COPYING", "README", "Changelog", "INSTALL", "ABOUT-NLS", "THANKS")
