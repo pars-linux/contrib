95228134d1612dcde61b40e2cb3f485be57e3caa
Murat Senel <msenel@gmail.com>
2006-10-24 14:32:07 +0000

    up

--- pisidepo/desktop/enlightenment/extra/engrave/actions.py
+++ pisidepo/desktop/enlightenment/extra/engrave/actions.py
@@ -9,15 +9,13 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="engrave-0.1.0"
+WorkDir="engrave-20060926"
 
 def setup():
-    autotools.system("./autogen.sh --prefix=/usr \
-                                   --enable-dmalloc \
-                                   --with-ecore \
-                                   --with-ecore-exec \
-                                   --with-evas \
-                                   --with-evas-exec")
+    autotools.configure("--with-ecore \
+                         --with-ecore-exec \
+                         --with-evas \
+                         --with-evas-exec")
 
 def build():
     autotools.make()
