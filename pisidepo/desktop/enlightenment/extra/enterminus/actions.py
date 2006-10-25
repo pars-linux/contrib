eefc5dc2cb1a88fd3c569586bd19f43891461e23
Murat Senel <msenel@gmail.com>
2006-10-25 15:33:39 +0000

    added...

---
+++ pisidepo/desktop/enlightenment/extra/enterminus/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Murat Şenel <muratasenel@gmail.com>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "INSTALL", "NEWS", "README", "TODO")
