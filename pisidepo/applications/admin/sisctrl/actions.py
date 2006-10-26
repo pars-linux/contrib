36a453c638de1db38927809a6301c52d8df05249
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-10-26 06:07:38 +0000

    sisctrl

---
+++ pisidepo/applications/admin/sisctrl/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+#
+# Ertuğrul Erata <ertugrulerata@gmail.com>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="sisctrl-0.0.20051202"
+
+def setup():
+    autotools.configure("--with-xv-path=/usr/lib")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("AUTHORS","ChangeLog","NEWS","README")
+    pisitools.insinto("/usr/share/","icons")
+    pisitools.insinto("/usr/share/applications","extra/sisctrl.desktop")
