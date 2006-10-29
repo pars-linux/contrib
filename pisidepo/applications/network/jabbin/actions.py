2f92c66171be937fb4e4a36ddfe613a7b819bfe7
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-10-29 14:35:44 +0000

    added...

---
+++ pisidepo/applications/network/jabbin/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Murat Şenel <muratasenel@gmail.com>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="jabbin-2.0beta"
+
+def setup():
+    pisitools.dosed("install.sh","INSTALL_PATH=\"/usr\"","INSTALL_PATH=\"%s/usr\"" % get.installDIR())
+    shelltools.export("QTDIR", "%s" % get.qtDIR())
+
+def build():
+
+    shelltools.system("%s/bin/qmake -o Makefile jabbin.pro" % get.qtDIR())
+    autotools.make()
+
+def install():
+    #autotools.install doesn't do the trick here :\
+    shelltools.system("./install.sh")
+    pisitools.insinto("/usr/share/applications", "jabbin.desktop")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "INSTALL", "NEWS", "README")
