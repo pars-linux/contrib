cb2a1906500e1c9a012ba0e8a79954fdd21c8ba1
Uğur Çetin <ugur@ugurcetin.com.tr>
2006-11-05 09:51:14 +0000

    maksat kurmak isteyenlerin işi kolaylaşsın

---
+++ pisidepo/desktop/freedesktop/beryl/actions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+
+WorkDir = get.srcNAME()
+items = ["beryl-core", "beryl-plugins", "beryl-settings", "beryl-manager", "emerald", 
+"emerald-themes"]
+
+def setup():
+    for item in items:
+        shelltools.cd(item)
+        shelltools.system("./autogen.sh --prefix=/usr")
+        shelltools.cd("..")
+
+def build():
+    for item in items:
+        shelltools.cd(item)
+        autotools.make()
+        shelltools.cd("..")
+
+def install():
+    for item in items:
+        shelltools.cd(item)
+        autotools.install()
+        shelltools.cd("..")
+    pisitools.dosym("/usr/share/icons/Tulliana-2.0", "/usr/share/icons/gnome")
