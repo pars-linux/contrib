314ab4fbd859b9d32ccf8312e326e88e6429e7e8
Eren Türkay <turkay.eren@gmail.com>
2006-11-08 20:35:49 +0000

    added

---
+++ pisidepo/programming/languages/php/PEAR-File/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+
+WorkDir="File-1.2.2"
+
+def install():
+    pisitools.insinto("/usr/share/php5/PEAR","File.php")
+    pisitools.insinto("/usr/share/php5/PEAR/File","File/*")
+    pisitools.insinto("/usr/share/php5/PEAR/File/tests","tests/*")
