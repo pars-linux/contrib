7f282c456b6832680b29779128b8a7a165226f49
Eren Türkay <turkay.eren@gmail.com>
2006-11-10 17:27:14 +0000

    added

---
+++ pisidepo/programming/languages/php/PEAR-File_Passwd/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+
+WorkDir="File_Passwd-1.1.6"
+
+def install():
+    pisitools.insinto("/usr/share/php5/PEAR/File", "Pass*")
+    pisitools.insinto("/usr/share/php5/tests/File_Passwd", "tests/*")
