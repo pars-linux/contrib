dbc5a265cc42076f315860c32dde5b368b979334
alsaberk <alsaberk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-11-12 14:41:26 +0000

    Del

--- pisidepo/applications/network/kasablanca/package.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall():
-    os.system('wall "Merhaba PiSi paketi sisteme kuruldu!"')
-
-def preRemove():
-    os.system('wall "Merhaba PiSi paketi sistemden kaldırılıyor!"') 
