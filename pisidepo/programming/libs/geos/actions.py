28071ff1610406614d454491bd9bd0bc5341fd74
Murat Senel <msenel@gmail.com>
2006-11-17 21:30:58 +0000

    version bump

--- pisidepo/programming/libs/geos/actions.py
+++ pisidepo/programming/libs/geos/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "geos-2.2.3"
+WorkDir = "geos-3.0.0rc2"
 
 def setup():
     shelltools.export("WANT_AUTOCONF", "2.5")
