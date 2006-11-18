13209b4bca11ae6070e0b80effe526e7f70836a6
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-11-18 09:04:08 +0000

    notification-daemon-xfce add

---
+++ pisidepo/desktop/xfce4/notification-daemon-xfce/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.insinto("/usr/libexec/","src/daemon/notification-daemon-xfce")
+    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
