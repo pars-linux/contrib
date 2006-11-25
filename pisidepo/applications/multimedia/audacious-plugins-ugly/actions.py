df5c0474f4b4372a791d1f2744eb8878d61ee70d
Murat Senel <msenel@gmail.com>
2006-11-25 22:39:12 +0000

    added

---
+++ pisidepo/applications/multimedia/audacious-plugins-ugly/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--enable-ipv6 \
+                         --enable-chardet \
+                         --enable-notify \
+                         --enable-mplayer \
+                         --enable-sap \
+                         --enable-iris")
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.removeDir("/usr/bin")
+    pisitools.removeDir("/usr/include")
+
+    pisitools.dodoc("ChangeLog", "COPYING", "AUTHORS", "INSTALL", "NEWS")
+
