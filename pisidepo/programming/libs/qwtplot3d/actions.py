0bfba544751ec5d6650b3b9e269432fc5675ff6c
Ertuğrul Erata <ertugrulerata@gmail.com>
2006-11-26 12:53:13 +0000

    qtiplot deps2

---
+++ pisidepo/programming/libs/qwtplot3d/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir="qwtplot3d"
+
+def setup():
+    shelltools.export("QTDIR","/usr/qt/3/")
+    shelltools.system("/usr/qt/3/bin/qmake")
+
+def build():
+    autotools.make()
+
+
+def install():
+    pisitools.insinto("/usr/lib/","lib/*")
+    pisitools.insinto("/usr/include/","include/*")
+    pisitools.insinto("/usr/share/doc/%s" % get.srcTAG(), "examples")
