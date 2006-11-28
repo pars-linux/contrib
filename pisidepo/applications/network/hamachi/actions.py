0baedc1aa26a7dde17f3959cf70acaf66a2afbdb
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-11-28 14:15:07 +0000

    temizlik

--- pisidepo/applications/network/hamachi/actions.py
+++ pisidepo/applications/network/hamachi/actions.py
@@ -7,20 +7,18 @@
 WorkDir="hamachi-0.9.9.9-20-lnx"
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
+
 
 def setup():
     pass
 
 def build():
-    pisitools.dosed("tuncfg/Makefile","/sbin","%s/sbin" % get.installDIR())
-    pisitools.dosed("Makefile","/usr/bin","%s/usr/bin" % get.installDIR())
-    pisitools.dosed("Makefile","/sbin","%s/sbin" % get.installDIR())
+    shelltools.system("rm tuncfg/tuncfg")
+    autotools.make()
 
 def install():
-    # we should create a dir before install
-    pisitools.dodir("/usr/bin")
-    pisitools.dodir("/sbin")
-    autotools.install()
+    pisitools.dobin("hamachi")
+    pisitools.dosbin("tuncfg/tuncfg")
     pisitools.dodoc("README","LICENSE")
