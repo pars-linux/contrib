2cf904b5719511649be1c0219d85139804ba1d93
Eren Türkay <turkay.eren@gmail.com>
2006-11-28 13:44:55 +0000

    fix directories

--- pisidepo/applications/network/hamachi/actions.py
+++ pisidepo/applications/network/hamachi/actions.py
@@ -7,7 +7,6 @@
 WorkDir="hamachi-0.9.9.9-20-lnx"
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
@@ -15,11 +14,13 @@ def setup():
     pass
 
 def build():
-    pisitools.dosed("tuncfg/Makefile","/sbin","%s/usr/bin" % get.installDIR())
+    pisitools.dosed("tuncfg/Makefile","/sbin","%s/sbin" % get.installDIR())
     pisitools.dosed("Makefile","/usr/bin","%s/usr/bin" % get.installDIR())
-    pisitools.dosed("Makefile","/sbin","%s/usr/bin" % get.installDIR())
+    pisitools.dosed("Makefile","/sbin","%s/sbin" % get.installDIR())
 
 def install():
-    shelltools.system("/usr/bin/mkdir -p %s/usr/bin" % get.installDIR())
+    # we should create a dir before install
+    pisitools.dodir("/usr/bin")
+    pisitools.dodir("/sbin")
     autotools.install()
     pisitools.dodoc("README","LICENSE")
