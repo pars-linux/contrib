f589dbbacdbf21462feee39517923e8ff6c96ea0
Murat Senel <msenel@gmail.com>
2006-12-02 14:55:31 +0000

    version bump

--- pisidepo/applications/multimedia/prokyon3/actions.py
+++ pisidepo/applications/multimedia/prokyon3/actions.py
@@ -8,13 +8,13 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="prokyon3-0.9.5"
+WorkDir="prokyon3-0.9.6RC1"
 
 def setup():
     autotools.configure("--with-x \
-                         --with-qtdir=/usr/qt/3 \
-                         --with-qt-includes=/usr/qt/3/include \
-                         --with-qt-libs=/usr/qt/3/lib \
+                         --with-qtdir=%s \
+                         --with-qt-includes=%s/include \
+                         --with-qt-libs=%s \
                          --with-taglib \
                          --with-musicextras \
                          --with-mixxx \
@@ -22,14 +22,17 @@ def setup():
                          --with-ogg \
                          --with-mad \
                          --with-id3 \
-                         --with-mysql --with-mysql-lib --with-mysql-include")
+                         --with-mysql --with-mysql-lib --with-mysql-include" % (get.qtDIR(), get.qtDIR(), get.qtLIBDIR()))
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
     pisitools.insinto("/usr/share/applications", "prokyon3.desktop")
-    pisitools.insinto("/usr/share/pixmaps", "images/prokyon_logo.png")
+    pisitools.insinto("/usr/share/pixmaps", "images/prokyon_logo.png", "prokyon.png")
+
+    pisitools.domove("usr/share/prokyon3/html", "%s/%s" % (get.docDIR(), get.srcTAG()))
     pisitools.dodoc("ChangeLog", "AUTHORS", "COPYING", "INSTALL", "NEWS", "README")
 
