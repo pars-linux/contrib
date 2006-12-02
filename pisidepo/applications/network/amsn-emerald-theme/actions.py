94b6c4fcfd9f154dab4efbed293375e2befed459
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-02 05:32:34 +0000

    what's the packages' component??

---
+++ pisidepo/applications/network/amsn-emerald-theme/actions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+
+WorkDir="Emerald-1.0"
+
+def install():
+    pisitools.insinto("/usr/share/amsn/skins/Emerald-1.0", "*")
