b15f257425dc406d277f2ee2561d9d10803d44cc
Ertuğrul Erata <ertugrulerata@gmail.com>
2006-12-04 07:01:51 +0000

    labplot data analyse and plotting program

--- pisidepo/applications/science/labplot/actions.py
+++ pisidepo/applications/science/labplot/actions.py
@@ -4,23 +4,22 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+from pisi.actionsapi import kde
 
 WorkDir="LabPlot-1.5.1.4"
 
 def setup():
-    autotools.configure("--enable-system-qwtplot3d --disable-KexiDB --disable-texvc --disable-ocaml --disable-qsa")
+    kde.configure("--enable-system-qwtplot3d --enable-qsa --disable-KexiDB  --enable-ocaml --enable-texvc ")
 
 
 def build():
-    autotools.make()
+    kde.make()
 
 
 
 def install():
-    autotools.install()
-    pisitools.remove("/usr/bin/opj2dat")
-    pisitools.remove("/usr/lib/liborigin.*")
+    kde.install()
+    pisitools.remove("/usr/kde/3.5/bin/opj2dat")
+    pisitools.remove("/usr/kde/3.5/lib/liborigin.*")
