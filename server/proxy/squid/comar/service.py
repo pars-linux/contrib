b2ce4108c42bbc96bfff2e95075463a50ad2629f
erdinc <erdinc@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-04 14:51:25 +0000

    alıcı ayarlarınız ile oynamayınız, evdeki ses evdeki ses..

--- server/proxy/squid/comar/service.py
+++ server/proxy/squid/comar/service.py
@@ -2,20 +2,25 @@ import os
 from comar.service import *
 
 serviceType = "server"
-serviceDesc = "Squid Daemon"
+serviceDesc = _({"en": "Squid Daemon",
+                 "tr": "Squid Sunucusu"})
 
 def start():
     ret = run("/usr/sbin/squid -DYC")
     if ret == 0:
         notify("System.Service.changed", "started")
+    else:
+        fail("Unable to start service")
 
 def stop():
     ret = run("/usr/sbin/squid -k shutdown")
     if ret == 0:
         notify("System.Service.changed", "stopped")
+    else:
+        fail("Unable to stop service")
 
 def reload():
     run("/usr/sbin/squid -k reconfigure")
 
 def status():
-    return checkDeamon("/var/run/squid.pid")
+    return checkDaemon("/var/run/squid.pid")
