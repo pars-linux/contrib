33527fd8f7d1fe17904398f415df4b98f48c7b78
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-05 12:05:38 +0000

    libsift autopano-sift dependency deps

---
+++ programming/libs/libsift/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Copyright 2005 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir="libsift-1.8/src"
+
+
+def build():
+    autotools.make("lib")
+
+def install():
+    pisitools.insinto("/usr/lib","libsift.dll")
