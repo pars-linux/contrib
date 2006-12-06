8f587196bea40225809e344f67f4bdab87a11076
Ertuğrul Erata <ertugrulerata@gmail.com>
2006-12-06 06:49:19 +0000

    add sun-jdk build deps

--- applications/multimedia/libpano12/actions.py
+++ applications/multimedia/libpano12/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--with-java=/opt/sun-jdk")
 
 def build():
     autotools.make()
