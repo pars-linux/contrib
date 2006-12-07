96ff6822b1c4c0a90febd8dc63ba626f20750c1b
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-07 18:04:57 +0000

    File.php is not readable for other users? Also please check this package: http://svn.pardus.org.tr/contrib/programming/languages/php/PEAR-Text_Diff/actions.py

--- programming/languages/php/PEAR-File_Find/actions.py
+++ programming/languages/php/PEAR-File_Find/actions.py
@@ -5,9 +5,12 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actisonapi import shelltools
 
 WorkDir="File_Find-1.3.0"
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/File","Find.php")
+    shelltools.chmod("%s/usr/share/php5/PEAR/File/Find.php" % get.installDIR())
     pisitools.insinto("/usr/share/php5/PEAR/tests/File_Find","tests/*")
