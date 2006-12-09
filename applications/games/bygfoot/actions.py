f3665627299d4ea5d649041dfe83b7951146cf2b
Murat Senel <msenel@gmail.com>
2006-12-09 21:03:31 +0000

    football management game... added...

---
+++ applications/games/bygfoot/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto("/usr/share/pixmaps", "support_files/pixmaps/bygfoot_icon.png", "bygfoot.png")
+    pisitools.insinto("/usr/share/applications", "bygfoot.desktop")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "ReleaseNotes", "INSTALL", "NEWS", "TODO", "UPDATE")
