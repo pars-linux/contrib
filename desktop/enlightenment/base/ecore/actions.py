859c3fa2c62634f92a3cea447a61c54ec62857ce
Murat Senel <msenel@gmail.com>
2006-12-09 19:08:25 +0000

    version bump

--- desktop/enlightenment/base/ecore/actions.py
+++ desktop/enlightenment/base/ecore/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="ecore-0.9.9.036"
-
 def setup():
     autotools.configure("--enable-ecore-txt \
                          --enable-ecore-x \
