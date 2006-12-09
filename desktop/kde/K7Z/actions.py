e9b634ad4c2e805ba9782065e896a406222bdee3
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-09 09:43:55 +0000

    K7Z: version bump

--- desktop/kde/K7Z/actions.py
+++ desktop/kde/K7Z/actions.py
@@ -9,22 +9,18 @@ from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 
-WorkDir="."
-
 def setup():
-    shelltools.system("7z x %s.7z" % get.srcDIR())
     shelltools.export("KDEDIR", get.installDIR() + get.kdeDIR())
 
 def build():
-    shelltools.cd(get.srcDIR())
-    autotools.make()
+    shelltools.cd("Build")
+    autotools.make("all")
 
 def install():
-    shelltools.cd(get.srcDIR())
+    shelltools.cd("Build")
     # Let's help the lazy makefile
-    pisitools.insinto("%s/bin" % get.kdeDIR(), "Bin/K7Z.py")
+    pisitools.insinto("%s/bin" % get.kdeDIR(), "../Bin/K7Z.py")
     pisitools.dodir("%s/share/apps/konqueror/servicemenus" % get.kdeDIR())
     pisitools.dodir("%s/share/icons/hicolor/32x32/actions" % get.kdeDIR())
-    
-    autotools.install()
 
+    autotools.rawInstall()
