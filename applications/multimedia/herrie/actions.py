82b3bf76089f5df6b1e668557760d0dbaf77ea3d
Murat Senel <msenel@gmail.com>
2006-12-09 17:46:00 +0000

    command line player... added...

---
+++ applications/multimedia/herrie/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def build():
+    shelltools.cd("src")
+    autotools.make()
+
+def install():
+    pisitools.dobin("src/herrie")
+
+    pisitools.dodoc("COPYING", "ChangeLog", "TODO", "README")
