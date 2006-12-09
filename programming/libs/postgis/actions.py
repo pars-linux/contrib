ad43688b0f78cacf0ab2667be96bba5893c849ab
Murat Senel <msenel@gmail.com>
2006-12-09 15:50:05 +0000

    version bump

--- programming/libs/postgis/actions.py
+++ programming/libs/postgis/actions.py
@@ -7,22 +7,30 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 def setup():
     autotools.autoconf()
     autotools.rawConfigure("--with-pgsql \
                             --with-geos \
                             --with-geos-libdir=/usr/lib \
-                            --with-proj=/usr \
+                            --with-proj \
                             --with-proj-libdir=/usr/lib \
+                            --with-docdir=/usr/share/doc \
                             --datadir=/usr/share/postgresql/contrib \
                             --libdir=/usr/lib/postgresql")
 
 def build():
     autotools.make()
+    shelltools.cd("topology")
+    autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("topology")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("..")
+
     pisitools.dodoc("CHANGES", "COPYING", "CREDITS", "TODO", "README.*", "doc/README")
     pisitools.dohtml("doc/html/*")
     pisitools.doman("doc/man/*")
