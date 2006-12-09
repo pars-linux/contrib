fbff2073af2a959fd05763c71b1445e52be1847f
Murat Senel <msenel@gmail.com>
2006-12-09 20:26:41 +0000

    version bump

--- applications/multimedia/freevo/actions.py
+++ applications/multimedia/freevo/actions.py
@@ -6,14 +6,12 @@
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-
-WorkDir="freevo-1.6.0"
 
 def install():
     pisitools.dodir("/etc/freevo")
     pisitools.dodir("/var/cache/freevo")
     pisitools.dodir("/var/log/freevo")
+
     pythonmodules.install()
-    pisitools.insinto("/etc/freevo", "local_conf.py.example")
-    pisitools.rename("/etc/freevo/local_conf.py.example", "local_conf.py")
+
+    pisitools.insinto("/etc/freevo", "local_conf.py.example", "local_conf.py")
