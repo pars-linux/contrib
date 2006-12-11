62f55f172ff7c75f51dd76071dd2ae24adb25957
Eren Türkay <turkay.eren@gmail.com>
2006-12-11 16:48:49 +0000

    added

---
+++ programming/languages/php/PEAR-MDB2_Driver_pgsql/actions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+
+WorkDir="MDB2_Driver_pgsql-1.3.0"
+
+def install():
+    pisitools.insinto("/usr/share/php5/PEAR/MDB2/Driver", "MDB2/Driver/*")
