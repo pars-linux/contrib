35e4d6fe86ec2697bff810a41eae32f21981af57
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-13 12:40:31 +0000

    last cleanup ;)

--- programming/languages/php/PEAR-Text_Diff/actions.py
+++ programming/languages/php/PEAR-Text_Diff/actions.py
@@ -5,14 +5,10 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
 
 WorkDir="Text_Diff-0.2.1"
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Text", "Diff*")
-    # set permission
-    shelltools.chmod("%s/usr/share/php5/PEAR/Text/Diff/Engine/string.php" % get.installDIR())
     pisitools.insinto("/usr/share/php5/PEAR/doc/Text_Diff", "docs/*")
     pisitools.insinto("/usr/share/php5/PEAR/tests/Text_Diff", "tests/*")
