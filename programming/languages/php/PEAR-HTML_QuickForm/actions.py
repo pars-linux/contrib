8122fad9cf18187050e7054c92467eb13f41642f
Eren Türkay <turkay.eren@gmail.com>
2006-12-13 12:24:06 +0000

    clean

--- programming/languages/php/PEAR-HTML_QuickForm/actions.py
+++ programming/languages/php/PEAR-HTML_QuickForm/actions.py
@@ -5,15 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 WorkDir="HTML_QuickForm-3.2.7"
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTML", "QuickForm*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/HTML_QuickForm", "docs/*")
-    # set permissions..
-    items = ['*', '*/*', '*/*/*', '*/*/*/*']
-    for item in items:
-        shelltools.chmod("%s/usr/share/php5/PEAR/doc/HTML_QuickForm/%s" % (get.installDIR(), item))
