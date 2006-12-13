301658cd1ada3dd5bd7f76474c2e60e8a97f56fc
Eren Türkay <turkay.eren@gmail.com>
2006-12-13 19:09:12 +0000

    heheh, konsol adamları için :P

---
+++ applications/network/mcabber/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--with-ssl --with-utf8")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "ChangeLog", "mcabberrc.example", "NEWS", "README", "TODO")
+    pisitools.insinto("/usr/share/mcabber/themes", "contrib/themes/*")
