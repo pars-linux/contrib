a3afd0d9da86f7f13dfd8330894a244e569fa9be
Uğur Çetin <ugur@ugurcetin.com.tr>
2006-12-16 17:54:51 +0000

    ksmoothdock: Version bump

--- desktop/kde/ksmoothdock/actions.py
+++ desktop/kde/ksmoothdock/actions.py
@@ -4,9 +4,10 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import get
 from pisi.actionsapi import kde
 
-WorkDir="ksmoothdock"
+WorkDir="%s-%s_automake1.9" % (get.srcNAME(), get.srcVERSION())
 
 def setup():
     kde.configure()
