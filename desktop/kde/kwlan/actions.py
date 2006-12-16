1bbc08281a9b888ac27e7d83b253c541c27a0d3d
ilker <ilker@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-16 18:03:01 +0000

    First release.
    A    kwlan
    A    kwlan/actions.py
    A    kwlan/pspec.xml

---
+++ desktop/kde/kwlan/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+
+def install():
+	autotools.install()
+    
+def build():
+	autotools.make()
+	
+def setup():
+	autotools.configure() 
