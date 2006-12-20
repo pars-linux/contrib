b55cc5889d209812055ff2c1475730b60d1b60f3
İsmail Dönmez <ismail@users.noreply.github.com>
2006-12-20 21:38:16 +0000

    opera 9.1, removed files with unknown licenses

--- applications/network/opera/actions.py
+++ applications/network/opera/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="opera-9.02-20060919.5-shared-qt.i386-en-434"
+WorkDir="opera-9.10-20061214.6-shared-qt.i386-en-521"
 
 def install():
     shelltools.system("./install.sh DESTDIR=%s" % get.installDIR())
