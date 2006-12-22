fa404b01892a1a7b5f3ba98e7905638381da864e
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-22 12:12:35 +0000

    R enable-BLAS-shlib, deps atlas not needed, deps sun-jdk added

--- programming/languages/R/actions.py
+++ programming/languages/R/actions.py
@@ -8,18 +8,21 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
+def define_global():
+    shelltools.export("R_HOME_DIR", "/usr/lib/R")
 
 def setup():
-    shelltools.export("R_HOME_DIR", "/usr/lib/R")
-    autotools.configure("--enable-R-profiling --enable-R-shlib --enable-shared --with-tcltk --with-tcl-config=/usr/lib/tclConfig.sh --with-tk-config=/usr/lib/tkConfig.sh --with-system-pcre --with-system-zlib --with-system-bzlib --with-jpeglib --with-png --with-readline --with-blas --enable-mbcs --enable-utf8")
+    define_global()
+    autotools.configure("-enable-R-profiling --enable-R-shlib --enable-shared --enable-BLAS-shlib --with-blas --enable-mbcs --enable-utf8 --with-tcltk --with-tcl-config=/usr/lib/tclConfig.sh --with-tk-config=/usr/lib/tkConfig.sh --with-system-pcre --with-system-zlib --with-system-bzlib --with-jpeglib --with-png --with-readline --with-x")
 
 def build():
+    define_global()
     autotools.make()
 
 
 
 def install():
+    define_global()
     autotools.install()
     pisitools.removeDir("/usr/bin")
-    pisitools.remove("/usr/lib/R/bin/R")
     pisitools.dosym("/usr/lib/R/bin/R","/usr/bin/R")
