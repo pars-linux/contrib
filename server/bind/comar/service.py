0457d1d88875b0a9e8dd7fadc726f6966ca4a8fa
erdinc <erdinc@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-23 23:06:10 +0000

    before the dawn

--- server/bind/comar/service.py
+++ server/bind/comar/service.py
@@ -1,18 +1,25 @@
+serviceType = "server"
+serviceDesc = _({"en": "BIND Daemon",
+                 "tr": "BIND DNS Sunucusu"})
+serviceConf = "named"
+
 import os
 from comar.service import *
 
-serviceType = "server"
-serviceDesc = "BIND Daemon"
-
 def start():
-    ret = run("start-stop-daemon --start --quiet --exec /usr/sbin/named --p /var/run/named/named.pid -u named -n 1")
+    ret = run("/sbin/start-stop-daemon --start --quiet --exec /usr/sbin/named --p /var/run/named/named.pid -u named -n \
+              -- %s %s" % (config.get("OPTION", ""), config.get("CPU", "")))
     if ret == 0:
         notify("System.Service.changed", "started")
+    else:
+        fail("Unable to start service")
 
 def stop():
-    ret = run("start-stop-daemon --stop --quiet /var/run/named/named.pid --exec /usr/sbin/named -- stop")
+    ret = run("/sbin/start-stop-daemon --stop --quiet /var/run/named/named.pid --exec /usr/sbin/named -- stop")
     if ret == 0:
         notify("System.Service.changed", "stopped")
+    else:
+        fail("Unable to stop service")
 
 def status():
     return checkDeamon("/var/run/named/named.pid")
