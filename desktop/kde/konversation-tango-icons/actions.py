96c2db37380ef7eeb0aebe61501dd85acfe0e1dd
İlker Kesen <ilkerksn@gmail.com>
2006-12-24 10:43:37 +0000

    First release.
    A    konversation-tango-icons
    A    konversation-tango-icons/actions.py
    A    konversation-tango-icons/pspec.xml

---
+++ desktop/kde/konversation-tango-icons/actions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+
+WorkDir="Tango"
+
+def install():
+    pisitools.insinto("/usr/kde/3.5/share/apps/konversation/themes/tango", "*") 
