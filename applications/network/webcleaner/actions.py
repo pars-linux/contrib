93b65a40f3538dcb5031800e563cdb36954bc5cf
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-28 12:37:26 +0000

    webcleaner, python based proxy filtreling

---
+++ applications/network/webcleaner/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Copyright 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import pythonmodules
+
+
+def setup():
+    autotools.configure("--enable-shared")
+
+def build():
+    autotools.make()
+
+def install():
+    pythonmodules.install()
