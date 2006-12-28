76b17ede8915b978b702241e50f007130a927afe
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-28 07:42:38 +0000

    ssl support and docs

--- applications/network/tmsnc/actions.py
+++ applications/network/tmsnc/actions.py
@@ -8,10 +8,11 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--with-ssl")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+    pisitools.dodoc("AUTHORS", "NEWS", "README", "ChangeLog")
