fd76724a6a3c84f5391d4403dfc100ace964b263
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-29 13:35:16 +0000

    trousers e service betiği eklemesi

---
+++ applications/crypto/trousers/comar/service.py
@@ -0,0 +1,34 @@
+from comar.service import *
+import comar
+import os
+
+serviceType="server"
+serviceDesc = _({"en": "TrouSers",
+                 "tr": "TrouSers"})
+
+
+
+def unlink():
+    try:
+        os.unlink("/var/run/tcsd.pid")
+    except:
+        pass
+
+def start():
+    ret = run("/sbin/start-stop-daemon --start --chuid tss --quiet --background \
+                --pidfile /var/run/tcsd.pid --exec /usr/sbin/tcsd")
+    if ret == 0:
+        notify("System.Service.changed", "started")
+    else:
+        fail("Unable to start service")
+
+def stop():
+    ret = run("/sbin/start-stop-daemon --stop --quiet --pidfile /var/run/tcsd.pid")
+    if ret == 0:
+        unlink()
+        notify("System.Service.changed", "stopped")
+    else:
+        fail("Unable to stop service")
+
+def status():
+    return checkDaemon("--pidfile /var/run/tcsd.pid")
