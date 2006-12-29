7ebcec1823c3871c1f6b33d9436a87a4b9ded5f5
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2006-12-29 08:06:29 +0000

    added qtemu: GUI for QEMU

---
+++ applications/emulators/qtemu/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir = "qtemu"
+
+def build():
+    shelltools.system("qmake-qt4")
+    autotools.make()
+
+def install():
+    shelltools.system("lrelease-qt4 translations/qtemu_de.ts")
+    pisitools.insinto("/usr/share/qtemu", "qtemu")
+    pisitools.insinto("/usr/share/qtemu/translations", "translations/qtemu_de.qm")
+    pisitools.insinto("/usr/share/qtemu", "help")
+
