f97f1eadd83a44989bb953fb8ac8119d79b55619
Eren Türkay <turkay.eren@gmail.com>
2006-12-29 19:25:00 +0000

    o şimdi devel'de

--- programming/languages/php/PEAR-File_Find/comar/package.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall():
-    os.system("chmod 0755 /usr/share/php5/PEAR/File/Find.php")
--- programming/languages/php/PEAR-HTML_QuickForm/comar/package.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall():
-    os.system("chmod -R 0755 /usr/share/php5/PEAR/doc/HTML_QuickForm")
