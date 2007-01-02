dab706b634c55a7ee7903e101fbb0143347427b9
İsmail Dönmez <ismail@users.noreply.github.com>
2007-01-02 18:59:33 +0000

    estetik fixes

--- applications/multimedia/hugin/actions.py
+++ applications/multimedia/hugin/actions.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #
-# Copyright 2005 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 WorkDir="hugin-0.7-beta1"
 
 def setup():
-    autotools.configure("--prefix=/usr --enable-shared --disable-static --with-unicode --disable-desktop")
+    autotools.configure("--enable-shared \
+                         --disable-static \
+                         --with-unicode \
+                         --disable-desktop")
 
 def build():
     autotools.make()
