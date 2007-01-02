461e3ff4fe0446613b11bdee54537e4962a0436b
İsmail Dönmez <ismail@users.noreply.github.com>
2007-01-02 19:05:06 +0000

    estetik

--- applications/multimedia/libpano12/actions.py
+++ applications/multimedia/libpano12/actions.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #
-# Copyright 2005 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--with-java=/opt/sun-jdk")
