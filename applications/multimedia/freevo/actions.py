9b1f1dd2a88fa3ed4544c894a1eaf76ed643fc20
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-04 14:53:22 +0000

    version bump

--- applications/multimedia/freevo/actions.py
+++ applications/multimedia/freevo/actions.py
@@ -7,6 +7,8 @@
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 
+WorkDir = "freevo-1.7.0-rc2"
+
 def install():
     pisitools.dodir("/etc/freevo")
     pisitools.dodir("/var/cache/freevo")
@@ -14,4 +16,5 @@ def install():
 
     pythonmodules.install()
 
+    pisitools.insinto("/usr/share/pixmaps", "share/images/logo_david.png", "freevo.png")
     pisitools.insinto("/etc/freevo", "local_conf.py.example", "local_conf.py")
