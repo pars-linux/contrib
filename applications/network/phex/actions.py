62bf83ce6097ec10373cef263675899acb539419
Uğur Çetin <ugur@ugurcetin.com.tr>
2007-01-08 18:54:49 +0000

    phex: version bump

--- applications/network/phex/actions.py
+++ applications/network/phex/actions.py
@@ -8,12 +8,10 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "phex_2.8.10.98"
+WorkDir = "phex_3.0.0.99"
 
 def install():
-    #turkish patch
-    shelltools.system("zip -r phex.jar phex && rm -fr phex")
-
     pisitools.dodir("/usr/share/pixmaps")
-    shelltools.system("unzip -j phex.jar phex/gui/resources/icons/phex/phex_wiz.gif -d %s/usr/share/pixmaps" % get.installDIR())
+    shelltools.system("unzip -j lib/phex.jar phex/gui/resources/icons/phex/phex_wiz.gif -d %s/usr/share/pixmaps" % get.installDIR())
     pisitools.insinto("/opt/phex", "*")
+
