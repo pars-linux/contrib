3b2044f80b1fc69c05fd2a5f71639192c0112de9
dfisek <dfisek@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-16 13:46:20 +0000

    MySQL Server GUI Administration Tool

---
+++ applications/network/mysql-administrator/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+WorkDir='mysql-gui-tools-5.0r8'
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+
+def setup():
+    shelltools.cd('mysql-gui-common')
+    autotools.configure()
+    shelltools.cd('../mysql-administrator')
+    autotools.configure()
+
+def build():
+    shelltools.cd('mysql-gui-common')
+    autotools.make()
+    shelltools.cd('../mysql-administrator')
+    autotools.make()
+
+def install():
+    shelltools.cd('mysql-administrator')
+    autotools.install()
