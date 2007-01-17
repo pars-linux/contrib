e5fc3ee4d8e534a0006ab390bd1ed53202d4dcc0
dfisek <dfisek@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-17 00:54:18 +0000

    MySQL Database Modelling Tool

---
+++ applications/network/mysql-workbench/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+WorkDir='mysql-gui-tools-5.0r8'
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+
+def setup():
+    shelltools.cd('mysql-gui-common')
+    autotools.configure('--enable-grt --enable-canvas')
+    shelltools.cd('../mysql-workbench')
+    autotools.configure('--enable-python-modules')
+
+def build():
+    shelltools.cd('mysql-gui-common')
+    autotools.make()
+    shelltools.cd('../mysql-workbench')
+    autotools.make()
+
+def install():
+    shelltools.cd('mysql-workbench')
+    autotools.install()
