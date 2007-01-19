a5e99eb7c451f3728a32189cb5611760dd891369
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-19 18:59:11 +0000

    version bump

--- applications/multimedia/herrie/actions.py
+++ applications/multimedia/herrie/actions.py
@@ -5,14 +5,14 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 
 def build():
-    shelltools.cd("src")
     autotools.make()
 
 def install():
     pisitools.dobin("src/herrie")
+    pisitools.domo("lang/nl.po", "nl", "herrie.mo")
 
+    pisitools.doman("herrie.1")
     pisitools.dodoc("COPYING", "ChangeLog", "TODO", "README")
