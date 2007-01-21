cd2257593dba0df448e6259961f40c33f998649a
Ertuğrul Erata <ertugrulerata@gmail.com>
2007-01-21 11:48:20 +0000

    tork use external geoip, tr mo added

--- applications/network/tork/actions.py
+++ applications/network/tork/actions.py
@@ -9,11 +9,13 @@ from pisi.actionsapi import kde
 from pisi.actionsapi import pisitools
 
 def setup():
-    kde.configure()
+    kde.configure("--with-external-geoip")
 
 def build():
     kde.make()
 
 def install():
     kde.install()
+    pisitools.domo("po/tr.po","tr","tork.mo")
+    pisitools.domove("/usr/share/locale/tr/LC_MESSAGES/tork.mo","/usr/kde/3.5/share/locale/tr/LC_MESSAGES")
     pisitools.removeDir("/usr/kde/3.5/share/icons/crystalsvg")
