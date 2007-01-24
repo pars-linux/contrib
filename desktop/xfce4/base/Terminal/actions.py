96356ce4571e150841e03e188bc22e45f7099ae2
Eren Türkay <turkay.eren@gmail.com>
2007-01-24 15:59:55 +0000

    new version

--- desktop/xfce4/base/Terminal/actions.py
+++ desktop/xfce4/base/Terminal/actions.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
-WorkDir="Terminal-0.2.5.8rc2"
-
 def setup():
-    autotools.configure("--enable-dbus")
+    autotools.configure("--enable-static=no")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+    pisitools.dodoc("TODO", "THANKS", "README", "NEWS", "HACKING", "ChangeLog", "AUTHORS")
+    # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
-    pisitools.removeDir("/usr/share/doc/Terminal/ja")
