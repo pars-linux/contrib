e616fd628495016a3c7f6dd274499d1ed5bcd99a
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-24 13:20:15 +0000

    version bump to stable

--- desktop/xfce4/base/xfce4-appfinder/actions.py
+++ desktop/xfce4/base/xfce4-appfinder/actions.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
-#Ertugrul Erata ertugrulerata at gmail.com
-#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
@@ -12,10 +13,7 @@ def setup():
 def build():
     autotools.make()
 
-
 def install():
     autotools.install()
-    pisitools.removeDir("/usr/share/xfce4/doc/fr")
-    pisitools.removeDir("/usr/share/xfce4/doc/he")
+    pisitools.dodoc("README", "NEWS", "TODO", "ChangeLog", "AUTHORS")
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
-    pisitools.domo("po/tr.po","tr","xfce4-appfinder.mo")
