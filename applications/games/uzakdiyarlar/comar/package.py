b15e9a672017f0df9db9dc387bbbadf83311f510
Eren Türkay <turkay.eren@gmail.com>
2007-01-24 06:39:37 +0000

    better security and functionality

--- applications/games/uzakdiyarlar/comar/package.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall():
-    os.system("chmod 0777 /usr/share/uzakdiyarlar/log")
-    os.system("chmod 0777 /usr/share/uzakdiyarlar/player")
-    os.system("chmod 0755 /usr/share/uzakdiyarlar/area/*")
