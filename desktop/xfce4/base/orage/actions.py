1be29a8b0490d61775bb0c506cbc6246d02cbb0f
Eren Türkay <turkay.eren@gmail.com>
2007-01-24 16:00:42 +0000

    new release

--- desktop/xfce4/base/orage/actions.py
+++ desktop/xfce4/base/orage/actions.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
-#Ertugrul Erata ertugrulerata at gmail.com
-#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
-
-
 def setup():
-    autotools.configure("--with-bdb4")
+    autotools.configure("--with-bdb4 --enable-static=no")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+    pisitools.dodoc("NEWS", "README", "ChangeLog", "AUTHORS")
+    # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
