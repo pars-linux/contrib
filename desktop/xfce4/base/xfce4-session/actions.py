996e8678b08ad7ab86abc05daa046d0217af938a
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-24 14:06:03 +0000

    new release

--- desktop/xfce4/base/xfce4-session/actions.py
+++ desktop/xfce4/base/xfce4-session/actions.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
-#Ertugrul Erata ertugrulerata at gmail.com
-#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
-
 def setup():
-    autotools.configure()
+    autotools.configure("--enable-static=no")
 
 def build():
     autotools.make()
 
-
 def install():
     autotools.install()
-    pisitools.removeDir("/usr/share/xfce4/doc/fr")
-    pisitools.removeDir("/usr/share/xfce4/doc/he")
+    pisitools.dodoc("TODO", "README", "NEWS", "ChangeLog", "BUGS", "AUTHORS")
+    # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
-    pisitools.domo("po/tr.po","tr","xfce4-session.mo")
