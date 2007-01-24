ec40b45f19086e59aa208ab4f9abc7435f039c5c
Eren Türkay <turkay.eren@gmail.com>
2007-01-24 10:28:11 +0000

    version bump to stable

--- desktop/xfce4/base/libxfce4util/actions.py
+++ desktop/xfce4/base/libxfce4util/actions.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
-#Ertugrul Erata ertugrulerata at gmail.com
-#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
-
 def setup():
-    autotools.configure()
+    autotools.configure("--enable-static=no")
 
 def build():
     autotools.make()
@@ -18,3 +17,4 @@ def build():
 def install():
     autotools.install()
     pisitools.removeDir("/usr/share/gtk-doc")
+    pisitools.dodoc("README", "NEWS", "TODO", "THANKS", "ChangeLog", "AUTHORS")
