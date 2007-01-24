2aa2705e2a3b059d849887e42ec660e60489852a
Eren Türkay <turkay.eren@gmail.com>
2007-01-24 11:37:43 +0000

    version bump to stable

--- desktop/xfce4/base/xfce4-icon-theme/actions.py
+++ desktop/xfce4/base/xfce4-icon-theme/actions.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
-#Ertugrul Erata ertugrulerata at gmail.com
-#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
-
+from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure()
@@ -13,6 +13,6 @@ def setup():
 def build():
     autotools.make()
 
-
 def install():
     autotools.install()
+    pisitools.dodoc("README", "NEWS", "ChangeLog", "AUTHORS")
