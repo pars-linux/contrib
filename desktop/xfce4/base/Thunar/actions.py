6ac50ada76055d002b59718755ae0803f9a9631a
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-24 16:16:20 +0000

    version bump to stable

--- desktop/xfce4/base/Thunar/actions.py
+++ desktop/xfce4/base/Thunar/actions.py
@@ -1,20 +1,20 @@
-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
-WorkDir="Thunar-0.5.0rc2"
-
 def setup():
-    autotools.configure("--enable-dbus \
-                         --disable-gnome-thumbnailers \
-                         --enable-startup-notification \
-                         --with-volume-manager=hal \
-                         ")
+    autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+    pisitools.dodoc("TODO", "THANKS", "README", "HACKING", "FAQ", "AUTHORS", "ChangeLog")
+    # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
