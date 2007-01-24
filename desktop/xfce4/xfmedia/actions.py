d564e449a3e5c6da075a63a706ced1fb64a2a977
Eren Türkay <turkay.eren@gmail.com>
2007-01-24 15:14:02 +0000

    make-up

--- desktop/xfce4/xfmedia/actions.py
+++ desktop/xfce4/xfmedia/actions.py
@@ -4,17 +4,17 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--enable-static=no")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-    # remove conflict: /usr/share/icons/hicolor/icon-theme.cache from xfce-mcs-manager
+    pisitools.dodoc("TODO", "README*", "NEWS", "ChangeLog", "AUTHORS")
+    # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
