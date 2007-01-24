bf765f1fc598813d230e1ccbf9fe453a9557b466
Eren Türkay <turkay.eren@gmail.com>
2007-01-24 13:08:36 +0000

    make-up

--- desktop/xfce4/goodies/notification-daemon-xfce/actions.py
+++ desktop/xfce4/goodies/notification-daemon-xfce/actions.py
@@ -4,17 +4,17 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--enable-static=no")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-    pisitools.insinto("/usr/libexec/","src/daemon/notification-daemon-xfce")
+    pisitools.dodoc("README", "NEWS", "ChangeLog", "AUTHORS")
+    # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
