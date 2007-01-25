f43a8e3a6ddf7a96c8b4a5b2bde499e178508eb7
Murat Senel <msenel@gmail.com>
2007-01-25 20:51:36 +0000

    version bump

--- applications/games/gcompris/actions.py
+++ applications/games/gcompris/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "gcompris-8.2BETA1"
+WorkDir = "gcompris-8.2.2-2"
 
 def setup():
     autotools.configure("--disable-sdltest \
