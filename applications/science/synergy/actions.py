fc62338f4e83b3987c86debaad4987ba379b4957
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-25 10:31:47 +0000

    '- fix' and cleanup

--- applications/science/synergy/actions.py
+++ applications/science/synergy/actions.py
@@ -4,10 +4,7 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 WorkDir = "weber"
 NoStrip = "/"
@@ -16,3 +13,4 @@ def install():
     pisitools.insinto("/usr/weber", "*")
     pisitools.insinto("/usr/share/applications", "Synergy.desktop")
     pisitools.dosym("/usr/weber/bin/synergy", "/usr/bin/synergy")
+
