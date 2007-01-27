e6a015d356e41a797556b437e7d8f0530899e656
ayan <ayan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-27 23:49:46 +0000

    ve gEDA minimal.. suite daha sonra

---
+++ applications/science/electronics/geda/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006, 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+
+from pisi.actionsapi import autotools
+
+WorkDir = "geda-gattrib-20061020"
+
+def setup():
+    autotools.configure("--disable-dependency-tracking \
+                         --with-docdir=/usr/share/doc \
+                         --with-pcbconfdir=/usr/share/pcb \
+                         --with-pcbm4dir=/usr/share/pcb/m4")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
