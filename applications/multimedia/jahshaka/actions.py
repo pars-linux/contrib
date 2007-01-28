88b51a30502796dfd1f109b058d38cb4eb11526c
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2007-01-28 15:31:40 +0000

    Real-time video compositing and editing software

---
+++ applications/multimedia/jahshaka/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = 'jahshaka'
+
+def setup():
+    autotools.configure('--disable-debug')
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall('INSTALL_ROOT="%s"' % get.installDIR())
+    pisitools.dodoc('COPYING', 'INSTALL', 'README', 'TODO')
