0ce90e459473b388a1b714d6270042227cb37af5
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-29 07:13:32 +0000

    mlt ve mlt++ problemleri de çözüldü, devel'e girmeye hazır gibi görünüyor?

--- applications/multimedia/jahshaka/actions.py
+++ applications/multimedia/jahshaka/actions.py
@@ -6,6 +6,7 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 WorkDir = 'jahshaka'
@@ -14,6 +15,7 @@ def setup():
     autotools.configure('--disable-debug')
 
 def build():
+    shelltools.export("QTDIR", "%s" % get.qtDIR())
     autotools.make()
 
 def install():
