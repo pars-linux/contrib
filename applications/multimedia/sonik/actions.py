1b1a64d43468fc39a1c0e3afc45b174144fbf27a
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-29 10:59:27 +0000

    what's autoheader? autoconf? :)

--- applications/multimedia/sonik/actions.py
+++ applications/multimedia/sonik/actions.py
@@ -5,15 +5,11 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import kde
-from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 
 def setup():
     kde.configure()
 
 def build():
-    shelltools.system("autoheader")
-    autotools.autoconf()
     kde.make()
 
 def install():
