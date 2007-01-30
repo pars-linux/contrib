53263c779a7aed185c31f6bea8ee4d08a59870f9
loker <loker@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-01-30 22:12:29 +0000

    kdenlive is in devel rather than kino, kino can stay in contrib

---
+++ applications/multimedia/kino/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure('--disable-debug \
+                         --disable-dependency-tracking \
+			 --enable-quicktime')
+    
+def build():
+    autotools.make()
+    
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("README", "ABOUT-NLS", "BUGS", "AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "TODO", "README_jogshuttle")
