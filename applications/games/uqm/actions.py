a927cbdab3f469ff2ab6a3a614f3169fc5d7c54d
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-02 17:19:44 +0000

    nostalji severlere için, Star Control II: The Ur-Quan Masters

---
+++ applications/games/uqm/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+WorkDir = "%s-%s/sc2" % (get.srcNAME(), get.srcVERSION())
+
+def build():
+    shelltools.system("sh build.sh uqm")
+
+def install():
+    pisitools.dobin("uqm", "/usr/share/uqm")
+    pisitools.dodoc("README", "COPYING", "AUTHORS", "doc/users/manual.txt")
