d7d9d6bdabb4a3b19b356df91758074b567ade41
ilker <ilker@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-03 13:11:51 +0000

    Updated.

--- desktop/blackbox/bbrb/actions.py
+++ desktop/blackbox/bbrb/actions.py
@@ -7,10 +7,9 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
-WorkDir = "bbrb-0.4.1"
-
 def setup():
     autotools.configure()
+
 def build():
     autotools.make()
 
