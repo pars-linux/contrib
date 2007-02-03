81c26b7677e8ecb044d5985c69ab8d88d4b516ad
İlker Kesen <ilkerksn@gmail.com>
2007-02-03 13:13:24 +0000

    Updated.

--- desktop/blackbox/bbweather/actions.py
+++ desktop/blackbox/bbweather/actions.py
@@ -8,10 +8,9 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "bbweather-0.6.2"
-
 def setup():
     autotools.configure()
+
 def build():
     autotools.make()
 
