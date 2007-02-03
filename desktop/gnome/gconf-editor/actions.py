b1535a73cb93da37df4118c443ae1b4cb7880fad
Ayan Öztürk <ayanozturk@gmail.com>
2007-02-03 13:50:46 +0000

    version bump

--- desktop/gnome/gconf-editor/actions.py
+++ desktop/gnome/gconf-editor/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
@@ -17,5 +17,4 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.removeDir("/var/lib/scrollkeeper")
     pisitools.dodoc("ABOUT-NLS", "AUTHORS", "ChangeLog", "COPYING*", "INSTALL", "NEWS", "README", "TODO")
