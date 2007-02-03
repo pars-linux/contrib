0bc59ef794a8021e08acbea5231631810c3d7f63
Ayan Öztürk <ayanozturk@gmail.com>
2007-02-03 13:18:44 +0000

    version bump

--- desktop/gnome/libbonoboui/actions.py
+++ desktop/gnome/libbonoboui/actions.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+
+WorkDir = "libbonoboui-2.17.90"
 
 def setup():
     autotools.configure("--enable-debug")
