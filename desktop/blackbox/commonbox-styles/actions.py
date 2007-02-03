e4487b7919c7eda1735ae4d84e2cb99b7a15125c
ilker <ilker@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-03 13:14:04 +0000

    Updated.

--- desktop/blackbox/commonbox-styles/actions.py
+++ desktop/blackbox/commonbox-styles/actions.py
@@ -4,10 +4,7 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-
-WorkDir = "commonbox-styles-0.6"
 
 def install():
     pisitools.insinto("/usr/share/commonbox/backgrounds", "backgrounds/*")
