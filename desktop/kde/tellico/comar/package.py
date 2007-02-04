394c9a113dd7c48810d847638557ae173a049a5b
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2007-02-04 22:24:55 +0000

    Aka omrumu yedin hatali svn hook'u (yine de duzeldikten sonra ise yaradi tabii), Caglar sen o aktarmayi daha sok ararsin :)
    Fix comar script path. Thanks to Gurer and Onur for comar help.
    Fixed pspec.xml syntax, thanks to rng svn hook.
    Add icon, though package manager doesn't show the package cos there's no build number.
    
    Version bump to 1.2.8 :
    
        * Fixed bug with secondary and tertiary sorting.
        * Fixed bug with some z39.50 search results not showing up.
        * Fixed bug with comparing relative URLs when importing a file catalog.
        * Fixed "ISBN Not Found" dialog to have selectable text.
        * Fixed "busy cursor doesn't go away" in the download stuff dialog.
        * Fixed the newstuff download to uninstall cleanly.
        * Fixed compilation error in yaz include, patch from Markus Brueffer.
        * Updated CDDB import to grab extd data as comments, and category as keyword.
        * Updated audio file importer to take disc number into account for mp3, ogg, and flac files.
        * Changed IMDB rating to allow float values.
        * Updated translations for pt and pt_BR.

--- desktop/kde/tellico/comar/package.py
+++ desktop/kde/tellico/comar/package.py
@@ -2,24 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import os
+from comar import service
+
+service.loadEnvironment()
+kdedir = os.environ["KDEDIR"]
 
 def postInstall():
     os.system("/usr/bin/xmlcatalog --noout --add \"delegatePublic\" \
                \"-//Robby Stephenson/DTD Tellico V9.0//EN\" \
-               \"file:///usr/share/apps/tellico/tellico.dtd\" \
-               /etc/xml/catalog")
+               \"file://%s/share/apps/tellico/tellico.dtd\" \
+               /etc/xml/catalog" % kdedir)
 
     os.system("/usr/bin/xmlcatalog --noout --add \"delegateSystem\" \
                \"http://www.periapsis.org/tellico/dtd/v9/tellico.dtd\" \
-               \"file:///usr/share/apps/tellico/tellico.dtd\" \
-               /etc/xml/catalog")
+               \"file://%s/share/apps/tellico/tellico.dtd\" \
+               /etc/xml/catalog" % kdedir)
 
     os.system("/usr/bin/xmlcatalog --noout --add \"delegateURI\" \
                \"http://www.periapsis.org/tellico/dtd/v9/tellico.dtd\" \
-               \"file:///usr/share/apps/tellico/tellico.dtd\" \
-               /etc/xml/catalog")
+               \"file://%s/share/apps/tellico/tellico.dtd\" \
+               /etc/xml/catalog" % kdedir)
 
 def preRemove():
     os.system("/usr/bin/xmlcatalog --noout --del \
-               \"file:///usr/share/apps/tellico/tellico.dtd\" \
-               /etc/xml/catalog")
+               \"file://%s/share/apps/tellico/tellico.dtd\" \
+               /etc/xml/catalog" % kdedir)
