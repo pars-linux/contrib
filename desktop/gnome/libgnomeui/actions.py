20564cb137b951ad30d1a8046a82bfe49465fdda
ayan <ayan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-05 23:39:16 +0000

    version bump

--- desktop/gnome/libgnomeui/actions.py
+++ desktop/gnome/libgnomeui/actions.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.copy("aclocal.m4", "old_macros.m4")
-    shelltools.export("AT_M4DIR", "")
-    shelltools.export("WANT_AUTOCONF", "1.7")
-    autotools.autoconf()
     autotools.configure("--enable-jpeg")
 
 def build():
-    autotools.make("-j1")
+    autotools.make()
 
 def install():
     autotools.install()
