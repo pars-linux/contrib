85e63cc8d2ac47757a13d8586897f153b670b9d6
Ayan Öztürk <ayanozturk@gmail.com>
2007-02-05 23:23:48 +0000

    version bump

--- desktop/gnome/libgnome/actions.py
+++ desktop/gnome/libgnome/actions.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("WANT_AUTOMAKE", "1.7")
-    shelltools.copy("aclocal.m4", "old_macros.m4")
-    autotools.aclocal("-I .")
-    autotools.autoconf()
-    autotools.configure("--disable-schemas-install --enable-static --enable-esd")
+    autotools.configure()
 
 def build():
     autotools.make()
