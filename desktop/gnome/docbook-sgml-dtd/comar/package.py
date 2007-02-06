7239a449d422f2d421bae358ef953f811232148e
Eren Türkay <turkay.eren@gmail.com>
2007-02-06 17:59:56 +0000

    get rid of Fethi's packages

--- desktop/gnome/docbook-sgml-dtd/comar/package.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall():
-    os.system("/usr/bin/install-catalog --add /etc/sgml/sgml-docbook-4.4.cat /usr/share/sgml/docbook/sgml-dtd-4.4/catalog")
-    os.system("/usr/bin/install-catalog --add /etc/sgml/sgml-docbook-4.4.cat /etc/sgml/sgml-docbook.cat")
