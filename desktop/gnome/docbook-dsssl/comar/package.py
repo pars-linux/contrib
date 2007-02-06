c55b5417b49ebb0c24b083764f16bac1e52bf367
Eren Türkay <turkay.eren@gmail.com>
2007-02-06 18:03:56 +0000

    get rid of gnome packages

--- desktop/gnome/docbook-dsssl/comar/package.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall():
-    os.system("/usr/bin/install-catalog --add /etc/sgml/dsssl-docbook-stylesheets.cat \
-    /usr/share/sgml/docbook/dsssl-stylesheets-1.79/catalog")
-    os.system("/usr/bin/install-catalog --add /etc/sgml/sgml-docbook.cat /etc/sgml/dsssl-docbook-stylesheets.cat")
