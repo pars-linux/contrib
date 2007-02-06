46926c44d960f753540311fb5ee4750f8bf50041
Eren Türkay <turkay.eren@gmail.com>
2007-02-06 18:07:54 +0000

    get rid of gnome packages

--- desktop/gnome/gnome-alsamixer/comar/package.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall():
-    os.system("/usr/bin/update-desktop-database -q /usr/share/applications")
-    os.system("/usr/bin/update-mime-database /usr/share/mime")
