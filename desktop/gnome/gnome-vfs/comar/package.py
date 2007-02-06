c86890f09bec2721cbab95c5f8b1a5302bd13d5b
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-06 18:15:42 +0000

    get rid of gnome packages

--- desktop/gnome/gnome-vfs/comar/package.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def postInstall():
-    os.system("gconftool-2 --install-schema-file=/etc/gconf/schemas/desktop_default_applications.schemas")
-    os.system("gconftool-2 --install-schema-file=/etc/gconf/schemas/desktop_gnome_url_handlers.schemas")
-    os.system("gconftool-2 --install-schema-file=/etc/gconf/schemas/system_dns_sd.schemas")
-    os.system("gconftool-2 --install-schema-file=/etc/gconf/schemas/system_http_proxy.schemas")
-    os.system("gconftool-2 --install-schema-file=/etc/gconf/schemas/system_smb.schemas")
