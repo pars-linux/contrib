be387874fdd81746c8ad6b0193fee3ae047b5ebe
ayan <ayan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-06 11:44:33 +0000

    change packager

--- desktop/gnome/oaf/actions.py
+++ desktop/gnome/oaf/actions.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
