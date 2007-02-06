94d8e12d64c395e2601e9c77c45139a9c881e365
Eren Türkay <turkay.eren@gmail.com>
2007-02-06 18:13:07 +0000

    get rid of gnome packages

--- desktop/gnome/gnome-print/comar/package.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def postInstall():
-    os.system("ldconfig >/dev/null 2>/dev/null")
-    os.system("perl /usr/share/fonts/run-gnome-font-install \
-              /usr/bin/gnome-font-install \
-              /usr/share/fonts /usr/share/fonts /etc >/dev/null 2>/dev/null")
