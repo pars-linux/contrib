91eabc5d6fc812e58e6cd504ebb87cb60cd244c5
Eren Türkay <turkay.eren@gmail.com>
2007-02-06 18:01:34 +0000

    get rid of gnome packages

--- desktop/gnome/docbook-xml/comar/package.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def postInstall():
-    os.system("/usr/bin/install-catalog --add /etc/sgml/xml-docbook-4.5.cat /usr/share/sgml/docbook/xml-dtd-4.5/docbook.cat")
-    os.system("/usr/bin/build-docbook-catalog")
