61478e27a53efddd8be2e1f23cb0d318c2b7d403
Eren Türkay <turkay.eren@gmail.com>
2007-02-06 19:23:46 +0000

    get contrib stable

--- desktop/gnome/sgml-common/comar/package.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def postInstall():
-    os.system("/usr/bin/install-catalog --add /etc/sgml/sgml-ent.cat /usr/share/sgml/sgml-iso-entities-8879.1986/catalog")
-    os.system("/usr/bin/install-catalog --add /etc/sgml/sgml-docbook.cat /etc/sgml/sgml-ent.cat")
