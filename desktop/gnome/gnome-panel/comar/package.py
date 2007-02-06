46fc329d9d60923cce1729f6f8c08e8200b171f9
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-06 18:11:33 +0000

    get rid of gnome packages

--- desktop/gnome/gnome-panel/comar/package.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-#-*- coding: UTF-8 -*-
-
-import os
-
-def postInstall():
-   os.system("/usr/bin/gconftool-2 --direct --config-source \
-             `/usr/bin/gconftool-2 --get-default-source` --load=/etc/gconf/schemas/panel-default-setup.entries")
