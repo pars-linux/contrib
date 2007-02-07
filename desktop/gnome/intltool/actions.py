095227b0ae7f296dda9f81d9d0b5f988e30a0e9d
Eren Türkay <turkay.eren@gmail.com>
2007-02-07 12:43:50 +0000

    Version bump and change pakager

--- desktop/gnome/intltool/actions.py
+++ desktop/gnome/intltool/actions.py
@@ -7,7 +7,6 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
     autotools.configure()
@@ -16,6 +15,5 @@ def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "TODO", "INSTALL", "doc/I18N-HOWTO")
+    autotools.install()
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "TODO", "doc/I18N-HOWTO")
