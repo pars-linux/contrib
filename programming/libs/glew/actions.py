fb182b60540369f85f17d5d5a2b9ba51f8858920
Koray Löker <loker@radiobrecht.org>
2007-02-07 21:22:28 +0000

    jahshaka ve bağımlılıkları tam olarak geri döner...

---
+++ programming/libs/glew/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+WorkDir = 'glew'
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("GLEW_DEST=%s/usr" % get.installDIR())
+
+    # No static libraries
+    pisitools.remove("/usr/lib/libGLEW.a")
+
+    pisitools.dodoc("README.txt", "doc/*.txt")
+    pisitools.dohtml("doc/*")
