348a714e1677f7e61846cc786ea33eb86ba443c4
Uğur Çetin <ugur@ugurcetin.com.tr>
2007-02-08 11:31:13 +0000

    makeup

--- desktop/kde/kstreamripper/actions.py
+++ desktop/kde/kstreamripper/actions.py
@@ -19,3 +19,4 @@ def install():
     scons.install()
     pisitools.insinto("%s/share/icons/hicolor/32x32/apps" % get.kdeDIR(), "src/hi32-app-kstreamripper.png", "kstreamripper.png")
     pisitools.insinto("%s/share/icons/hicolor/16x16/apps" % get.kdeDIR(), "src/hi16-app-kstreamripper.png", "kstreamripper.png")
+
