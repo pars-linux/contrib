a03be5d1c659ec3eb2a5fb3b3502d277465a0817
Fatih Aşıcı <fatih-asici@users.noreply.github.com>
2007-02-09 19:16:07 +0000

    bump

--- applications/science/aqsis/actions.py
+++ applications/science/aqsis/actions.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 TUBITAK/UEKAE
+# Copyright 2006-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import scons
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
-WorkDir = "aqsis-1_1_0"
-
-def build():
-    scons.make("no_exr=1")
+WorkDir = "dist_aqsis-1.2.0rc2"
 
 def install():
-    pisitools.dobin("output/bin/aqs*")
-    pisitools.dobin("output/bin/*ser")
+    shelltools.export("LD_LIBRARY_PATH", "%s/%s/lib" % (get.installDIR(), get.defaultprefixDIR()))
+    scons.install("install_prefix=/%s sysconfdir=/%s/aqsis install" % (get.defaultprefixDIR(), get.confDIR()), get.installDIR(), "destdir")
 
-    pisitools.dolib("output/bin/*.so", "/usr/lib/aqsis")
-    pisitools.domove("/usr/lib/aqsis/libaqsis.so", "/usr/lib")
-
-    pisitools.insinto("/etc", "output/etc/aqsisrc")
-    pisitools.insinto("/usr/include/aqsis", "output/include/aqsis/*")
-    pisitools.insinto("/usr/share/aqsis/shaders", "output/shaders/*")
-
-    pisitools.dodoc("AUTHORS", "COPYING", "README*")
-
+    pisitools.dodoc("AUTHORS", "COPYING", "README", "ReleaseNotes")
