d42c7c37a5d1f126f4413278d50c322c0f6f70ba
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-10 10:07:29 +0000

    heta olmuş

--- desktop/kde/servicemenus/kpaste/actions.py
+++ desktop/kde/servicemenus/kpaste/actions.py
@@ -5,9 +5,10 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def install():
     pisitools.insinto("/usr/bin", "src/kpaste")
     pisitools.insinto("/usr/share/pixmaps", "src/kpaste.png")
-    pisitools.insinto("/usr/kde/3.5/share/apps/konqueror/servicemenus", "src/kpaste.desktop")
+    pisitools.insinto("%s/share/apps/konqueror/servicemenus" % get.kdeDIR(), "src/kpaste.desktop")
     pisitools.dodoc("AUTHORS", "README")
