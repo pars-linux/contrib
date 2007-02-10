de6c6edf308577565ef3b444f2609e8457ec3d81
Eren Türkay <turkay.eren@gmail.com>
2007-02-10 17:35:42 +0000

    heta yapmişsin murat, gcompris çeviri eksikleri seni bekler :P

--- applications/games/gcompris/actions.py
+++ applications/games/gcompris/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "gcompris-8.2.2-2"
+WorkDir = "gcompris-8.2.2"
 
 def setup():
     autotools.configure("--disable-sdltest \
