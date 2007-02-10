e5f2c76f1dffdbb228838ed6f311f04f7e5993aa
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-10 16:12:09 +0000

    Amarokta ne dinlediğinizi ve albüm kapaklarını her zaman panelde gösteren bir uygulama

---
+++ desktop/kde/kirocker/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import kde
+
+WorkDir="kirocker"
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
