e03ec85d54f961b528d4b51ec03823cb223dea44
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-10 16:31:16 +0000

    doesn't needed

--- programming/languages/python/gnome-python/actions.py
+++ programming/languages/python/gnome-python/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--prefix=/usr --disable-glibtest --disable-gtktest")
+    autotools.configure("--disable-glibtest --disable-gtktest")
 
 def build():
     autotools.make()
