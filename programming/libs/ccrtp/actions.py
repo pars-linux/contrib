463acf2f26e3a96f2f8432da93d240495ea17be3
ayan <ayan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-11 10:36:22 +0000

    düzenle

--- programming/libs/ccrtp/actions.py
+++ programming/libs/ccrtp/actions.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2007, TUBITAK/UEKAE
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free
-# Software Foundation; either version 2 of the License, or (at your option)
-# any later version.
+# Copyright 2005-2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-
 
 def setup():
     autotools.configure()
@@ -21,4 +17,3 @@ def build():
 def install():
     autotools.install()
     pisitools.dodoc("README","ChangeLog","AUTHORS","NEWS","TODO")
-
