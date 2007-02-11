fb1a18bfef2cbb76baf6bce671a1740ed8a53985
Eren Türkay <turkay.eren@gmail.com>
2007-02-11 09:34:42 +0000

    add

---
+++ desktop/xfce4/base/xfprint/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--enable-static=no \
+                                  --enable-cups \
+                                  --disable-bsdlpr")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("README", "TODO", "NEWS", "AUTHORS", "ChangeLog")
+    # conflict
+    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
