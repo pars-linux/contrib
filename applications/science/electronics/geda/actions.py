5c206e34d309378f03d98632e60eb0f06f966ed8
Ayan Öztürk <ayanozturk@gmail.com>
2007-02-11 14:10:10 +0000

    copyright

--- applications/science/electronics/geda/actions.py
+++ applications/science/electronics/geda/actions.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006, 2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
-
 
 from pisi.actionsapi import autotools
 
@@ -21,4 +19,3 @@ def build():
 
 def install():
     autotools.install()
-
