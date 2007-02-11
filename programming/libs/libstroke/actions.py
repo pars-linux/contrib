ee41318d1fa12cfb37817df4fd915084eedc8c8d
Ayan Öztürk <ayanozturk@gmail.com>
2007-02-11 14:15:51 +0000

    copyright

--- programming/libs/libstroke/actions.py
+++ programming/libs/libstroke/actions.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006, 2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-
 
 def setup():
     autotools.configure()
@@ -18,4 +16,3 @@ def build():
 def install():
     autotools.install()
     pisitools.dodoc("CREDITS","ChangeLog","README")
-
