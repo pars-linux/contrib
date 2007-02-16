1bd0863729860bfe0d48de33e840adf02f75cb36
Eren Türkay <turkay.eren@gmail.com>
2007-02-16 18:52:31 +0000

    add clean package

---
+++ desktop/gnome/docbook-xsl/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pisitools
+
+def install():
+    items = ['template', 'fo', 'xhtml', 'profiling', 'website', 'images', 'html', 'params', 'htmlhelp', 'roundtrip', 'tools', 'highlighting', 'slides', 'docsrc', 'eclipse', 'manpages', 'javahelp', 'extensions', 'common', 'lib']
+    for item in items:
+        pisitools.insinto("/usr/share/sgml/docbook/xsl-stylesheets-1.72.0/%s" % item, "%s/*" % item)
+    pisitools.dodoc("AUTHORS", "BUGS", "COPYING", "NEWS", "README", "RELEASE-NOTES.txt", "TODO", "VERSION")
