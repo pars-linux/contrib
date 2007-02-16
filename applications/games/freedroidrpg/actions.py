2972552523bccf826a4e30c7d6cbecc3cde9bd09
Murat Senel <msenel@gmail.com>
2007-02-16 23:20:14 +0000

    Fix permissions, adjust icon file, fix deps and icon tag added

--- applications/games/freedroidrpg/actions.py
+++ applications/games/freedroidrpg/actions.py
@@ -5,9 +5,22 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi.libtools import gnuconfig_update
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+
+import os
+
+WorkDir = "freedroidrpg-%s" % get.srcVERSION()
+NoStrip = "/"
+
+def fixperms(d):
+    for root, dirs, files in os.walk(d):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
 
 def setup():
     gnuconfig_update()
@@ -25,4 +38,13 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
+    # Croppy is a standalone program to post-process png files. It crops PNG files to teh smallest dimension without losing data. So, we don't need this for game play...
+    pisitools.remove("/usr/bin/croppy")
+
+    # Pngtoico takes the FDRPG png files and turns them into Win32 .ico files and then those .ico files get compiled into the Win32 binary. So, we don't need this neither for game play...
+    pisitools.remove("/usr/bin/pngtoico")
+
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "FILES", "HELP_WANTED", "INSTALL*", "IDEAS", "NEWS", "README")
+
+    for d in ["dialogs", "graphics", "map", "sound"]:
+        fixperms("%s/usr/share/freedroidrpg/%s" % (get.installDIR(), d))
