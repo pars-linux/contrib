588bdd315dc99aa50b06ecbca8c0d751cbf7b63b
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-16 22:48:39 +0000

    correct installation

--- applications/editors/gedit/comar/package.py
+++ applications/editors/gedit/comar/package.py
@@ -3,4 +3,11 @@
 import os
 
 def postInstall():
-    os.system("/usr/bin/scrollkeeper-update -q -p /var/lib/scrollkeeper -o /usr/share/omf")
+    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gedit")
+
+def preRemove():
+    PATH = "/usr/share/omf/gedit"
+
+    for root, dirs, files in os.walk(PATH):
+        for file in files:
+            os.system("scrollkeeper-uninstall -p /var/lib/scrollkeeper %s/%s" % (PATH, file))
