64348ded473e5ce495e2502a40b9e7e20477db62
İlker Kesen <ilkerksn@gmail.com>
2007-02-17 16:33:34 +0000

    let glade be glade3

--- programming/tools/glade3/actions.py
+++ programming/tools/glade3/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "glade3-%s" % get.srcVERSION()
-
 def setup():
     autotools.configure("--enable-python")
 
