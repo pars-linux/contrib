bc692561b974dd87c8f421fa70945cb9a438dd4f
Eren Türkay <turkay.eren@gmail.com>
2007-02-19 20:48:21 +0000

    add missing VERSION file used in docbook.xsl

--- desktop/gnome/docbook-xsl/actions.py
+++ desktop/gnome/docbook-xsl/actions.py
@@ -10,4 +10,6 @@ def install():
     items = ['template', 'fo', 'xhtml', 'profiling', 'website', 'images', 'html', 'params', 'htmlhelp', 'roundtrip', 'tools', 'highlighting', 'slides', 'docsrc', 'eclipse', 'manpages', 'javahelp', 'extensions', 'common', 'lib']
     for item in items:
         pisitools.insinto("/usr/share/sgml/docbook/xsl-stylesheets-1.72.0/%s" % item, "%s/*" % item)
+    pisitools.insinto("/usr/share/sgml/docbook/xsl-stylesheets-1.72.0", "VERSION")
+    
     pisitools.dodoc("AUTHORS", "BUGS", "COPYING", "NEWS", "README", "RELEASE-NOTES.txt", "TODO", "VERSION")
