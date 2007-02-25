e147dc5eeb6469676907cc5a6a3eb8944305802d
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-25 13:22:42 +0000

    create icon-theme.cache, previous command doesn't do anything

--- desktop/gnome/gthumb/comar/package.py
+++ desktop/gnome/gthumb/comar/package.py
@@ -3,7 +3,7 @@
 import os
 
 def postInstall():
-    os.system("gtk-update-icon-cache /usr/share/icons/hicolor")
+    os.system("gtk-update-icon-cache -f /usr/share/icons/hicolor")
     os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gthumb")
 
 def preRemove():
