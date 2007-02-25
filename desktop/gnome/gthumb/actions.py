ea2b33a7b2179bdec034f20a0f307b9928e66fd6
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-25 16:25:36 +0000

    make-up

--- desktop/gnome/gthumb/actions.py
+++ desktop/gnome/gthumb/actions.py
@@ -15,6 +15,7 @@ def build():
 
 def install():
     autotools.install()
+
     pisitools.dodoc("README", "TODO", "NEWS", "ChangeLog", "MAINTAINERS", "AUTHORS")
     # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
