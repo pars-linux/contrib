5bec3ec6f81939280223bfa86c72c08403bece6f
ilker <ilker@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-25 15:29:14 +0000

    #5102

---
+++ programming/languages/perl/Tk/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import pisitools
+
+def setup():
+    perlmodules.configure("X11INC=/usr/include/X11/ X11LIB=/usr/lib/X11/ ")
+
+def build():
+    perlmodules.make()
+
+def install():
+    perlmodules.install()
+    pisitools.dodoc("Change.log", "Changes", "COPYING", "INSTALL", "README", "README.linux", "VERSIONS")
+
