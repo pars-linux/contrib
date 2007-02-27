8fa7f7eeb3a89d0f46df23d65fc15f1a5ef14510
Murat Senel <msenel@gmail.com>
2007-02-27 20:17:19 +0000

    version bump

--- applications/multimedia/freevo/actions.py
+++ applications/multimedia/freevo/actions.py
@@ -7,8 +7,6 @@
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 
-WorkDir = "freevo-1.7.0-rc2"
-
 def install():
     pisitools.dodir("/etc/freevo")
     pisitools.dodir("/var/cache/freevo")
