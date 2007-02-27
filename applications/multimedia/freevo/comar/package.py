d6b8563fd397c72a4db9544aa9eb5014cbe739b3
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-02-27 20:17:19 +0000

    version bump

--- applications/multimedia/freevo/comar/package.py
+++ applications/multimedia/freevo/comar/package.py
@@ -3,6 +3,6 @@
 import os
 
 def postInstall():
-    os.system("chmod 777 /var/cache/freevo")
-    os.system("chmod 777 /var/log/freevo")
+    os.system("/usr/bin/chmod 1777 /var/cache/freevo")
+    os.system("/usr/bin/chmod 1777 /var/log/freevo")
     os.system("freevo setup")
