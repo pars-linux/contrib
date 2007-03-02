70d815edbcb8c8975729c66cf303be50663c6159
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-02 14:18:59 +0000

    fix

--- applications/crypto/trousers/comar/package.py
+++ applications/crypto/trousers/comar/package.py
@@ -4,7 +4,7 @@ import os
 
 def postInstall():
     os.system("/bin/chown tss:tss /etc/tcsd.conf")
-    os.system("/bin/chown tss:tss /sbin/tcsd")
+    os.system("/bin/chown tss:tss /usr/sbin/tcsd")
     os.system("/usr/bin/mkdir /var/lib/tpm")
     os.system("/usr/bin/chown tss:tss /var/lib/tpm")
     os.system("/bin/chmod 1777 /var/lib/tpm")
