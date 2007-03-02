a233ae9942b930d236584f1118d846be17ccb8db
Eren Türkay <turkay.eren@gmail.com>
2007-03-02 14:30:13 +0000

    take over

--- desktop/xfce4/goodies/thunar-media-tags-plugin/actions.py
+++ desktop/xfce4/goodies/thunar-media-tags-plugin/actions.py
@@ -4,7 +4,6 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
@@ -16,4 +15,5 @@ def build():
 
 def install():
     autotools.install()
+
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
