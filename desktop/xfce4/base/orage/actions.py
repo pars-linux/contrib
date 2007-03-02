e72976d15d62b01c44b742ef315f59a72f5bcef6
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-02 14:18:37 +0000

    take over

--- desktop/xfce4/base/orage/actions.py
+++ desktop/xfce4/base/orage/actions.py
@@ -15,6 +15,7 @@ def build():
 
 def install():
     autotools.install()
+    
     pisitools.dodoc("NEWS", "README", "ChangeLog", "AUTHORS")
     # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
