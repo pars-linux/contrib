cdf1f057b78fa14ac09a6d97be1578cd042570d0
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-03 21:06:33 +0000

    no need to do this, scrollkeeper-update automatically does it

--- applications/editors/gedit/comar/package.py
+++ applications/editors/gedit/comar/package.py
@@ -6,8 +6,4 @@ def postInstall():
     os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gedit")
 
 def preRemove():
-    PATH = "/usr/share/omf/gedit"
-
-    for root, dirs, files in os.walk(PATH):
-        for file in files:
-            os.system("scrollkeeper-uninstall -p /var/lib/scrollkeeper %s/%s" % (PATH, file))
+    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gedit")
