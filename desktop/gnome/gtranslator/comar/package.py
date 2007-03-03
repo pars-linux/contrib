b62f9bef20f5b2053e381288778adb931805f081
Eren Türkay <turkay.eren@gmail.com>
2007-03-03 21:09:44 +0000

    scrollkeeper-update automatically does it

--- desktop/gnome/gtranslator/comar/package.py
+++ desktop/gnome/gtranslator/comar/package.py
@@ -7,4 +7,5 @@ def postInstall():
     os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gtranslator")
 
 def preRemove():
-    os.system("scrollkeeper-uninstall -p /var/lib/scrollkeeper /usr/share/omf/gtranslator/gtranslator-C.omf")
+    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gtranslator")
+
