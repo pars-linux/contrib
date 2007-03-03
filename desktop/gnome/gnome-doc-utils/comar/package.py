855a9e9b8085d0ad605bebab88595bff07261e11
Eren Türkay <turkay.eren@gmail.com>
2007-03-03 21:09:06 +0000

    scrollkeeper-update automatically does it

--- desktop/gnome/gnome-doc-utils/comar/package.py
+++ desktop/gnome/gnome-doc-utils/comar/package.py
@@ -7,5 +7,5 @@ def postInstall():
     os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gnome-doc-xslt")
 
 def preRemove():
-    os.system("scrollkeeper-uninstall -p /var/lib/scrollkeeper /usr/share/omf/gnome-doc-make/gnome-doc-make-C.omf")
-    os.system("scrollkeeper-uninstall -p /var/lib/scrollkeeper /usr/share/omf/gnome-doc-xslt/gnome-doc-xslt-C.omf")
+    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gnome-doc-make")
+    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gnome-doc-xslt")
