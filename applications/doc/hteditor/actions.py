d97dd76b4002fc28068c234e6a612779193dd337
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-09 00:56:24 +0000

    clean

--- applications/doc/hteditor/actions.py
+++ applications/doc/hteditor/actions.py
@@ -4,10 +4,8 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 WorkDir="ht-2.0"
 
