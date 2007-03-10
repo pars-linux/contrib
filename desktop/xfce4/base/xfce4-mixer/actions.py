49a0044e15e84f2e7d932590c7dfcf1542efe926
Eren Türkay <turkay.eren@gmail.com>
2007-03-10 20:50:45 +0000

    snapshot

--- desktop/xfce4/base/xfce4-mixer/actions.py
+++ desktop/xfce4/base/xfce4-mixer/actions.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 #
-#Ertugrul Erata ertugrulerata at gmail.com
-#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+
+WorkDir="xfce4-mixer-4.4.0"
 
 def setup():
     autotools.configure()
