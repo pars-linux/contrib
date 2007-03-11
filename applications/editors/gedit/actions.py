fbbb038f5f05dcc84b0564d7b25d0e9f4a784eea
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-11 19:49:28 +0000

    version bump

--- applications/editors/gedit/actions.py
+++ applications/editors/gedit/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-scrollkeeper")
+    autotools.configure("--disable-scrollkeeper --disable-spell")
 
 def build():
     autotools.make()
