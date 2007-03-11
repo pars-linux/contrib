7fc1d3b8a4f45bfffe886bd6e2d0e4efc89ccd96
Eren Türkay <turkay.eren@gmail.com>
2007-03-11 17:14:24 +0000

    revert

--- desktop/xfce4/base/xfprint/actions.py
+++ desktop/xfce4/base/xfprint/actions.py
@@ -7,8 +7,6 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
-WorkDir="xfprint-4.4.0"
-
 def setup():
     autotools.configure("--enable-static=no \
                                   --enable-cups \
