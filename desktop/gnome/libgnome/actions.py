0fe7ab7c8d8f534dc3b60c6513fb901f7a252738
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-12 14:17:15 +0000

    bump

--- desktop/gnome/libgnome/actions.py
+++ desktop/gnome/libgnome/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-static=no --disable-schemas-install")
+    autotools.configure("--enable-static=no")
 
 def build():
     autotools.make()
