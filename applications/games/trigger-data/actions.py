4c526f870a55dcd67f7c1da40049a42a0a978c4d
onur <onur@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-13 20:10:51 +0000

    tidy up the package

--- applications/games/trigger-data/actions.py
+++ applications/games/trigger-data/actions.py
@@ -5,8 +5,21 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
-WorkDir = "trigger-0.5.2-data"
+import os
+
+WorkDir = "trigger-%s-data" % get.srcVERSION()
+
+def fixperms(d):
+    for root, dirs, files in os.walk(d):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
 
 def install():
+    fixperms("./")
     pisitools.insinto("/usr/share/trigger", "*")
+
