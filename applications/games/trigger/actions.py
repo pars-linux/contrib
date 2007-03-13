0860a109e75dc281f34fb57feed18268e2e10e85
Onur Küçük <zaburt@users.noreply.github.com>
2007-03-13 20:09:59 +0000

    fix summary/description and icon tag

--- applications/games/trigger/actions.py
+++ applications/games/trigger/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "trigger-0.5.2.1-src"
+WorkDir = "trigger-%s-src" % get.srcVERSION()
 
 def setup():
     autotools.configure("--disable-sdltest \
