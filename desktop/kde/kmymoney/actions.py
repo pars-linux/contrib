d97c4197501338b2130585c682197a170e7182f1
soytetir <soytetir@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-17 03:57:20 +0000

    paketin yerelleştimesi yaklaşık %50 seviyesine geldi, hesap şablonu dosyası eklendi, ufaktefek yamalar yapıldı vs... Bu uygulamayı kullanan varsa lütfen çeviriler konusunda gördüğü yamuklukları bildirsin, muhasebeci olacam yoksa :) Devam edecek...

--- desktop/kde/kmymoney/actions.py
+++ desktop/kde/kmymoney/actions.py
@@ -6,6 +6,7 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import kde
+from pisi.actionsapi import pisitools
 
 WorkDir = "kmymoney2-0.8.6"
 
@@ -16,4 +17,6 @@ def build():
     kde.make()
 
 def install():
+    pisitools.domo("po/tr.po","tr","kmymoney2.mo")
+    pisitools.domove("/usr/share/locale/*", "/usr/kde/3.5/share/locale/")
     kde.install()
