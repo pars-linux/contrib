b5c67d5d4ab3bde8f465efdea585e473a8826881
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-20 12:23:01 +0000

    vtk visiulation toolkit
    eylemlerimiz devam edecek

---
+++ programming/libs/vtk/actions.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import get
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+
+WorkDir="VTK"
+
+def setup():
+    shelltools.system("cmake -DCMAKE_INSTALL_PREFIX=%s/usr \
+                          -DBUILD_SHARED_LIBS=ON \
+                          -DVTK_USE_SYSTEM_JPEG=ON \
+                          -DVTK_USE_SYSTEM_PNG=ON \
+                          -DVTK_USE_SYSTEM_TIFF=ON \
+                          -DVTK_USE_SYSTEM_ZLIB=ON \
+                          -DVTK_USE_SYSTEM_EXPAT=ON \
+                          -DVTK_WRAP_PYTHON=ON \
+                          -DPYTHON_INCLUDE_PATH:PATH=/usr/include/python2.4 \
+                          -DVTK_USE_GUISUPPORT=ON \
+                          -DVTK_USE_QVTK=ON \
+                          -DQT_WRAP_CPP=ON \
+                          -DQT_WRAP_UI=ON \
+                          -DVTK_INSTALL_QT_DIR:PATH=/qt/3/plugins \
+                          -DDESIRED_QT_VERSION:STRING=3 \
+                          -DQT_MOC_EXECUTABLE=/usr/qt/3/bin/moc \
+                          -DQT_UIC_EXECUTABLE=/usr/qt/3/bin/uic \
+                          -DQT_INCLUDE_DIR:PATH=/usr/qt/3/include \
+                          -DQT_QMAKE_EXECUTABLE:PATH=/usr/qt/3/bin/qmake \
+                          -DDOCUMENTATION_HTML_HELP=ON \
+                          -DBUILD_DOCUMENTATION=ON \
+                          -DBUILD_EXAMPLES=ON \
+                          -DVTK_DATA_ROOT:PATH=/usr/share/VTK/data" % get.installDIR())
+
+
+def build():
+    cmaketools.make()
+
+
+def install():
+    cmaketools.install()
