ef801fcfde30881d1205a0fe77774078f0a5bfe4
ilker <ilker@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-22 15:41:17 +0000

    version bump and some configuration

--- programming/tools/gazpacho/actions.py
+++ programming/tools/gazpacho/actions.py
@@ -5,6 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def install():
     pythonmodules.install()
+    pisitools.rename("/usr/share/doc/gazpacho", get.srcTAG())
