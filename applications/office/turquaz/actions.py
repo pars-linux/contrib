9929afc7518f3fd5c718a8ceb22614c5b0f12457
soytetir <soytetir@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-23 11:54:01 +0000

    svn eski revisiondan çekip düzenlemeler yaptım, paketi üzerime aldım,felis sürümümüzde gayet güzel çalışıyor.

---
+++ applications/office/turquaz/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "TurquazLinux08Beta5"
+NoStrip = "/"
+
+def install():
+    shelltools.unlinkDir(get.workDIR() + "/TurquazLinux08Beta5/java")
+
+    pisitools.dodir("/usr/share/pixmaps")
+    shelltools.system('unzip -j %s/TurquazLinux08Beta5/turquaz/08Beta5/lib/turquaz-client.jar \
+                       icons/turquaz_paw.gif -d %s/usr/share/pixmaps'
+                       % (get.workDIR(), get.installDIR()) )
+    pisitools.insinto("/opt/TurquazLinux08Beta5/", "turquaz/08Beta5/*")
+    pisitools.remove("/opt/TurquazLinux08Beta5/database/turquaz.script")
+    pisitools.domove("/opt/TurquazLinux08Beta5/turquaz.sh", "/usr/bin")
+    pisitools.dodoc("install.txt", "yukleme.txt", "lisans/*")
