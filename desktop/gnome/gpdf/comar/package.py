c1c01f1314020faa958e557e256b48e66a614e3e
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-23 03:59:56 +0000

    isn't developed anymore

--- desktop/gnome/gpdf/comar/package.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall():
-    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gpdf")
-
-def preRemove():
-    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gpdf")
