1717f90a5e296d6f1942e5553b5bd137692511a6
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-24 20:13:38 +0000

    version bump, fix dependency and comar script

--- programming/tools/glade/comar/package.py
+++ programming/tools/glade/comar/package.py
@@ -4,3 +4,5 @@ import os
 
 def postInstall():
     os.system("gtk-update-icon-cache -f /usr/share/icons/hicolor")
+    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/glade")
+
