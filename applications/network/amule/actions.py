047910b4edcc425eb9d1fd326b9a5f954421a9d4
İlker Kesen <ilkerksn@gmail.com>
2007-03-25 15:53:18 +0000

    off be, revert

--- applications/network/amule/actions.py
+++ applications/network/amule/actions.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2006-2007 TUBITAK/UEKAE
+# Copyright © 2006 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
-# for stable versions
-# WorkDir = "aMule-%s" % get.srcVERSION()
-
-# for snapshots
-WorkDir = "amule-cvs"
+WorkDir = "aMule-%s" % get.srcVERSION()
 
 def setup():
     autotools.configure()
