fb718958e2a88965e975bc665515c549f6f08f24
ilker <ilker@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-25 15:51:53 +0000

    silindi, bu uygulama çok kirli, amule varken buna gerek yok. amule yeni sürüm çıkartınca hatalar giderilecek(giderilmesi lazım, adamlar hatayı kapatıyolar direk.).

--- applications/network/amule/actions.py
+++ applications/network/amule/actions.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2006 TUBITAK/UEKAE
+# Copyright © 2006-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
-WorkDir = "aMule-%s" % get.srcVERSION()
+# for stable versions
+# WorkDir = "aMule-%s" % get.srcVERSION()
+
+# for snapshots
+WorkDir = "amule-cvs"
 
 def setup():
     autotools.configure()
