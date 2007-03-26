9e1b4cea3c4a1b5008601f870066d9545fa98806
ftugrul <ftugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-03-26 22:44:05 +0000

    partimage eklendi

---
+++ applications/admin/partimage/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2005  TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+#
+# Fahri Tuğrul Gürkaynak <ftugrul@gmail.com>
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    shelltools.system("useradd partimag -g root")
+    
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.dodoc("ChangeLog", "AUTHORS", "INSTALL*", "NEWS", "README*")
