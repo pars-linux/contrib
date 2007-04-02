421e73070adf6e36a42d01255812bb6a70337677
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-02 08:51:55 +0000

    geoip version bump
    bu paketi üzerime alsam da gözden kaçmasa ?

--- programming/libs/geoip/actions.py
+++ programming/libs/geoip/actions.py
@@ -9,10 +9,10 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "GeoIP-1.4.1"
+WorkDir = "GeoIP-1.4.2"
 
 def setup():
-    autotools.configure("--enable-shared")
+    autotools.configure("--enable-shared --disable-static")
 
 def build():
     autotools.make()
