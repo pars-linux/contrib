0d24ebbe7d253a62a64d1d5ee55ae2fa46a59016
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-04 17:42:59 +0000

    MiniRacer is an OpenGL car racing game, based on the Quake engine

---
+++ applications/games/miniracer/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir="miniracer-1.04"
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.insinto("/usr/bin", "miniracer")
+    pisitools.insinto("/usr/share/MiniRacer", "engine.glx")
+    pisitools.insinto("/usr/share/MiniRacer/data", "data/pak0.pak")
+    pisitools.insinto("/usr/share/MiniRacer/data", "data/config.cfg")
+    pisitools.insinto("/usr/share/MiniRacer/data", "data/maps/*.bsp")
+    pisitools.insinto("/usr/share/MiniRacer/data", "data/maps/maps1.lst")
+    pisitools.insinto("/usr/share/man", "miniracer.6")
+
+    pisitools.dodoc("AUTHORS", "CREDITS", "COPYING", "INSTALL", "ChangeLog", "README")
