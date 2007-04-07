e4166f633e038524ec5876a6d33e5fe7980540c2
Uğur Çetin <ugur@ugurcetin.com.tr>
2007-04-07 05:48:13 +0000

    pdfedit: version bump

--- applications/editors/pdfedit/actions.py
+++ applications/editors/pdfedit/actions.py
@@ -4,17 +4,25 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from os import path, walk
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
+def fixperms(d):
+    for root, dirs, files in walk(d):
+        for name in dirs:
+            shelltools.chmod(path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(path.join(root, name), 0644)
+
 def setup():
-    shelltools.export("INSTALL_ROOT", get.installDIR())
     autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.install("INSTALL_ROOT=%s" % get.installDIR())
+    fixperms("%s/usr/share/pdfedit" % get.installDIR())
 
