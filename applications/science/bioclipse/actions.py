d5323ffcb8a0ffdc47f403c0f6f5bb1b87badf9f
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-08 18:13:22 +0000

    version bump

--- applications/science/bioclipse/actions.py
+++ applications/science/bioclipse/actions.py
@@ -7,11 +7,8 @@
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 
-WorkDir="."
-
 def install():
     pisitools.dodir("/opt/bioclipse")
-    shelltools.system("unzip bioclipse-1.0.linux.gtk.x86.zip")
     shelltools.cd("bioclipse")
     pisitools.insinto("/opt/bioclipse", "*")
 
