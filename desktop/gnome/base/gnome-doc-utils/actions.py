3b13c8d9febcb347c155fe91963f78b178b7cbcc
Eren Türkay <turkay.eren@gmail.com>
2007-04-09 18:37:35 +0000

    bump

--- desktop/gnome/base/gnome-doc-utils/actions.py
+++ desktop/gnome/base/gnome-doc-utils/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-scrollkeeper")
+    autotools.configure()
 
 def build():
     autotools.make()
