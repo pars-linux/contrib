cb2d07648c811280df848992bb87d41929e626da
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-10 21:36:55 +0000

    hata.. make up

--- applications/network/weechat/actions.py
+++ applications/network/weechat/actions.py
@@ -4,21 +4,21 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure("--enable-static=no \
                          --disable-qt \
-                         --disable-gtk")
+                         --disable-gtk \
+                         --disable-wxwidgets")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-    pisitools.dobin("src/gui/curses", "/usr/bin/weechat-curses")
+    pisitools.dobin("src/gui/curses/weechat-curses", "/usr/bin")
     pisitools.dosym("/usr/bin/weechat-curses", "/usr/bin/weechat")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README")
