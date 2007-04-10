85fee019467ff67de4ee551d520bc33be65ec727
Ertuğrul Erata <ertugrulerata@gmail.com>
2007-04-10 14:49:12 +0000

    rpy for R  Python interferace

---
+++ programming/languages/python/rpy/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+
+WorkDir="rpy-1.0-RC2"
+
+def install():
+    pythonmodules.install()
+    pisitools.remove("/usr/lib/python2.4/site-packages/rpy_wintools.py")
