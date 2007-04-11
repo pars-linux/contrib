fb2017f96f51ff2ec111e99c2e4da4c86bc4342f
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-11 18:04:31 +0000

    caior and gnome support, add scrollkeeper patch, change package name to 'dia'

--- applications/office/Dia/actions.py
+++ applications/office/Dia/actions.py
@@ -5,14 +5,18 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
-
-WorkDir = "dia-0.96.1"
+from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--with-python \
+                                  --with-gnomeprint \
+                                  --with-gnome \
+                                  --with-cairo)
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+    
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
