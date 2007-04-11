39ad31ce21533d73852406036b924742769bd80c
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-11 19:37:44 +0000

    fix summary and add Turkish translation

--- desktop/xfce4/base/Thunar/actions.py
+++ desktop/xfce4/base/Thunar/actions.py
@@ -16,6 +16,7 @@ def build():
 def install():
     autotools.install()
     
+    pisitools.domo("po/tr.po", "tr", "Thunar.mo")
     pisitools.dodoc("TODO", "THANKS", "README", "HACKING", "FAQ", "AUTHORS", "ChangeLog")
     # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
