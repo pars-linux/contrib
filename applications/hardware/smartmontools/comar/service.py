1abf93e29482008b3821c6876b4a241bc21f6f51
Ertuğrul Erata <ertugrulerata@gmail.com>
2007-04-13 17:25:33 +0000

    smartmontools

--- applications/hardware/smartmontools/comar/service.py
+++ applications/hardware/smartmontools/comar/service.py
@@ -6,7 +6,7 @@ serviceType="server"
 serviceDesc = _({"en": "S.M.A.R.T. monitoring daemon",
                  "tr": "S.M.A.R.T. monitoring daemon"})
 
-serviceConf = "smartd"
+serviceConf = "smartd.conf"
 
 def unlink():
     try:
@@ -16,12 +16,7 @@ def unlink():
 
 def start():
 
-    config = loadConfig()
-    opts = ""
-    if "SMARTD_OPTS" in config:
-        opts = "-- %s" % config["SMARTD_OPTS"]
-
-    ret = run("/sbin/start-stop-daemon --start --exec /usr/sbin/smartd --pidfile /var/run/smartd.pid  -- -p /var/run/smartd.pid %s" % opts)
+    ret = run("/sbin/start-stop-daemon --start --exec /usr/sbin/smartd --pidfile /var/run/smartd.pid  -- -p /var/run/smartd.pid")
 
     if ret == 0:
         notify("System.Service.changed", "started")
