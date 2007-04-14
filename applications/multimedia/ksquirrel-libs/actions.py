7d1a3f0658715beebdabf7eee31fb551a46af40d
alsaberk <alsaberk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-14 17:27:09 +0000

    Depend eksikleri giderildi

---
+++ applications/multimedia/ksquirrel-libs/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+WorkDir="ksquirrel-libs-0.7.0-pre2"
+
+def setup():
+    autotools.configure("--disable-camera \
+                         --disable-djvu \
+                         --disable-jpeg2000 \
+                         --disable-mng \
+                         --disable-svg \
+                         --disable-ttf \
+                         --disable-wmf \
+                         --enable-final")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README")
