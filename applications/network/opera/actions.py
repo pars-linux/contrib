d09843095335d51eb4a69b40c9e34eb2ce7d1a56
İsmail Dönmez <ismail@users.noreply.github.com>
2007-04-15 21:11:21 +0000

    re-add Opera now that its security bugs fixed

---
+++ applications/network/opera/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2006,2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir="opera-9.20-20070409.6-shared-qt.i386-en-638"
+
+def install():
+    shelltools.system("./install.sh DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto("/etc","config/opera6rc")
+    pisitools.insinto("/usr/share/pixmaps","images/*.png")
