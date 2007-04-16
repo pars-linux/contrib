bf4c561a3f60bc8e8271964cd8aa113915f730a8
Fatih Aşıcı <fatih-asici@users.noreply.github.com>
2007-04-16 05:45:52 +0000

    bump

--- applications/science/aqsis/actions.py
+++ applications/science/aqsis/actions.py
@@ -10,8 +10,6 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
 
-WorkDir = "dist_aqsis-1.2.0rc2"
-
 def install():
     shelltools.export("LD_LIBRARY_PATH", "%s/%s/lib" % (get.installDIR(), get.defaultprefixDIR()))
     scons.install("install_prefix=/%s sysconfdir=/%s/aqsis install" % (get.defaultprefixDIR(), get.confDIR()), get.installDIR(), "destdir")
