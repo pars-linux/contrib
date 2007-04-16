4642fd6ebff80639a17dbb414a9b73b08d811027
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-16 14:03:05 +0000

    bump and disable static libs

--- desktop/gnome/base/libxklavier/actions.py
+++ desktop/gnome/base/libxklavier/actions.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
-
 def setup():
-    autotools.configure("--enable-xkb-support --enable-xmm-support --enable-doc --enable-doxygen")
+    autotools.configure("--enable-xkb-support --enable-static=no")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+    
+    pisitools.dodoc("NEWS", "README", "CREDITS", "AUTHORS", "ChangeLog")
