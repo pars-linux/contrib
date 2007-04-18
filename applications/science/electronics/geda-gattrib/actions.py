6fc99945cb52ef7e8cbeb87e0e2d2f5e7c12de25
ayan <ayan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-18 21:22:06 +0000

    biraz daha düzenle

--- applications/science/electronics/geda-gattrib/actions.py
+++ applications/science/electronics/geda-gattrib/actions.py
@@ -5,6 +5,7 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir = "geda-gattrib-%s" % get.srcVERSION()
@@ -17,3 +18,4 @@ def build():
 
 def install():
     autotools.install()
+    pisitools.dodoc("AUTHORS","ChangeLog","INSTALL","NEWS","README")
