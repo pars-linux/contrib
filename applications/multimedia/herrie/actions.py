522c4cec19c7cce786892ed3162ad59acdac8c39
Eren Türkay <turkay.eren@gmail.com>
2007-04-21 18:01:17 +0000

    bump

--- applications/multimedia/herrie/actions.py
+++ applications/multimedia/herrie/actions.py
@@ -17,7 +17,7 @@ def install():
     pisitools.dobin("herrie")
     pisitools.doman("herrie.1")
     
-    locales = ['nl','tr','de']
+    locales = ['nl','tr','de','pl']
     for locale in locales:
         pisitools.insinto("/usr/share/locale/%s/LC_MESSAGES" % locale, "%s.mo" % locale, "herrie.mo")
 
