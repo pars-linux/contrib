2785641aa654750834258f8bee85f1ab518971b0
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-22 17:48:00 +0000

    Don't install INSTALL file

--- programming/libs/rasqal/actions.py
+++ programming/libs/rasqal/actions.py
@@ -24,7 +24,7 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog*", "COPYING*", "INSTALL", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog*", "COPYING*", "NEWS", "README")
     pisitools.insinto("%s/html" % docdir, "%s/%s/rasqal/*" % (get.installDIR(), docdir))
     pisitools.removeDir("%s/rasqal" % docdir)
     pisitools.dohtml("*.html")
