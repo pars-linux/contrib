67cb92c4ed00ad3744bce3c3bb4b25c433e58e11
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-22 16:37:56 +0000

    Don't install INSTALL file

--- applications/games/kskippingstones/actions.py
+++ applications/games/kskippingstones/actions.py
@@ -18,4 +18,4 @@ def build():
 
 def install():
     autotools.install()
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
