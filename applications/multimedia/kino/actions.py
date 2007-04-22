b94df6ee6d9d10d7eaf7b6405f14b285ce142b7a
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-22 17:05:33 +0000

    Don't install INSTALL file

--- applications/multimedia/kino/actions.py
+++ applications/multimedia/kino/actions.py
@@ -20,4 +20,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("README", "ABOUT-NLS", "BUGS", "AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "TODO", "README_jogshuttle")
+    pisitools.dodoc("README", "ABOUT-NLS", "BUGS", "AUTHORS", "ChangeLog", "COPYING", "NEWS", "TODO", "README_jogshuttle")
