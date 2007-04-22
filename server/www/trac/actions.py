fac7fdb9cee6dff88e8e0c7412aec28fa6829039
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-22 17:53:23 +0000

    Don't install INSTALL file

--- server/www/trac/actions.py
+++ server/www/trac/actions.py
@@ -10,4 +10,4 @@ from pisi.actionsapi import pisitools
 def install():
     pythonmodules.install()
     pisitools.insinto("/usr/share/trac/contrib", "contrib/*")
-    pisitools.dodoc("AUTHORS", "ChangeLog", "INSTALL", "NEWS", "README", "contrib/README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "contrib/README")
