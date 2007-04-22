3a96fa6d0c629858846031ae45b0d62e8fa4117f
ilker <ilker@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-22 13:01:02 +0000

    micq: version bump, fix dependency

--- applications/network/micq/actions.py
+++ applications/network/micq/actions.py
@@ -17,4 +17,4 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "FAQ", "INSTALL", "NEWS", "README", "TODO")
