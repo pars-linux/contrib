39f570be1223e8e9b91ef4725b4596fafe4c5c86
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-22 17:40:34 +0000

    Don't install INSTALL file

--- programming/libs/geoip/actions.py
+++ programming/libs/geoip/actions.py
@@ -20,4 +20,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README", "TODO")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
