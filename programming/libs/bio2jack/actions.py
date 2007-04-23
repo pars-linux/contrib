f5df08522a836f86128c59d27ad0a05583339214
cartman <cartman@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-23 15:55:51 +0000

    small fixes

--- programming/libs/bio2jack/actions.py
+++ programming/libs/bio2jack/actions.py
@@ -4,27 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
-from pisi.actionsapi import libtools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir="bio2jack"
 
 def setup():
-    shelltools.export("WANT_AUTOMAKE", "1.8")
-    shelltools.export("WANT_AUTOCONF", "2.5")
-    autotools.aclocal()
-    autotools.automake()
-    autotools.autoconf()
-    libtools.libtoolize("--copy --force")
-    autotools.configure()
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("ChangeLog", "AUTHORS", "NEWS", "README*")
 
+    pisitools.dodoc("ChangeLog", "AUTHORS", "NEWS", "README*")
