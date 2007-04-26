6e0ef4eac1e71aec09d49edf0ef6ece7f3efddc2
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-26 07:40:37 +0000

    PyQwt pyqwt5 ı çıkartıp stable release e geçiyoruz aynı zamanda qt ve qt4 desteği var pyc ler de temizlendi

---
+++ programming/libs/PyQwt/actions.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+
+
+WorkDir="PyQwt-5.0.0"
+
+def setup():
+    shelltools.copytree("../PyQwt-5.0.0","../PyQwt-5.0.0-qt4")
+    shelltools.cd("configure")
+    shelltools.system("python configure.py -3 -I /usr/qt/3/include/qwt/")
+
+    shelltools.cd("../../PyQwt-5.0.0-qt4/configure")
+    shelltools.system("python configure.py -I /usr/qt/4/include/qwt/")
+
+def build():
+    shelltools.cd("configure")
+    autotools.make()
+
+    shelltools.cd("../../PyQwt-5.0.0-qt4/configure")
+    autotools.make()
+
+
+
+def install():
+    shelltools.cd("configure")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pythonmodules.fixCompiledPy()
+
+    shelltools.cd("../../PyQwt-5.0.0-qt4/configure")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pythonmodules.fixCompiledPy()
