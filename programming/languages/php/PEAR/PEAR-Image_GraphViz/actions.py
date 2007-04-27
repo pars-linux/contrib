a887418f4f3ae950bb86479cf6925ccd1481fb91
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-27 15:18:10 +0000

    add PEAR-Image*

---
+++ programming/languages/php/PEAR/PEAR-Image_GraphViz/actions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+
+WorkDir="Image_GraphViz-1.2.1"
+
+def install():
+    pisitools.insinto("/usr/share/php5/PEAR/Image", "GraphViz.php")
