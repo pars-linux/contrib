2645e5fc0d8066cc92931cd4ba0dc51eddcb46ed
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-29 16:14:55 +0000

    new game

---
+++ applications/games/secondlife/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import scons
+
+WorkDir="linden"
+
+def build():
+    shelltools.cd("indra")
+    shelltools.export("TEMP_BUILD_DIR","build")
+
+    scons.make("DISTCC=no BTARGET=client BUILD=release MOZLIB=no GRID=firstlook")
+
+def install():
+    for data in ["secondlife-i686-bin-globalsyms","featuretable.txt","gpu_table.txt","app_settings","skins","fonts","character","help","res-sdl"]:
+        pisitools.insinto("/usr/share/secondlife","indra/newview/%s" % data)
+
+    pisitools.insinto("/usr/share/secondlife/app_settings","scripts/messages/message_template.msg")
+    pisitools.insinto("/usr/share/secondlife","indra/newview/res/ll_icon.ico","secondlife.ico")
+    pisitools.insinto("/usr/share/secondlife","indra/newview/linux_tools/launch_url.sh")
+
+    pisitools.dosym("/usr/share/fonts/dejavu/DejaVuSerif.ttf","/usr/share/secondlife/unicode.ttf")
+
+    pisitools.dohtml("indra/newview/lsl_guide.html")
+    pisitools.dodoc("indra/newview/releasenotes.txt","indra/newview/licenses-linux.txt")
