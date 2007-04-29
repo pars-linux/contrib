83f642873471ca10281d9025536682e78e75dbb8
Murat Senel <msenel@gmail.com>
2007-04-29 15:22:10 +0000

    disable static libs

--- desktop/enlightenment/base/edje/actions.py
+++ desktop/enlightenment/base/edje/actions.py
@@ -9,7 +9,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--enable-edje-cc \
+    autotools.configure("--disable-static \
+                         --enable-edje-cc \
                          --with-eet \
                          --with-eet-exec \
                          --with-evas \
