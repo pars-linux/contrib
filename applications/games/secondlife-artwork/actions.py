7fa539f96dedaa5fdcf3e99b8a5f92967329ebda
Murat Senel <msenel@gmail.com>
2007-04-29 16:15:09 +0000

    sl artwork

---
+++ applications/games/secondlife-artwork/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pisitools
+
+WorkDir = "linden/indra"
+
+def install():
+    pisitools.insinto("/usr/share/secondlife/","newview/*")
+
