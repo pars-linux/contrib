5034b2a351efee26c2863899a7c1992ae7279f64
S.Çağlar Onur <caglar10ur@users.noreply.github.com>
2007-04-29 00:25:57 +0000

    i don't have enough time to i18nize this :)

---
+++ programming/tools/gquilt/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pythonmodules.fixCompiledPy("/usr/share/gquilt/")
