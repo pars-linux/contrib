9d871ced7558e5e4cddac4fe5f5ac86318070337
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-29 16:03:09 +0000

    new game

---
+++ applications/games/risk/actions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "Risk"
+datadir = "/usr/share/risk"
+
+def fixperms(d):
+    import os
+
+    for root, dirs, files in os.walk(d):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
+
+def build():
+    shelltools.system("ant")
+
+def install():
+    shelltools.cd("build/game")
+
+    pisitools.dodir(datadir)
+
+    pisitools.insinto(datadir, "src.zip")
+    pisitools.insinto(datadir, "Risk.jar")
+
+    for f in ["help", "maps", "resources" ,"saves"]:
+        fixperms(f)
+        shelltools.copytree(f, "%s/%s" % (get.installDIR(), datadir))
+
+    pisitools.dodoc("*.txt")
+    pisitools.dohtml("*.htm")
