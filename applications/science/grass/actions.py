f68de15b4ace0af3766e2173cfe940c55db09a12
Murat Senel <msenel@gmail.com>
2007-04-30 19:25:56 +0000

    disable static libs

--- applications/science/grass/actions.py
+++ applications/science/grass/actions.py
@@ -14,6 +14,7 @@ def setup():
 
     autotools.configure("--prefix=%s/opt \
                          --disable-static \
+                         --enable-shared \
                          --with-x \
                          --with-cxx \
                          --with-jpeg \
