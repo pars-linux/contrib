3c0cf534e8ca1ff5ad3bf9be3b736c9aa1b3b94d
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-04-30 18:02:08 +0000

    fixCompiledPy

--- programming/libs/geos/actions.py
+++ programming/libs/geos/actions.py
@@ -8,13 +8,16 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import libtools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
 
 WorkDir = "geos-3.0.0rc4"
 
 def setup():
     shelltools.export("WANT_AUTOCONF", "2.5")
+
     libtools.libtoolize("--copy --force")
+
     autotools.configure("--with-pic \
                          --disable-static \
                          --enable-inline \
@@ -28,4 +31,7 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pythonmodules.fixCompiledPy()
+
     pisitools.dodoc("AUTOHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
