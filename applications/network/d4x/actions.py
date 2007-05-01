418535b912e01e85cc8d7b0faafa7cf856caee32
Murat Senel <msenel@gmail.com>
2007-05-01 19:51:28 +0000

    fix freezing problem

--- applications/network/d4x/actions.py
+++ applications/network/d4x/actions.py
@@ -11,14 +11,12 @@ WorkDir="d4x-2.5.7.1"
 
 def setup():
     autotools.configure("--enable-release \
-                         --enable-esd \
                          --enable-alsa \
                          --enable-libao \
                          --enable-openssl \
                          --enable-nls \
                          --disable-rpath \
                          --disable-aotest \
-                         --disable-esdtest \
                          --disable-glibtest \
                          --disable-gtktest \
                          --with-gnu-ld \
@@ -31,11 +29,13 @@ def build():
 
 def install():
     autotools.install()
+
     pisitools.insinto("/usr/share/applications", "share/nt.desktop")
     pisitools.insinto("/usr/share/pixmaps", "share/*.png")
     pisitools.insinto("/usr/share/pixmaps", "share/*.xpm")
+
     pisitools.domo("po/tr.po","tr","d4x.mo")
     pisitools.remove("/usr/share/locale/locale.alias")
+
     pisitools.doman("DOC/nt.1")
     pisitools.dodoc("ABOUT-NLS", "AUTHORS", "ChangeLog*", "COPYING", "NEWS", "PLANS", "README", "TODO", "DOC/FAQ", "DOC/LICENSE", "DOC/THANKS", "DOC/TROUBLES")
-
