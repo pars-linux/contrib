387ab1e75058fccc6cab472375de4ff84c810682
Eren Türkay <turkay.eren@gmail.com>
2007-05-03 19:24:06 +0000

    bump

--- programming/languages/php/PEAR/PEAR-MDB2_Driver_sqlite/actions.py
+++ programming/languages/php/PEAR/PEAR-MDB2_Driver_sqlite/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="MDB2_Driver_sqlite-1.4.0"
+WorkDir="MDB2_Driver_sqlite-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/MDB2/Driver", "MDB2/Driver/*")
