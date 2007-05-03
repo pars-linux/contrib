42a2728d4271aff42822d04e7e9b6b59c9782cdd
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-03 19:17:55 +0000

    bump

--- programming/languages/php/PEAR/PEAR-MDB2_Driver_mysqli/actions.py
+++ programming/languages/php/PEAR/PEAR-MDB2_Driver_mysqli/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="MDB2_Driver_mysqli-1.4.0"
+WorkDir="MDB2_Driver_mysqli-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/MDB2/Driver", "MDB2/Driver/*")
