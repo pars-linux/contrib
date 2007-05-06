78e71b21b5ec59a2f9271eb862484ec4c634f906
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-06 21:37:46 +0000

    version bump

--- applications/multimedia/uade/actions.py
+++ applications/multimedia/uade/actions.py
@@ -4,7 +4,6 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
@@ -19,4 +18,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
