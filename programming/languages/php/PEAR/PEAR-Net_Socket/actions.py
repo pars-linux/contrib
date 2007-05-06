507ddb60b167ae8bf6d188d9dfcc11bcccb7b44d
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-06 15:17:13 +0000

    bump

--- programming/languages/php/PEAR/PEAR-Net_Socket/actions.py
+++ programming/languages/php/PEAR/PEAR-Net_Socket/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Net_Socket-1.0.7"
+WorkDir="Net_Socket-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Net","Socket.php")
