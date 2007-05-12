9b4b12992271d0e9639d263b09b7a6519d207acf
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-12 11:48:16 +0000

    add pycurl

---
+++ programming/languages/python/pycurl/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def install():
+    pythonmodules.install()
+    pisitools.remove("/usr/share/doc/pycurl/INSTALL")
+    pisitools.domove("/usr/share/doc/pycurl/", "/usr/share/doc/%s/" % get.srcTAG())
