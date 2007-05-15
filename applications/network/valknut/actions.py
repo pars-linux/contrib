a0becc2ef990c1b367f95e3fe8236bede0ce2d4e
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-15 16:09:09 +0000

    Version bump

--- applications/network/valknut/actions.py
+++ applications/network/valknut/actions.py
@@ -4,10 +4,8 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--disable-xmltest \
@@ -23,4 +21,4 @@ def install():
     pisitools.insinto("/usr/share/pixmaps", "valknut/icons/icon_22x22.png")
     pisitools.rename("/usr/share/pixmaps/icon_22x22.png" , "valknut.png")
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "INSTALL", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
