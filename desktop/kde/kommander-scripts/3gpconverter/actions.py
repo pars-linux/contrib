e0ed897700c1e8c8ece68515fb5871a032e1e505
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-15 11:07:08 +0000

    Version bump

--- desktop/kde/kommander-scripts/3gpconverter/actions.py
+++ desktop/kde/kommander-scripts/3gpconverter/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="3gpconverter-0.4"
+WorkDir="3gpconverter-%s" % get.srcVERSION()
 
 def install():
-    pisitools.insinto("/usr/kde/3.5/bin", "3gpconverter-0.4.kmdr","3gpconverter.kmdr")
+    pisitools.insinto("/usr/kde/3.5/bin", "3gpconverter-0.6.kmdr","3gpconverter.kmdr")
