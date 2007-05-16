9daab3d3f9f690cbee4d3838312fdc72e08fbc87
Uğur Çetin <ugur@ugurcetin.com.tr>
2007-05-16 20:25:26 +0000

    kbfx: version bump

--- desktop/kde/kbfx/actions.py
+++ desktop/kde/kbfx/actions.py
@@ -4,16 +4,15 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import kde
-
-WorkDir = "kbfx-0.4.9.2rc4"
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
 
 def setup():
-    kde.configure()
+    cmaketools.configure(installPrefix = "%s/%s" % (get.installDIR(), get.kdeDIR()))
 
 def build():
-    kde.make()
+    cmaketools.make()
 
 def install():
-    kde.install()
+    cmaketools.install()
 
