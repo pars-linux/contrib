56d9983c8f7f971f2f87d1925d3386c5c1ba5b20
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:02:54 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-HTML_QuickForm/actions.py
+++ programming/languages/php/PEAR/PEAR-HTML_QuickForm/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="HTML_QuickForm-3.2.7"
+WorkDir="HTML_QuickForm-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTML", "QuickForm*")
