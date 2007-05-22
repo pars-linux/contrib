5e25ceede68212726e438012fd7d90c60eab0b4d
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:02:45 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-HTML_BBCodeParser/actions.py
+++ programming/languages/php/PEAR/PEAR-HTML_BBCodeParser/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="HTML_BBCodeParser-1.1"
+WorkDir="HTML_BBCodeParser-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTML", "*")
