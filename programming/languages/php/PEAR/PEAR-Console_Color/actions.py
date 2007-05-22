33898a9f2e5e1c400cfb3602545467bcee6f266d
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:03:33 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Console_Color/actions.py
+++ programming/languages/php/PEAR/PEAR-Console_Color/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Console_Color-1.0.2"
+WorkDir="Console_Color-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Console", "Color.php")
