336869826cab7f487462502bd4c0b75b2be32bfd
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:04:38 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Net_URL/actions.py
+++ programming/languages/php/PEAR/PEAR-Net_URL/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Net_URL-1.0.14"
+WorkDir="Net_URL-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Net", "URL.php")
