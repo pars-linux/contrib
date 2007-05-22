900747097c9fdc8f6de0dd5c5af34e08199c328c
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:05:50 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-HTTP_Session/actions.py
+++ programming/languages/php/PEAR/PEAR-HTTP_Session/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="HTTP_Session-0.5.5"
+WorkDir="HTTP_Session-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTTP", "Session*")
