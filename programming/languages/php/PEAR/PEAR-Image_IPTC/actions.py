186e44b05554d965af85acb00b02f827d6acae19
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:03:36 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Image_IPTC/actions.py
+++ programming/languages/php/PEAR/PEAR-Image_IPTC/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Image_IPTC-1.0.2"
+WorkDir="Image_IPTC-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Image", "IPTC.php")
