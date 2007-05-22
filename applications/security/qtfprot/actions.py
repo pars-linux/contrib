bd50582cd62ab4cccf368dacecd241d3f4feb6fb
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 19:41:37 +0000

    * remove unused modules

--- applications/security/qtfprot/actions.py
+++ applications/security/qtfprot/actions.py
@@ -5,8 +5,6 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 WorkDir="qtfprot-0.2.1c" 
 
