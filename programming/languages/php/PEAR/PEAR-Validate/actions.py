f8bd4183d606d580e66f105e59962df739221db9
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:03:21 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Validate/actions.py
+++ programming/languages/php/PEAR/PEAR-Validate/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Validate-0.7.0"
+WorkDir="Validate-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR", "Validate.php")
