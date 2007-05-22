431115a6df2f0ba1e545e4e7fd2cc29608e850e0
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:03:39 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Services_Weather/actions.py
+++ programming/languages/php/PEAR/PEAR-Services_Weather/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Services_Weather-1.4.0"
+WorkDir="Services_Weather-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Services", "Weather*")
