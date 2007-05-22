82c8db567e177477e558f915bd314243214cf45f
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:05:56 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-DB/actions.py
+++ programming/languages/php/PEAR/PEAR-DB/actions.py
@@ -5,11 +5,13 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="DB-1.7.11"
+WorkDir="DB-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR","DB.php")
     pisitools.insinto("/usr/share/php5/PEAR/DB","DB/*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/DB","doc/*")
     pisitools.insinto("/usr/share/php5/PEAR/tests/DB","tests/*")
+
