ab7b8fce6b2dd802bebafa29b5d20d1759cf5dad
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:03:03 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Archive_Tar/actions.py
+++ programming/languages/php/PEAR/PEAR-Archive_Tar/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Archive_Tar-1.3.2"
+WorkDir="Archive_Tar-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Archive", "Archive/Tar.php")
