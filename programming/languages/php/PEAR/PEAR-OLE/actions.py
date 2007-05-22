b9d64739612d5edadc94cc46d7610e7def07ff9f
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:04:05 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-OLE/actions.py
+++ programming/languages/php/PEAR/PEAR-OLE/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="OLE-0.5"
+WorkDir="OLE-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR", "OLE.php")
