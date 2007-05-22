b831d3ed06b04c94d49c747c359e8044111f2224
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:05:59 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-XML_RSS/actions.py
+++ programming/languages/php/PEAR/PEAR-XML_RSS/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="XML_RSS-0.9.10"
+WorkDir="XML_RSS-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/XML", "RSS.php")
