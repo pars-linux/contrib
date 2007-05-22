fb9c9a3097c1d0879ad5367668f957a8b529c3b7
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:04:29 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Net_Ping/actions.py
+++ programming/languages/php/PEAR/PEAR-Net_Ping/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Net_Ping-2.4.1"
+WorkDir="Net_Ping-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Net", "Ping.php")
