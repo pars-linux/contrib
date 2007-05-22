89864139884d107ff48c3501e0f0897375219e7f
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:05:23 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Image_Canvas/actions.py
+++ programming/languages/php/PEAR/PEAR-Image_Canvas/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Image_Canvas-0.3.0"
+WorkDir="Image_Canvas-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Image", "Canvas*")
