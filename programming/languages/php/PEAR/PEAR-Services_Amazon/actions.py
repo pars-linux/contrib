4069bd09aa73f6931fceca4e2597f11eb9ee919d
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:03:09 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Services_Amazon/actions.py
+++ programming/languages/php/PEAR/PEAR-Services_Amazon/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Services_Amazon-0.7.0"
+WorkDir="Services_Amazon-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Services", "Amazon*")
