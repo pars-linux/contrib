4cd408dab217a570bb7240671e46c6b133d52e09
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:03:42 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-MDB/actions.py
+++ programming/languages/php/PEAR/PEAR-MDB/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="MDB-1.3.0"
+WorkDir="MDB-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR", "MDB.php")
