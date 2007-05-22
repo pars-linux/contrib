c804897d1c2b69af8df4ace14e921fe7366962b1
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:03:48 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-HTTP_Download/actions.py
+++ programming/languages/php/PEAR/PEAR-HTTP_Download/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="HTTP_Download-1.1.2"
+WorkDir="HTTP_Download-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTTP", "Download*")
