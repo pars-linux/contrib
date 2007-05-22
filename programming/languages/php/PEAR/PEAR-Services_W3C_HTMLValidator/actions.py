2d0c6f5b3cb8894c8837496c4141a8eb99a1ed05
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:03:18 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Services_W3C_HTMLValidator/actions.py
+++ programming/languages/php/PEAR/PEAR-Services_W3C_HTMLValidator/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Services_W3C_HTMLValidator-0.2.0"
+WorkDir="Services_W3C_HTMLValidator-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Services/W3C", "HTMLValidator*")
