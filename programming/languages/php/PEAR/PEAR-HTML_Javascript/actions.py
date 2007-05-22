41bfbe4a2d7e9a1514cc9bab0b243e0170b83a54
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:03:24 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-HTML_Javascript/actions.py
+++ programming/languages/php/PEAR/PEAR-HTML_Javascript/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="HTML_Javascript-1.1.1"
+WorkDir="HTML_Javascript-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTML", "Javascript*")
