6b7feb2432c646a704be5d4e5e4c733d65ec8036
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:05:02 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Net_UserAgent_Detect/actions.py
+++ programming/languages/php/PEAR/PEAR-Net_UserAgent_Detect/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Net_UserAgent_Detect-2.2.0"
+WorkDir="Net_UserAgent_Detect-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Net/UserAgent", "Detect.php")
