af57438cebc189e3d3a0c653775e0fda99342c36
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:04:35 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Numbers_Roman/actions.py
+++ programming/languages/php/PEAR/PEAR-Numbers_Roman/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Numbers_Roman-1.0.1"
+WorkDir="Numbers_Roman-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Numbers", "Roman.php")
