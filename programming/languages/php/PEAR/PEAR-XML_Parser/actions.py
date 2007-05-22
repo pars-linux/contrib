1d02befa875cc02eb227bf8d46cc3492907bdbef
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:02:39 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-XML_Parser/actions.py
+++ programming/languages/php/PEAR/PEAR-XML_Parser/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="XML_Parser-1.2.7"
+WorkDir="XML_Parser-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/XML","Parser.php")
