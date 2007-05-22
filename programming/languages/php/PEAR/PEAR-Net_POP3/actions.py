9432b4c634f19087ee8caeabdec95673261e9288
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:02:57 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Net_POP3/actions.py
+++ programming/languages/php/PEAR/PEAR-Net_POP3/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Net_POP3-1.3.6"
+WorkDir="Net_POP3-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Net", "POP3.php")
