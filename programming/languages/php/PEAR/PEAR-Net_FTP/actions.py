0317f0e9106034b3343217a621ee454986482058
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:04:23 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Net_FTP/actions.py
+++ programming/languages/php/PEAR/PEAR-Net_FTP/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Net_FTP-1.3.2"
+WorkDir="Net_FTP-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Net", "Net/*")
