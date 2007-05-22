17fda024acd20de1396f2bfacba9bbd92a8c9bad
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:04:26 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-HTML_Table_Matrix/actions.py
+++ programming/languages/php/PEAR/PEAR-HTML_Table_Matrix/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="HTML_Table_Matrix-1.0.9"
+WorkDir="HTML_Table_Matrix-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTML/Table", "Matrix*")
