c728bd6f9943af02b9b6a3d9a0fc525d99f1c23a
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:02:29 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-DB_DataObject/actions.py
+++ programming/languages/php/PEAR/PEAR-DB_DataObject/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="DB_DataObject-1.8.5"
+WorkDir="DB_DataObject-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/DB", "DataObject*")
