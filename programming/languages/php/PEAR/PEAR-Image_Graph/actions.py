d5414e6d07b5bf84df9f0838a522f8e11af7c962
Eren Türkay <turkay.eren@gmail.com>
2007-05-22 22:05:20 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Image_Graph/actions.py
+++ programming/languages/php/PEAR/PEAR-Image_Graph/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Image_Graph-0.7.2"
+WorkDir="Image_Graph-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Image", "Graph*")
