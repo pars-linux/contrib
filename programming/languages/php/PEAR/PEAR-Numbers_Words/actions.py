5ddcb908c0b06aa5cba1e98bfe84af625d8162f1
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-22 22:06:05 +0000

    use get.srcVERSION()

--- programming/languages/php/PEAR/PEAR-Numbers_Words/actions.py
+++ programming/languages/php/PEAR/PEAR-Numbers_Words/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Numbers_Words-0.15.0"
+WorkDir="Numbers_Words-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Numbers", "Words*")
