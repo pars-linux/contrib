f4e10df62487d9357b998c5f1a78306e9b6fe819
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-24 08:58:17 +0000

    internal tsocks disabled
    tsocks ilgililerini sisteme yazıyor makefile hatası var

--- applications/network/tork/actions.py
+++ applications/network/tork/actions.py
@@ -8,14 +8,20 @@
 from pisi.actionsapi import kde
 from pisi.actionsapi import pisitools
 
+
 def setup():
-    kde.configure("--with-external-geoip")
+    kde.configure("--with-external-geoip \
+                   --with-external-tsocks")
 
 def build():
     kde.make()
 
 def install():
     kde.install()
+
+    # add.tr language 
     pisitools.domo("po/tr.po","tr","tork.mo")
     pisitools.domove("/usr/share/locale/tr/LC_MESSAGES/tork.mo","/usr/kde/3.5/share/locale/tr/LC_MESSAGES")
+
+    # remove files conflicts
     pisitools.removeDir("/usr/kde/3.5/share/icons/crystalsvg")
