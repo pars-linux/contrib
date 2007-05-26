06fa134d0523c248f6c4c3406edcab7622f54d97
Eren Türkay <turkay.eren@gmail.com>
2007-05-26 11:28:09 +0000

    * dependency cleaning * libxfcegui4 -> gtk2 -> cairo -> pango

--- desktop/xfce4/base/orage/actions.py
+++ desktop/xfce4/base/orage/actions.py
@@ -15,7 +15,7 @@ def build():
 
 def install():
     autotools.install()
-    
+
     pisitools.dodoc("NEWS", "README", "ChangeLog", "AUTHORS")
     # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
