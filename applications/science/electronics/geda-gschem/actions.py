50863332bfe04538cef16aeb57669a31b97ccc51
Ayan Öztürk <ayanozturk@gmail.com>
2007-05-27 10:10:28 +0000

    bump && 0.0_x

--- applications/science/electronics/geda-gschem/actions.py
+++ applications/science/electronics/geda-gschem/actions.py
@@ -8,6 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir="geda-gattrib-%s" % get.srcVERSION().split('_')[-1]
+
 def setup():
     autotools.configure()
 
@@ -18,5 +20,5 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     pisitools.domo("po/tr.po", "tr", "geda-gschem.mo")
     pisitools.dosym("/usr/bin/gschem", "/usr/bin/geda")
-    
+
     pisitools.dodoc("AUTHORS","ChangeLog","NEWS","README","TODO","VOCABULARY")
