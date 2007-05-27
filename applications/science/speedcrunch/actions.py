cec01db29f4c54d8d81959df7210faf90ca33fa9
cartman <cartman@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-27 10:08:01 +0000

    merge

---
+++ applications/science/speedcrunch/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+
+def setup():
+    cmaketools.configure()
+
+def build():
+    cmaketools.make()
+
+def install():
+    pisitools.dobin("speedcrunch")
+
+    # add translation files and png
+    pisitools.insinto("/usr/share/crunch","crunch_*.qm")
+    pisitools.insinto("/usr/share/pixmaps","crunch.png")
