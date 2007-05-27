a0281b6e14d9da54f105923e08975e17d4d992f7
İsmail Dönmez <ismail@users.noreply.github.com>
2007-05-27 09:40:24 +0000

    fix versioning

--- programming/libs/libgeda/actions.py
+++ programming/libs/libgeda/actions.py
@@ -6,6 +6,9 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="libgeda-%s" % get.srcVERSION().split('_')[-1]
 
 def setup():
     autotools.configure("--disable-static")
