4719e0a860d0f39b5a7ccb93bd6aab52eb77b7c7
MeW <MeW@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-28 23:46:36 +0000

    use kde parametres

--- desktop/kde/kfuseiso/actions.py
+++ desktop/kde/kfuseiso/actions.py
@@ -4,20 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import kde
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 WorkDir= "kfuseiso-%s" % get.srcVERSION().split('_')[-1]
 
 def setup():
-    autotools.configure()
+    kde.configure()
 
-def make():
-    autotools.make()
+def build():
+    kde.make()
 
 def install():
-    autotools.install()
+    kde.install()
 
     pisitools.domo("po/tr/iso_image_plugin.po", "tr", "iso_image_plugin.mo")
     pisitools.domo("po/tr/kfile_iso_image.po", "tr", "kfile_iso_image.mo")
