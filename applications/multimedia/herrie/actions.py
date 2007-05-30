f8c4158bd5ec0eafa7c80a95b8e4b15998675f78
Murat Senel <msenel@gmail.com>
2007-05-30 21:02:26 +0000

    beauty contest

--- applications/multimedia/herrie/actions.py
+++ applications/multimedia/herrie/actions.py
@@ -16,7 +16,7 @@ def build():
 def install():
     pisitools.dobin("herrie")
     pisitools.doman("herrie.1")
-    
+
     locales = ['nl','tr','de','pl']
     for locale in locales:
         pisitools.insinto("/usr/share/locale/%s/LC_MESSAGES" % locale, "%s.mo" % locale, "herrie.mo")
