e9ad77e1d39f0bdb2019bf5375864fa4ce5bc8e9
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-05-30 21:00:48 +0000

    beauty contest

--- applications/multimedia/enblend/actions.py
+++ applications/multimedia/enblend/actions.py
@@ -5,8 +5,6 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
     autotools.configure()
