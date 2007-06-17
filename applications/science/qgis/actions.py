1f20d7c32f963e0216f3952c223869e3fdfc4798
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-06-17 19:55:31 +0000

    compile

--- applications/science/qgis/actions.py
+++ applications/science/qgis/actions.py
@@ -10,6 +10,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.system("sh autogen.sh")
+
     autotools.configure("--disable-static \
                          --enable-gpx \
                          --enable-georef \
