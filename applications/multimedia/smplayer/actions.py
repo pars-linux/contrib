4e49bea667616e6575a0031dc22d35a2880174e3
Tuxedup <Tuxedup@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-06-17 23:19:30 +0000

    An advanced mplayer gui.  First release

---
+++ applications/multimedia/smplayer/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+def setup():
+        autotools.make("prep")
+
+def build():
+        autotools.make("QMAKE=qmake-qt4 PREFIX=/usr")
+
+def install():
+	autotools.rawInstall("PREFIX=/usr DESTDIR=%s DOC_PATH=/usr/share/doc/%s" % (get.installDIR(),get.srcTAG()))
