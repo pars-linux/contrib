537e02a8f5c8e35f25335ba6d00e7161112bedf0
Eren Türkay <turkay.eren@gmail.com>
2007-06-25 23:33:37 +0000

    fix sanbox violation while installing dbus service, no need to bump release, thanks Nicolas Lara

--- desktop/gnome/base/gnome-vfs/actions.py
+++ desktop/gnome/base/gnome-vfs/actions.py
@@ -6,19 +6,20 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--enable-static=no \
-                                  --enable-ipv6 \
-                                  --enable-hal \
-                                  --with-hal-mount=/usr/bin/mount \
-                                  --with-hal-umount=/usr/bin/umount \
-                                  --with-hal-eject=/usr/bin/eject")
+                         --enable-ipv6 \
+                         --enable-hal \
+                         --with-hal-mount=/usr/bin/mount \
+                         --with-hal-umount=/usr/bin/umount \
+                         --with-hal-eject=/usr/bin/eject")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("TODO", "NEWS", "README", "HACKING", "AUTHORS", "ChangeLog")
