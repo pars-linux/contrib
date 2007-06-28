d4a6b032d6cfecaeadcc20e22bf8ac0b3320361f
Koray Löker <loker@radiobrecht.org>
2007-06-28 18:11:05 +0000

    greysctoration4gimp plugin

---
+++ applications/multimedia/gimp/greycstoration4gimp/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "GREYCstoration-%s" % get.srcVERSION()
+
+def build():
+    shelltools.cd("src")
+    autotools.make("gimp")
+
+def install():
+    pisitools.insinto("/usr/lib/gimp/2.0/plug-ins/", "src/greycstoration4gimp")
