4c542fc45533845325857923a898db9e8a2545e9
Eren Türkay <turkay.eren@gmail.com>
2007-06-30 17:24:52 +0000

    merge

---
+++ programming/languages/python/nltk/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+
+WorkDir = "nltk-0.8b2"
+
+def install():
+    pythonmodules.install()
+
+    pisitools.dodoc("README.txt")
