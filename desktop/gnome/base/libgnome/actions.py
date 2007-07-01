fdaab904e88d9b794505677852b0e236d396bcae
Eren Türkay <turkay.eren@gmail.com>
2007-07-01 14:38:28 +0000

    add package handler and post-install script

--- desktop/gnome/base/libgnome/actions.py
+++ desktop/gnome/base/libgnome/actions.py
@@ -8,7 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-static=no")
+    autotools.configure("--enable-static=no \
+                         --disable-schemas-install")
 
 def build():
     autotools.make()
