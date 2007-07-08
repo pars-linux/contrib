2c17aa613ac939dfc58a28bf4268f5c239067038
cartman <cartman@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-07-08 00:46:48 +0000

    point to GPLv2 text

--- programming/libs/imlib/actions.py
+++ programming/libs/imlib/actions.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # GNU General Public License, 
-# version 2. See the file http://www.gnu.org/copyleft/gpl.txt.
+# version 2. See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools 
