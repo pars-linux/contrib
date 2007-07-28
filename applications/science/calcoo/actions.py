d5731bb836afc3f8533255e4e92ffe2617b94c0c
ayan <ayan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-07-28 23:36:49 +0000

    küçük bir düzeltme

--- applications/science/calcoo/actions.py
+++ applications/science/calcoo/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("")
+    autotools.configure()
 
 def build():
     autotools.make()
