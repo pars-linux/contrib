d6c37a63fea62f7a721a650ae9686a9daa7cfe9a
ayan <ayan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-07-29 14:36:36 +0000

    bump phpmyadmin to latest stable

--- applications/admin/phpmyadmin/actions.py
+++ applications/admin/phpmyadmin/actions.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 
-WorkDir="phpMyAdmin-2.8.2"
+WorkDir="phpMyAdmin-%s-all-languages" % get.srcVERSION()
 
 def install():
     pisitools.dodir("/var/www/localhost/htdocs/phpmyadmin")
-    shelltools.move("%s/phpMyAdmin-2.8.2" % get.workDIR(),"%s/var/www/localhost/htdocs/phpmyadmin" % get.installDIR())
+    pisitools.insinto("/var/www/localhost/htdocs/phpmyadmin", "*")
