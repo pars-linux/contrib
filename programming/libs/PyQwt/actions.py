be74a10ea6c3c2f6b5d2e35759f381da2d99c99f
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-07-29 21:02:23 +0000

    thanks cartman
    disable qt3, add dpc and examples

--- programming/libs/PyQwt/actions.py
+++ programming/libs/PyQwt/actions.py
@@ -12,19 +12,14 @@ from pisi.actionsapi import pisitools
 
 
 def setup():
-    shelltools.copytree("../PyQwt-%s" % get.srcVERSION(),"../PyQwt-%s-qt4" % get.srcVERSION())
     shelltools.cd("configure")
-    shelltools.system("python configure.py -3 -I /usr/qt/3/include/qwt/")
-
-    shelltools.cd("../../PyQwt-%s-qt4/configure" % get.srcVERSION())
     shelltools.system("python configure.py -I /usr/qt/4/include/qwt/")
+
 
 def build():
     shelltools.cd("configure")
     autotools.make()
 
-    shelltools.cd("../../PyQwt-%s-qt4/configure" % get.srcVERSION())
-    autotools.make()
 
 
 
@@ -34,12 +29,5 @@ def install():
     pythonmodules.fixCompiledPy()
 
     # add Doc and examples for PyQwt-doc pisi
-    pisitools.insinto("/usr/share/doc/%s" % get.srcTAG(), "../qt3examples")
-    pisitools.insinto("/usr/share/doc/%s" % get.srcTAG(), "../Doc/html")
-
-    shelltools.cd("../../PyQwt-%s-qt4/configure" % get.srcVERSION())
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pythonmodules.fixCompiledPy()
-    
-    # add examples for PyQwt-doc pisi
     pisitools.insinto("/usr/share/doc/%s" % get.srcTAG(), "../qt4examples")
+    pisitools.insinto("/usr/share/doc/%s" % get.srcTAG(), "../Doc/html")
