c1e06839ee203bd09dc69ab93c57bc4e8185a65b
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-07-30 05:11:08 +0000

    utandım valla

--- programming/libs/PyQwt/actions.py
+++ programming/libs/PyQwt/actions.py
@@ -10,24 +10,18 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 
+WorkDir="PyQwt-%s/configure" % get.srcVERSION()
 
 def setup():
-    shelltools.cd("configure")
     shelltools.system("python configure.py -I /usr/qt/4/include/qwt/")
 
-
 def build():
-    shelltools.cd("configure")
     autotools.make()
 
-
-
-
 def install():
-    shelltools.cd("configure")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     pythonmodules.fixCompiledPy()
 
-    # add Doc and examples for PyQwt-doc pisi
+    # add Doc and examples 
     pisitools.insinto("/usr/share/doc/%s" % get.srcTAG(), "../qt4examples")
     pisitools.insinto("/usr/share/doc/%s" % get.srcTAG(), "../Doc/html")
