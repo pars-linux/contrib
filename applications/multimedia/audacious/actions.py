ce30aa1355751041547934f40c40e2f1f02eeff4
Murat Senel <msenel@gmail.com>
2007-07-31 19:34:40 +0000

    revert

--- applications/multimedia/audacious/actions.py
+++ applications/multimedia/audacious/actions.py
@@ -4,24 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("sh autogen.sh")
-
     autotools.configure("--enable-ipv6 \
                          --enable-chardet \
                          --enable-rpath \
                          --disable-gconf \
-                         --enable-dbus \
                          --enable-mcs \
                          --enable-xspf \
+                         --disable-xmltest \
                          --enable-samplerate \
-                         --enable-nls \
-                         --disable-xmltest")
+                         --enable-nls")
 def build():
     autotools.make()
 
