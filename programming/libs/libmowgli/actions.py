4f90e2eb7739792d2b3e60db5b3b05f7f06b3b5c
Murat Senel <msenel@gmail.com>
2007-07-31 17:53:18 +0000

    stable bump

--- programming/libs/libmowgli/actions.py
+++ programming/libs/libmowgli/actions.py
@@ -4,12 +4,10 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    shelltools.system("./autogen.sh")
     autotools.configure("--enable-examples")
 
 def build():
@@ -18,4 +16,4 @@ def build():
 def install():
     autotools.install()
 
-    pisitools.dodoc("AUTHORS", "COPYING", "README")
+    pisitools.dodoc("AUTHORS", "COPYING", "README", "doc/*")
