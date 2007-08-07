3c64d2921f823c3a6f26584f5261060934cb2c27
Uğur Çetin <ugur@ugurcetin.com.tr>
2007-08-07 08:29:46 +0000

    avoid sandbox violation

--- desktop/kde/K7Z/actions.py
+++ desktop/kde/K7Z/actions.py
@@ -6,22 +6,16 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 
+WorkDir = "%s/Build" % get.srcDIR()
+
 def setup():
-    shelltools.export("KDEDIR", get.installDIR() + get.kdeDIR())
+    pisitools.dosed("Makefile", "KDEDIR = /usr/", "KDEDIR = %s" % get.installDIR() + get.kdeDIR())
 
 def build():
-    shelltools.cd("Build")
     autotools.make("all")
 
 def install():
-    shelltools.cd("Build")
-    # Let's help the lazy makefile
-    pisitools.insinto("%s/bin" % get.kdeDIR(), "../Bin/K7Z.py")
-    pisitools.dodir("%s/share/apps/konqueror/servicemenus" % get.kdeDIR())
-    pisitools.dodir("%s/share/icons/hicolor/32x32/actions" % get.kdeDIR())
-
     autotools.rawInstall()
 
