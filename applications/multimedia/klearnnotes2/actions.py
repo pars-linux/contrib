e0faf3a299820287ce438840b5e5eb9010182049
İsmail Dönmez <ismail@users.noreply.github.com>
2007-08-09 19:38:12 +0000

    bana kompile yaptılar

--- applications/multimedia/klearnnotes2/actions.py
+++ applications/multimedia/klearnnotes2/actions.py
@@ -4,11 +4,12 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import kde
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import kde
 
 def setup():
+    kde.make("-f admin/Makefile.common")
     kde.configure()
 
 def build():
