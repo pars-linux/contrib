6f817b48e85df2f6baa741ee1f8e23777c2d64a4
İsmail Dönmez <ismail@users.noreply.github.com>
2007-08-11 14:18:39 +0000

    merge perlconsole from review

---
+++ programming/languages/perl/perlconsole/actions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+
+WorkDir = "perlconsole"
+
+def install():
+    pisitools.dobin("perlconsole")
+
+    pisitools.dodoc("CHANGES", "README", "COPYING")
