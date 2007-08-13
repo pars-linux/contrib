72f6fb96aa27be78df9a931149789c0f1d83ef88
Ahmet ERDOĞAN <tuxposta@gmail.com>
2007-08-13 17:58:47 +0000

    fix path

--- programming/libs/libBSUtilities/actions.py
+++ programming/libs/libBSUtilities/actions.py
@@ -23,7 +23,7 @@ def install():
 
     # install headers
     pisitools.insinto("/usr/include/bsutilities", "*.h")
-    pisitools.insinto("/usr/include/tinyxml", "tinyxml/*")
+    pisitools.insinto("/usr/include/bsutilities/tinyxml", "tinyxml/*")
 
     # install docs
     pisitools.dohtml("documentation/html")
