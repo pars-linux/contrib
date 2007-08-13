fdbe999b43b1cd70c62592a412c4db7d473bbf8c
Alper Tekinalp <alper.tekinalp@gmail.com>
2007-08-13 21:50:40 +0000

    merged to contrib

---
+++ applications/util/uudeview/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "uudeview-%s" % get.srcVERSION()
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.insinto("/usr/include/","uulib/uudeview.h")
+
+    pisitools.dodoc("COPYING", "README","HISTORY", "doc/*")
