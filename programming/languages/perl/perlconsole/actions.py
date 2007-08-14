9035dda322ede5db2c0b10255a1b81e953fa8ba5
cartman <cartman@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-08-14 22:03:57 +0000

    Alperi dövecem

--- programming/languages/perl/perlconsole/actions.py
+++ programming/languages/perl/perlconsole/actions.py
@@ -4,11 +4,13 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 WorkDir = "perlconsole"
 
 def install():
-    pisitools.dobin("perlconsole")
+    autotools.install("DEST=%s" % get.installDIR())
 
     pisitools.dodoc("CHANGES", "README", "COPYING")
