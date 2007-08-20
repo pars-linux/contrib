75b204e2377cbd9ee28b5bc16e3df12fb563ab27
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-08-20 19:44:47 +0000

    WorkDir'a gerek yok

--- desktop/kde/krusader/actions.py
+++ desktop/kde/krusader/actions.py
@@ -5,9 +5,7 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import kde
-from pisi.actionsapi import get
 
-WorkDir = "krusader-%s" % get.srcVERSION()
 
 def setup():
     kde.configure()
