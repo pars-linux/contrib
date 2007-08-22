70b1c5d6aaec4ad8c36e074609a46fd25191ee4d
Alper Tekinalp <alper.tekinalp@gmail.com>
2007-08-22 10:02:55 +0000

    version bump

--- applications/admin/osh/actions.py
+++ applications/admin/osh/actions.py
@@ -4,7 +4,10 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import pythonmodules
 
 def install():
     pythonmodules.install()
+
+    pisitools.dodoc("README", "LICENSE", "doc/*")
