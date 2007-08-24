c99a4ac48291adaedb90976a33e58be8ef582731
cartman <cartman@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-08-24 22:55:28 +0000

    add icc, this is pre-approved ages ago.

---
+++ programming/tools/icc/actions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+WorkDir="l_cc_c_%s_ia32/data" % get.srcVERSION()
+version_simple = (get.srcVERSION()).replace('.','')
+NoStrip="/"
+
+def setup():
+    shelltools.system("rpm2targz intel-icc%s-%s-1.i386.rpm" % (version_simple,get.srcVERSION()))
+    shelltools.system("tar xvf intel-icc%s-%s-1.i386.tar.gz" % (version_simple,get.srcVERSION()))
+
+def install():
+    pisitools.insinto("/","opt")
+
+    pisitools.dosed("%s/opt/intel/cc/*/bin/*" % get.installDIR(),"<INSTALLDIR>","/opt/intel/cc/%s" % get.srcVERSION())
+    pisitools.dosed("%s/opt/intel/cc/*/doc/csupport" % get.installDIR(),"<installpackageid>","l_cc_c_%s" % get.srcVERSION())
+
+    # Work around Turkish problems with FlexLM, Intel Premier Support Issue #366034
+    for app in ["icc","icpc"]:
+        pisitools.dosed("%s/opt/intel/cc/*/bin/%s" % (get.installDIR(),app),"#!/bin/sh","#!/bin/sh\n\nexport LC_ALL=C");
+
+    # Provide an empty licenses directory
+    pisitools.dodir("/opt/intel/licenses")
