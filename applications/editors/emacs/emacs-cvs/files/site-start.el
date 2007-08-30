ef0ec8275fcd6e2579bc70656ce268f35bb2c94f
İsmail Dönmez <ismail@users.noreply.github.com>
2007-08-30 12:16:24 +0000

    Support Pardus functions

--- applications/editors/emacs/emacs-cvs/files/site-start.el
+++ applications/editors/emacs/emacs-cvs/files/site-start.el
@@ -34,5 +34,58 @@
               "[']" nil ("") "~utf-8" utf-8)))
 
 
-;; Use with --enable-font-backend
-;; (set-default-font "DejaVu Sans Mono-10")
+;; Autoload init files under /etc/emacs/site-lisp
+(defun pardus-auto-load-all ()
+  "Autoload all init files under /etc/emacs/site-lisp"
+  (mapcar '(lambda (f)
+             (if (pardus-file-or-symlink-exists-p f)
+                 (load f)
+               nil))
+          (sort
+           (el-files-in-directory "/etc/emacs/site-lisp/")
+           'string-lessp)))
+
+(defun pardus-file-or-symlink-exists-p (f)
+  "Check if file or symlink exists"
+  (let ((f-symlink-target (file-symlink-p f)))
+    (if f-symlink-target
+        (let ((f-symlink-target-path
+               (concat (file-name-directory f) f-symlink-target)))
+          (if (file-exists-p f-symlink-target-path)
+              t
+            nil))
+      (file-exists-p f))))
+
+(defun el-files-in-directory (directory)
+  "List the .el files in DIRECTORY and in its sub-directories."
+  (let (el-files-list
+        (current-directory-list
+         (directory-files-and-attributes directory t)))
+    ;; while we are in the current directory
+    (while current-directory-list
+      (cond
+       ;; check to see whether filename ends in `.el'
+       ;; and if so, append its name to a list.
+       ((equal ".el" (substring (car (car current-directory-list)) -3))
+        (setq el-files-list
+              (cons (car (car current-directory-list)) el-files-list)))
+       ;; check whether filename is that of a directory
+       ((eq t (car (cdr (car current-directory-list))))
+        ;; decide whether to skip or recurse
+        (if
+            (equal (or "." "..")
+                   (substring (car (car current-directory-list)) -1))
+            ;; then do nothing if filename is that of
+            ;;   current directory or parent
+            ()
+          ;; else descend into the directory and repeat the process
+          (setq el-files-list
+                (append
+                 (el-files-in-directory
+                  (car (car current-directory-list)))
+                 el-files-list)))))
+      ;; move to the next filename in the list; this also
+      ;; shortens the list so the while loop eventually comes to an end
+      (setq current-directory-list (cdr current-directory-list)))
+    ;; return the filenames
+    el-files-list))
