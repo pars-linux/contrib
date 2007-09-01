212dc0b3dc6b560d488971ec8553ce612aaf5911
Uğur Çetin <ugur@ugurcetin.com.tr>
2007-09-01 01:38:45 +0000

    version bump

--- applications/games/openttd/actions.py
+++ applications/games/openttd/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
-WorkDir = "openttd-0.5.3-RC2"
+WorkDir = "openttd-0.5.3-RC3"
 
 def setup():
     #dummy configure enables freetype
