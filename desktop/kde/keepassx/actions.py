81edc26b9e368cf4019077eb14415ada357e5884
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-09-10 20:34:13 +0000

    Turkce ceviri icin Serdar Soytetir'e Tesekkurler ve bir iki fix

--- desktop/kde/keepassx/actions.py
+++ desktop/kde/keepassx/actions.py
@@ -8,15 +8,16 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
-
 def setup():
-    shelltools.system("/usr/bin/qmake-qt4 PREFIX=/{Install./usr/bin}")
+    shelltools.system("/usr/bin/qmake-qt4 prefix=/usr")
 
 def build():
     autotools.make()
 
-
 def install():
-    #autotools.install()
     pisitools.dobin("bin/keepass")
-    pisitools.insinto("/usr/share/","share/*")
+    pisitools.insinto("/usr/share/", "share/*")
+
+    # Turkish translation
+    shelltools.system("lrelease-qt4 src/translations/keepass-tr_TR.ts -qm src/translations/keepass-tr_TR.qm")
+    pisitools.insinto("/usr/share/keepass/i18n/", "src/translations/keepass-tr_TR.qm")
