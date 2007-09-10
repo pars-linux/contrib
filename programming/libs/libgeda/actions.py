149923138ceeb92f187d6145f4e2c0f85b476b31
Ayan Öztürk <ayanozturk@gmail.com>
2007-09-10 20:33:40 +0000

    geda-* version bump [1/9]

--- programming/libs/libgeda/actions.py
+++ programming/libs/libgeda/actions.py
@@ -6,10 +6,7 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
-
-WorkDir="libgeda-%s" % get.srcVERSION().split('_')[-1]
 
 def setup():
     autotools.configure("--disable-static")
