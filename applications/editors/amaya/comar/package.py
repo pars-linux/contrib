de9cf6dcd90650d935f6ccde6d91b27d4e933001
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-09-11 20:05:43 +0000

    stable update

--- applications/editors/amaya/comar/package.py
+++ applications/editors/amaya/comar/package.py
@@ -4,29 +4,8 @@ import os
 
 def postInstall():
     # Set Amaya language
-    lang = open("/etc/env.d/03locale").readline().strip("LANG=")[:5]
 
-    # Set lang as Turkish
-    if lang == "tr_TR":
-        os.system("sed -i -e 's/LANG=en/LANG=tr/g' /usr/share/Amaya/config/unix-thot.rc")
-    # Set lang as Spanish
-    elif lang == "es_ES":
-        os.system("sed -i -e 's/LANG=en/LANG=es/g' /usr/share/Amaya/config/unix-thot.rc")
-    # Set lang as German
-    elif lang == "de_DE":
-        os.system("sed -i -e 's/LANG=en/LANG=de/g' /usr/share/Amaya/config/unix-thot.rc")
-    # Set lang as French
-    elif lang == "fr_FR":
-        os.system("sed -i -e 's/LANG=en/LANG=fr/g' /usr/share/Amaya/config/unix-thot.rc")
-    # Set lang as Italian
-    elif lang == "it_IT":
-        os.system("sed -i -e 's/LANG=en/LANG=it/g' /usr/share/Amaya/config/unix-thot.rc")
-    # Set lang as Finnish
-    elif lang == "fi_FI":
-        os.system("sed -i -e 's/LANG=en/LANG=fi/g' /usr/share/Amaya/config/unix-thot.rc")
-    # Set lang as Russian
-    elif lang == "ru_RU":
-        os.system("sed -i -e 's/LANG=en/LANG=ru/g' /usr/share/Amaya/config/unix-thot.rc")
-    # Set lang as Portuguese
-    elif lang == "pt_PT":
-        os.system("sed -i -e 's/LANG=en/LANG=pt/g' /usr/share/Amaya/config/unix-thot.rc")
+    lang = open("/etc/env.d/03locale").readline().strip("LANG=")[:5]
+    for i in ["tr_TR", "es_ES", "de_DE", "fr_FR", "it_IT", "fi_FI", "ru_RU", "pt_PT"]:
+        if lang == i:
+            os.system("sed -i -e 's/LANG=en/LANG=%s/g' /usr/share/Amaya/config/unix-thot.rc" % i.split("_")[0])
