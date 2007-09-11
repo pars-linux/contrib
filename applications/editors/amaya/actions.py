87517e3b5041ecafc147a1ec4f254ca3fa740e61
Murat Senel <msenel@gmail.com>
2007-09-11 20:05:43 +0000

    stable update

--- applications/editors/amaya/actions.py
+++ applications/editors/amaya/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="Amaya"
+WorkDir = "Amaya9.55/Amaya"
 
 def setup():
     # Cleanup crap
