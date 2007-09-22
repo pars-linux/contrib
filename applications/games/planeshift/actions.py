411106c85d273823bb0060b1b57161ec2816f56f
Murat Senel <msenel@gmail.com>
2007-09-22 08:45:47 +0000

    version bump

--- applications/games/planeshift/actions.py
+++ applications/games/planeshift/actions.py
@@ -9,22 +9,21 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "planeshift"
 datadir = "/usr/share/planeshift"
 
 def setup():
     shelltools.system("./autogen.sh")
 
     autotools.configure("--disable-debug \
-                         --without-separate-debug-info \
+                         --enable-optimize-mode-debug-info=no \
+                         --with-optimize-debug-info=no \
+                         --enable-separate-debug-info=no \
                          --enable-cpu-specific-optimizations=no \
                          --with-cs-prefix=/usr \
                          --with-cel-prefix=/usr \
                          --with-x \
-                         --with-Xaw \
-                         --with-Xxf86vm \
                          --with-cal3d \
-                         --without-bfd")
+                         --with-bfd")
 
 def build():
     pisitools.dosed("Jamconfig", "-O3", get.CFLAGS())
@@ -33,25 +32,28 @@ def build():
     shelltools.system("jam -aq client")
 
 def install():
-    pisitools.dodir("%s/data" % datadir)
     shelltools.system("jam -s DESTDIR=%s install_bin" % get.installDIR())
 
-    shelltools.copytree("lang", "%s/%s" % (get.installDIR(), datadir))
+    for art in ["art/sfxfiles.dtd", "art/pawseditor.zip", "art/psclient-setup.zip", "art/eedit.zip", "art/runes", "art/music"]:
+        pisitools.insinto("%s/art" % datadir, art)
 
-    pisitools.domove("/usr/bin/*", datadir)
-    pisitools.removeDir("/usr/bin")
+    for data in ["data/eedit", "data/partview", "data/pawseditor", "data/schemas",
+                 "data/npcbehave.xml", "data/npcdefs.xml", "data/pvp_regions.xml",
+                 "data/rpgrules.xml", "data/npcbehave.xml", "data/npcdefs.xml",
+                 "data/pvp_regions.xml", "data/rpgrules.xml"]:
+        pisitools.insinto("%s/data" % datadir, data)
 
-    shelltools.copytree("data/eedit", "%s/%s/data/" % (get.installDIR(), datadir))
-    shelltools.copytree("data/partview", "%s/%s/data/" % (get.installDIR(), datadir))
-    shelltools.copytree("data/pawseditor", "%s/%s/data/" % (get.installDIR(), datadir))
-    shelltools.copytree("data/schemas", "%s/%s/data/" % (get.installDIR(), datadir))
+    for f in ["lang", "*.cfg"]:
+        pisitools.insinto(datadir, f)
 
-    pisitools.dosed("planeshift.cfg", "Video.FullScreen = false", "Video.FullScreen = true")
-    pisitools.dosed("psclient.cfg", "cvs", "elves")
-
-    pisitools.insinto(datadir, "*.cfg")
+    pisitools.insinto("%s/art/world" % datadir, "art/world/sound.xml")
 
     pisitools.dosym("/usr/share/fonts/dejavu/DejaVuSerif.ttf", "%s/data/ttf/arial.ttf" % datadir)
 
+    pisitools.domove("/usr/bin/*", datadir)
+    pisitools.removeDir("/usr/bin")
+    pisitools.remove("/usr/share/planeshift/art/music/README")
+
     pisitools.dodoc("docs/*.txt")
     pisitools.dohtml("docs/*")
+
