f52ee4287a98a439ce63104e31aaee44d04870f6
Murat Senel <msenel@gmail.com>
2007-09-26 15:28:52 +0000

    no need for it

--- applications/science/grass/actions.py
+++ applications/science/grass/actions.py
@@ -9,8 +9,6 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-grassdir = "grass-%s" % get.srcVERSION()
-
 def setup():
     autotools.configure("--disable-static \
                          --enable-shared \
