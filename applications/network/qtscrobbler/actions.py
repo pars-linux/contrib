500120ba0992571f572ed2caf9671ee45454097d
mgunay <mgunay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-10-04 23:06:07 +0000

    BUG:FIX:6403 merge from review

---
+++ applications/network/qtscrobbler/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "qtscrob-%s/src/qt/" % get.srcVERSION()
+
+def setup():
+    shelltools.system("qmake-qt4")
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dobin("qtscrob")
+
+    pisitools.insinto("/usr/share/qtscrobbler/resources", "resources/*")
+    pisitools.insinto("/usr/share/qtscrobbler/translations", "translations/*.qm")
+
+    pisitools.dodoc("README", "TODO")
