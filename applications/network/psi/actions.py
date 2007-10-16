3cbc07d623077fdfcbf2d636003c205b90802a6b
dfisek <dfisek@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-10-16 11:48:29 +0000

    Version bump to Qt4 port

--- applications/network/psi/actions.py
+++ applications/network/psi/actions.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.rawConfigure("--prefix=/usr") 
+    autotools.rawConfigure("--prefix=/usr --qtdir=/usr/qt/4") 
 
 def build():
     autotools.make()
