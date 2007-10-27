0b0a8a92cc0e921155365ec92ccc0584f5f0cba7
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-10-27 12:12:05 +0000

    * version bump

--- desktop/gnome/base/gtksourceview/actions.py
+++ desktop/gnome/base/gtksourceview/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -16,5 +16,6 @@ def build():
 
 def install():
     autotools.install()
+
     pisitools.dodoc("AUTHORS", "ChangeLog", "HACKING", "MAINTAINERS", "NEWS", "README", "TODO")
 
