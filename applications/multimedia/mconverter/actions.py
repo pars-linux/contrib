59d96846e4b8050adb9d348a40612a31db42faf8
mgunay <mgunay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-10-27 22:30:52 +0000

    pifff

--- applications/multimedia/mconverter/actions.py
+++ applications/multimedia/mconverter/actions.py
@@ -12,6 +12,6 @@ WorkDir="Multimedia Converter %s" % get.srcVERSION()
 
 def install():
     shelltools.system("tar xvf mconverter-1.5.tgz")
-    pisitools.insinto("/usr/kde/3.5/bin", "usr/local/LinConverter/LinConverter.kmdr", "mconverter.kmdr")
+    pisitools.insinto("/usr/kde/3.5/bin", "src/LinConverter.kmdr", "mconverter.kmdr")
 
     pisitools.dodoc("usr/share/doc/license.txt")
