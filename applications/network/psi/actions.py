91b63d77c9f29f944c3dda0f211eed756e93242e
İsmail Dönmez <ismail@users.noreply.github.com>
2007-10-28 14:30:18 +0000

    - Really use external QCA - Move qca-ossl to RuntimeDependencies

--- applications/network/psi/actions.py
+++ applications/network/psi/actions.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
@@ -9,11 +9,14 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.rawConfigure("--prefix=/usr --qtdir=/usr/qt/4") 
+    autotools.rawConfigure("--prefix=/usr \
+                            --qtdir=/usr/qt/4 \
+                            --disable-bundled-qca")
 
 def build():
     autotools.make()
 
 def install():
-    pisitools.dodoc("README","COPYING","ChangeLog","TODO")
     autotools.rawInstall("INSTALL_ROOT='%s'"  % get.installDIR())
+
+    pisitools.dodoc("README","COPYING","ChangeLog","TODO")
