c3cd4c90588ce823ee708c9c37de048b56c5fb57
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-10-29 12:36:08 +0000

    New snapshot

--- desktop/opencompositing/ccsm/actions.py
+++ desktop/opencompositing/ccsm/actions.py
@@ -6,8 +6,9 @@
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir = "ccsm"
+WorkDir="ccsm-%s" % get.srcVERSION()
 
 def install():
     pythonmodules.install("--prefix=/usr")
