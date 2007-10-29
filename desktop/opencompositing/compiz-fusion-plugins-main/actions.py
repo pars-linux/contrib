75643cdaf6cff412aa32b109bdceb0edef65af01
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-10-29 12:41:35 +0000

    New snapshot

--- desktop/opencompositing/compiz-fusion-plugins-main/actions.py
+++ desktop/opencompositing/compiz-fusion-plugins-main/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "plugins-main"
+WorkDir = "plugins-main-%s" % get.srcVERSION()
 
 def setup():
     shelltools.system("./autogen.sh")
