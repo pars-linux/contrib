47098d8a4997097662023cf0155c0225dfa50872
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-10-29 12:37:20 +0000

    New snapshot

--- desktop/opencompositing/compizconfig-backend-kconfig/actions.py
+++ desktop/opencompositing/compizconfig-backend-kconfig/actions.py
@@ -4,11 +4,13 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.system("./autogen.sh")
     autotools.configure("--disable-static")
 
 def build():
