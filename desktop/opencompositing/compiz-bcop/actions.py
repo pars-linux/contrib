f8f018c2b8b3b75de510017cfa0509b7fabeab39
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-10-29 12:36:44 +0000

    New snapshot

--- desktop/opencompositing/compiz-bcop/actions.py
+++ desktop/opencompositing/compiz-bcop/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "bcop"
+WorkDir = "bcop-%s" % get.srcVERSION()
 
 def setup():
     shelltools.system("./autogen.sh")
