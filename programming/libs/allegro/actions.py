60e8cf9c44965e1851bdd028a2553be4e7b057b8
cartman <cartman@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-10-30 18:12:05 +0000

    docs

--- programming/libs/allegro/actions.py
+++ programming/libs/allegro/actions.py
@@ -5,6 +5,7 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure("--disable-artsdigi \
@@ -17,3 +18,5 @@ def build():
 def install():
     autotools.install()
     autotools.make("install-man")
+
+    pisitools.dodoc("AUTHORS","CHANGES","THANKS","readme.txt")
