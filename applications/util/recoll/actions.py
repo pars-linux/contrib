c8859b44f349d870e206e74eb69aef2d84d5b180
mgunay <mgunay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-01 13:12:06 +0000

    merged from review

---
+++ applications/util/recoll/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import pisitools
+
+def setup():
+    shelltools.export("QTDIR", "/usr/qt/4")
+    autotools.configure("--prefix=/usr \
+                         --with-x \
+                         --with-inotify \
+                         --disable-static")
+
+    shelltools.cd("qtgui/i18n")
+    shelltools.system("lrelease-qt4 recoll_tr.ts")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("COPYING", "README", "ChangeLog", "VERSION")
