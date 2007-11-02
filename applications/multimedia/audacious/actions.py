448f0695f2a14254e29641cc47e5959c1d1c8b6f
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-02 18:00:33 +0000

    bump

--- applications/multimedia/audacious/actions.py
+++ applications/multimedia/audacious/actions.py
@@ -8,6 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir = "audacious-1.4.0-rc1"
+
 def setup():
     autotools.configure("--enable-ipv6 \
                          --enable-chardet \
