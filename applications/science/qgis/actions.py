c6e1c9102cac88160f317f60a454526d828cc93a
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-05 22:00:46 +0000

    stable bump

--- applications/science/qgis/actions.py
+++ applications/science/qgis/actions.py
@@ -4,35 +4,25 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-def setup():
-    shelltools.system("sh autogen.sh")
+WorkDir = "qgis_%s" % get.srcVERSION()
 
-    autotools.configure("--disable-static \
-                         --enable-gpx \
-                         --enable-georef \
-                         --enable-spit \
-                         --with-qtdir=/usr/qt/4 \
-                         --with-x \
-                         --with-python \
-                         --with-grass=/opt/grass-6.2.1 \
-                         --with-geos=/usr/bin/geos-config \
-                         --with-gdal=/usr/bin/gdal-config \
-                         --with-projdir=/usr \
-                         --with-sqlite3 \
-                         --with-postgresql \
-                         --with-gsl")
+def setup():
+    cmaketools.configure()
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    cmaketools.install("DESTDIR=%s" % get.installDIR())
 
     pisitools.insinto("/usr/share/pixmaps", "images/icons/qgis-icon.png", "qgis.png")
 
-    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
+    pisitools.domove("/usr/man", "/usr/share")
+
+    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "NEWS", "README", "TODO", "*.txt")
