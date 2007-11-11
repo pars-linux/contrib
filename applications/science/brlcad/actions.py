a34bd04f1dd3719cc24d1c22fab1d204310437ad
cartman <cartman@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-11 22:10:35 +0000

    ignore

--- applications/science/brlcad/actions.py
+++ applications/science/brlcad/actions.py
@@ -9,8 +9,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import libtools
-
 
 def setup():
     autotools.autoreconf("-fi")
