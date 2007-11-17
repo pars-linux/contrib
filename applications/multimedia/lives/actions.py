d0202faa12504c6204ff9ec8293c5576e70bad47
mgunay <mgunay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-17 03:05:21 +0000

    merged from review

---
+++ applications/multimedia/lives/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    pisitools.dodir("/usr/share/pixmaps")
+    pisitools.dodir("/usr/share/applications")
+
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog", "COPYING", "FEATURES","GETTING.STARTED", "NEWS", "README")
