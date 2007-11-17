fc5662ed6c4b0dced5ac4c0ecf792a2628d63ad3
aerdogan <aerdogan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-17 02:15:28 +0000

    move from review

---
+++ programming/libs/scipres/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+
+WorkDir = "scipres-1.0.0rc1"
+
+def install():
+    pythonmodules.install()
+
+    pisitools.dodoc("README")
