3089c9d3c8b852cf81e21f0ae85cd6768089e67e
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-19 14:38:15 +0000

    * merged

---
+++ applications/network/yougrabber/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+WorkDir = "YouGrabber-0.29-beta2"
+
+def build():
+    shelltools.cd("src")
+    shelltools.system("make")
+    shelltools.cd("..")
+
+def install():
+    pisitools.insinto("/usr/bin", "src/yg", "yougrabber")
+
+    pisitools.dodoc("doc/LICENSE", "doc/README", "doc/CHANGELOG", "yg.conf.sample")
