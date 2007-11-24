b609d154bbe6a9309bbfea425e4a5517e6c5da6a
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-24 17:11:19 +0000

    K7Z: version bump and rename package to Q7Z

--- desktop/kde/Q7Z/actions.py
+++ desktop/kde/Q7Z/actions.py
@@ -8,15 +8,14 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
-WorkDir = "%s/Build" % get.srcDIR()
+WorkDir = "%s/Build" % get.srcNAME()
 
 def setup():
     pisitools.dosed("Makefile", "PISI_KDEDIR", get.kdeDIR())
     pisitools.dosed("Makefile", "PISI_DESTDIR", get.installDIR())
 
 def build():
-    autotools.make("all")
+    autotools.make()
 
 def install():
     autotools.rawInstall()
-
