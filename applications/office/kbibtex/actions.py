9e850cfcdbbabd600d9430b74708156c0f263764
cartman <cartman@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-27 04:31:07 +0000

    ignore

--- applications/office/kbibtex/actions.py
+++ applications/office/kbibtex/actions.py
@@ -3,7 +3,7 @@
 #
 # Copyright 2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt .
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import kde
 from pisi.actionsapi import pisitools
