68d86bb66bc8170cb6c130d04543785891674b63
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-11-30 17:48:23 +0000

    kotaci: Version bump

--- applications/network/kotaci/actions.py
+++ applications/network/kotaci/actions.py
@@ -5,10 +5,18 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+
+def setup():
+    shelltools.system("qmake-qt4")
+
+def build():
+    autotools.make()
 
 def install():
-    pythonmodules.install()
+    pisitools.dobin("bin/kotaci")
+    pisitools.insinto("/usr/share/applications", "data/kotaci.desktop")
 
     pisitools.dodoc("AUTHORS", "COPYING", "README", "TODO")
