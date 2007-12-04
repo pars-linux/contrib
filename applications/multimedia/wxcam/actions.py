4aec771177107a19a6917c2b983710816868cf5e
mgunay <mgunay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-04 07:58:59 +0000

    merged from review

---
+++ applications/multimedia/wxcam/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+
+def setup():
+    autotools.configure("--with-wx-config=/usr/bin/wx-config-2.8")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.removeDir("/usr/doc")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
