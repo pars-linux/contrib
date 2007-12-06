3f89ee11abf72cde37bb4f4ca0958738fc5c4aec
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-06 06:05:31 +0000

    * version bump

--- desktop/xfce4/base/libxfce4mcs/actions.py
+++ desktop/xfce4/base/libxfce4mcs/actions.py
@@ -8,13 +8,12 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--with-x")
 
 def build():
     autotools.make()
 
-
 def install():
     autotools.install()
-    
+
     pisitools.dodoc("NEWS", "README", "TODO", "ChangeLog", "AUTHORS")
