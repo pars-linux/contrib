ef51dc5adc95e5f22f6d87de0d6b08acc82aa600
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-06 08:15:20 +0000

    * version bump

--- desktop/xfce4/base/mousepad/actions.py
+++ desktop/xfce4/base/mousepad/actions.py
@@ -8,12 +8,12 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-static=no")
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-    
+
     pisitools.dodoc("TODO", "README", "NEWS", "ChangeLog", "AUTHORS")
