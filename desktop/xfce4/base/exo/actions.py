03f92a79b2482345914a115322d2fe8db1e94e3d
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-06 07:05:13 +0000

    * version bump

--- desktop/xfce4/base/exo/actions.py
+++ desktop/xfce4/base/exo/actions.py
@@ -8,14 +8,15 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-static=no")
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-    
-    pisitools.dodoc("TODO", "THANKS", "README", "NEWS", "AUTHOR", "ChangeLog")
+
     # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
+
+    pisitools.dodoc("TODO", "THANKS", "README", "NEWS", "AUTHOR", "ChangeLog")
