d41c4c0763091cbf8914a6ed429296ed1afc764d
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-08 21:31:52 +0000

    version bump

--- applications/network/centerim/actions.py
+++ applications/network/centerim/actions.py
@@ -8,7 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --with-libotr=enable")
 
 def build():
     autotools.make()
