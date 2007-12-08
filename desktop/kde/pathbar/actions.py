b43f590e84a196ed30d56492278686f4db31db95
Uğur Çetin <ugur@ugurcetin.com.tr>
2007-12-08 15:19:47 +0000

    pathbar: package fixes

--- desktop/kde/pathbar/actions.py
+++ desktop/kde/pathbar/actions.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2006 TUBITAK/UEKAE
+# Copyright 2006, 2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import get
+from pisi.actionsapi import kde
 
 def setup():
-    autotools.configure()
+    kde.configure()
 
 def build():
-    autotools.make()
+    kde.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    kde.install()
