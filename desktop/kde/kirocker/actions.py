5d239392af9c1d297be90da1c1434a5ff00e34ff
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-09 01:04:58 +0000

    bump

--- desktop/kde/kirocker/actions.py
+++ desktop/kde/kirocker/actions.py
@@ -5,9 +5,6 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import kde
-from pisi.actionsapi import get
-
-WorkDir = "kirocker-%s" % get.srcVERSION()
 
 def setup():
     kde.configure()
