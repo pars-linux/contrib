b65b39c79fd444995fa760e0c59a497b3b5b607b
mgunay <mgunay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-17 13:00:09 +0000

    daboide is forked now

--- programming/tools/dabo/actions.py
+++ programming/tools/dabo/actions.py
@@ -6,10 +6,16 @@
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 WorkDir = "dabo"
 
 def install():
     pythonmodules.install()
 
-    pisitools.dodoc("ANNOUNCE", "AUTHORS", "ChangeLog", "LICENSE.TXT", "README", "TODO")
+    pisitools.insinto("/usr/lib/%s/site-packages/dabo/ide" % get.curPYTHON(), "ide/*")
+    pisitools.insinto("/usr/share/pixmaps", "ide/sampleDaboIcon.png", "daboide.png")
+
+    pisitools.domove("/usr/dabo/*", "/usr/lib/%s/site-packages/dabo" % get.curPYTHON())
+
+    pisitools.dodoc("ANNOUNCE", "AUTHORS", "ChangeLog", "dabo/LICENSE.TXT", "README", "TODO")
