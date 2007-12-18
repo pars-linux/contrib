3a511615abb9d7b0f3b59e8d30a55837e4e07ed3
Murat Senel <msenel@gmail.com>
2007-12-18 14:01:26 +0000

    pif

--- programming/libs/geos/actions.py
+++ programming/libs/geos/actions.py
@@ -14,10 +14,6 @@ from pisi.actionsapi import get
 WorkDir = "geos-3.0.0rc5"
 
 def setup():
-    #shelltools.export("WANT_AUTOCONF", "2.5")
-
-    #libtools.libtoolize("--copy --force")
-
     autotools.configure("--with-pic \
                          --disable-static \
                          --enable-inline \
