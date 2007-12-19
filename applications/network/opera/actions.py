561f4d553daf33c5491a145b463975ee08acf6ed
İsmail Dönmez <ismail@users.noreply.github.com>
2007-12-19 13:22:09 +0000

    Fix http://secunia.com/advisories/28169/e

--- applications/network/opera/actions.py
+++ applications/network/opera/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="opera-9.24-20071015.6-shared-qt.i386-en-671"
+WorkDir="opera-9.25-20071214.5-shared-qt.i386-en-687"
 
 def install():
     shelltools.system("./install.sh DESTDIR=%s" % get.installDIR())
