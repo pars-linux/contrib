2861a6574695483b60b52467d8fe74f711ebfb8c
aerdogan <aerdogan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-19 05:23:15 +0000

    Version Bump & some fixes

--- applications/science/massxpert/actions.py
+++ applications/science/massxpert/actions.py
@@ -4,20 +4,17 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
 
-WorkDir = "massxpert"
-
 def setup():
-    shelltools.system("qmake-qt4")
+    cmaketools.configure()
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.rename("usr/share/doc/massxpert",get.srcTAG())
+    pisitools.dodoc("COPYING")
