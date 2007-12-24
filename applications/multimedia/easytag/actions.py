d378b4184c163bdf39134e537076f5e038c34b31
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2007-12-24 19:25:38 +0000

    Drop id3lib in favor of libid3tag

--- applications/multimedia/easytag/actions.py
+++ applications/multimedia/easytag/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-id3v23")
 
 def build():
     autotools.make()
