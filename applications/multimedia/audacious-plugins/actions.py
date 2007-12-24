44df087352031c4d515ea9d7b27b759d6c43e2e7
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-24 22:13:26 +0000

    Fix deps and translation

--- applications/multimedia/audacious-plugins/actions.py
+++ applications/multimedia/audacious-plugins/actions.py
@@ -13,41 +13,15 @@ def setup():
                          --enable-rpath \
                          --enable-ipv6 \
                          --enable-chardet \
+                         --disable-pulse \
+                         --disable-coreaudio \
                          --disable-gconf \
-                         --enable-esd \
-                         --enable-mp3 \
-                         --enable-rocklight \
-                         --enable-lirc \
-                         --enable-evdevplug \
-                         --enable-statusicon \
-                         --enable-aosd \
-                         --enable-aosd-xcomp \
-                         --enable-adplug \
-                         --enable-vorbis \
-                         --enable-wavpack \
-                         --enable-aac \
-                         --enable-sndfile \
-                         --enable-modplug \
-                         --enable-flac \
-                         --enable-musepack \
-                         --enable-wma \
-                         --enable-jack \
-                         --enable-arts \
-                         --enable-sid \
-                         --enable-oss \
-                         --enable-alsa \
+                         --disable-gnomeshortcuts \
+                         --disable-neon \
                          --enable-amidiplug \
                          --enable-amidiplug-alsa \
-                         --enable-amidiplug-flysn \
+                         --disable-amidiplug-flysn \
                          --enable-amidiplug-dummy \
-                         --enable-timidity \
-                         --enable-mms \
-                         --enable-paranormal \
-                         --enable-xspf \
-                         --disable-xmltest \
-                         --enable-projectm \
-                         --enable-tta \
-                         --enable-lame \
                          --with-sidplay2=/usr \
                          --with-sidplay2-lib=/usr/lib \
                          --with-sidplay2-inc=/usr/include/sidplay \
