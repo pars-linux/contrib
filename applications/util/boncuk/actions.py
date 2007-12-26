3f6151622672b35d16ed2f7af6a70d42357376ea
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2007-12-26 15:22:26 +0000

    Replaced pysozluk-qt with boncuk
    BUG:FIXED:6406
    BUG:FIXED:6654

--- applications/util/boncuk/actions.py
+++ applications/util/boncuk/actions.py
@@ -4,13 +4,19 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 
 def setup():
-    pisitools.dosed("pysozlukqt/online.py", "www", "www1")
+    shelltools.system("qmake-qt4")
+
+def build():
+    shelltools.system("lrelease-qt4 boncuk.pro")
+    autotools.make()
 
 def install():
-    pythonmodules.install()
+    pisitools.dobin("bin/boncuk")
+    pisitools.insinto("/usr/share/pixmaps", "resources/boncuk.png")
 
     pisitools.dodoc("AUTHORS", "COPYING", "README", "TODO")
