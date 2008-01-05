4caa2e876e8c1ec8462c1aaa728e155e01650481
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-01-05 06:15:07 +0000

    fet version 5 thanks mehmet çatalbaş and  murat şenel

--- applications/science/fet/actions.py
+++ applications/science/fet/actions.py
@@ -4,10 +4,9 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
-
 
 def setup():
     shelltools.system("qmake-qt4 fet.pro")
@@ -15,12 +14,15 @@ def setup():
 def build():
     autotools.make()
 
-
 def install():
-    pisitools.dobin("fet","/usr/share/fet")
-    pisitools.dosym("/usr/share/fet/fet","/usr/bin/fet")
+    pisitools.dobin("fet")
+
+    pisitools.insinto("/usr/share/fet/sample_inputs","sample_inputs/*")
     pisitools.insinto("/usr/share/fet/translations","translations/*.qm")
     pisitools.insinto("/usr/share/fet/doc","doc/*")
-    pisitools.doman("doc/fet.1")
+
     pisitools.remove("/usr/share/fet/doc/fet.1")
-    pisitools.insinto("/usr/share/fet/sample_inputs","sample_inputs/*")
+
+    pisitools.doman("doc/fet.1")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "CONTRIBUTORS", "COPYING", "GUESTBOOK",
+    "LINKS", "README", "REFERENCES", "SPONSORS", "THANKS", "TODO", "TRANSLATORS")
