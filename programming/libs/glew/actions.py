804b11fef16c75959cf60e1e69fa568cf3132f20
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-01-12 22:42:12 +0000

    take over, fix library permission and cflags

--- programming/libs/glew/actions.py
+++ programming/libs/glew/actions.py
@@ -5,8 +5,8 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 WorkDir = 'glew'
 
@@ -16,8 +16,5 @@ def build():
 def install():
     autotools.rawInstall("GLEW_DEST=%s/usr" % get.installDIR())
 
-    # No static libraries
-    pisitools.remove("/usr/lib/libGLEW.a")
-
-    pisitools.dodoc("README.txt", "doc/*.txt")
     pisitools.dohtml("doc/*")
+	pisitools.dodoc("README.txt", "doc/*.txt")
