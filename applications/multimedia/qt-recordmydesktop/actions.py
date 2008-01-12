c81b115f878937af1aa31e78ef4378ee3df3efe2
mgunay <mgunay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-01-12 19:52:19 +0000

    version bump

--- applications/multimedia/qt-recordmydesktop/actions.py
+++ applications/multimedia/qt-recordmydesktop/actions.py
@@ -16,4 +16,4 @@ def build():
 def install():
     autotools.install()
 
-    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README")
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "NEWS", "README")
