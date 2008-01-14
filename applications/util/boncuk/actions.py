1a875ed3adc56dc45a65e7714aa3b5df2335fe8f
Uğur Çetin <ugur@ugurcetin.com.tr>
2008-01-14 09:07:32 +0000

    Change C/XXFLAGS

--- applications/util/boncuk/actions.py
+++ applications/util/boncuk/actions.py
@@ -5,11 +5,16 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 def setup():
     shelltools.system("qmake-qt4")
+
+    # Change C/XXFLAGS
+    pisitools.dosed("Makefile", "CFLAGS        = -pipe -O2", "CFLAGS        = %s" % get.CFLAGS())
+    pisitools.dosed("Makefile", "CXXFLAGS      = -pipe -O2", "CXXFLAGS      = %s" % get.CXXFLAGS())
 
 def build():
     shelltools.system("lrelease-qt4 boncuk.pro")
