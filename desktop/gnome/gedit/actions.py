ffdac3e6c8462bd86289dcd8b3a532c1408690d2
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-01-26 07:39:46 +0000

    yay, new gedit release :)

--- desktop/gnome/gedit/actions.py
+++ desktop/gnome/gedit/actions.py
@@ -6,16 +6,20 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
 
 def setup():
-    autotools.configure("--disable-scrollkeeper \
-                                  --disable-spell \
-                                  --enable-python")
+    autotools.configure("--disable-static \
+                         --disable-scrollkeeper \
+                         --disable-spell \
+                         --enable-python")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+
+    pythonmodules.fixCompiledPy(lookInto="/usr/lib/gedit-2/plugins")
 
     pisitools.dodoc("NEWS", "TODO", "README", "BUGS", "AUTHORS", "ChangeLog")
--- desktop/gnome/gedit/comar/package.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall():
-    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gedit")
-
-def preRemove():
-    os.system("scrollkeeper-update -p /var/lib/scrollkeeper -o /usr/share/omf/gedit")
