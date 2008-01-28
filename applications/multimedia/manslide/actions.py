a05294961d9a64f0aa980ef9d4ff70baaa3de4be
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-01-28 22:54:14 +0000

    Version bump, fix the white screen bug, add new presentation 3D model BUG:FIXED:6520

--- applications/multimedia/manslide/actions.py
+++ applications/multimedia/manslide/actions.py
@@ -9,8 +9,6 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "Manslide-%s" % get.srcVERSION()
-
 def setup():
     shelltools.system("qmake-qt4 Manslide.pro")
 
