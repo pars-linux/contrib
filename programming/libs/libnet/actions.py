26711a4d603cb85857aa42c3a7d196213dd0ea9c
Gökçen Eraslan <gokcen.eraslan@gmail.com>
2008-01-28 12:38:08 +0000

    libnet package

---
+++ programming/libs/libnet/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "libnet"
+
+def setup():
+    autotools.autoreconf("--force --install")
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dohtml("doc/html/*")
+    pisitools.doman("doc/man/man3/*")
+    pisitools.dodoc("doc/*", "README")
