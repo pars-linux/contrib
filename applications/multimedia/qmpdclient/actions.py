805e2de81e7d53812b2ee7cf7f8f6c62b5f651b0
soytetir <soytetir@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-02-01 04:16:29 +0000

    tr.patch kalsın, bir bilen pakete eklesin :)

--- applications/multimedia/qmpdclient/actions.py
+++ applications/multimedia/qmpdclient/actions.py
@@ -9,7 +9,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    shelltools.system("lrelease-qt4 tr_TR.ts -qm tr_TR.qm")
     shelltools.system("qmake-qt4 qmpdclient.pro")
 
 def build():
