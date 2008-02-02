4377d595fff5096b24ac95f0eab0ce8a58bd7d94
mgunay <mgunay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-02-02 19:13:01 +0000

    use external dxfliband fparser, fixed desktop, remove unnecessary dependency

--- applications/science/qcad/actions.py
+++ applications/science/qcad/actions.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import libtools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "qcad-2.0.5.0-1-community.src"
+WorkDir = "qcad-%s-1-community.src" % get.srcVERSION()
 
 def setup():
     shelltools.echo("defs.pro", "DEFINES += _REENTRANT QT_THREAD_SUPPORT")
@@ -27,26 +26,26 @@ def setup():
                     -DQT_TABLET_SUPPORT -DQT_THREAD_SUPPORT" % get.CFLAGS())
 
     pisitools.dosed("qcadlib/src/qcadlib.pro", "INCLUDEPATH \+= (.*)", "INCLUDEPATH += \\1 ../../qcadcmd/src")
+    pisitools.dosed("qcad/Makefile", "../dxflib/lib/libdxf.a", "")
 
 def build():
     modules = ["qcadlib", "qcadcmd", "qcadactions", "qcadguiqt", "qcad"]
-    
-    shelltools.cd("fparser/")
-    autotools.configure()
-    autotools.make()
-    shelltools.cd("../")
-    
-    shelltools.cd("dxflib/")
-    autotools.configure()
-    autotools.make()
-    shelltools.cd("../")
 
     for i in modules:
         shelltools.cd("%s/src/" % i)
         shelltools.system("qmake %s.pro" % i)
-        shelltools.cd("../")
+        shelltools.cd("..")
         autotools.make()
-        shelltools.cd("../")
+        shelltools.cd("..")
+        for lang in ["cs", "el", "fr", "nl", "ru", "da", "en", "hu", "sk", "de", "et", "it", "pl", "tr"]:
+            shelltools.system("lrelease %s/src/ts/%s_%s.ts -qm %s/src/ts/%s_%s.qm" %(i, i, lang, i, i, lang))
 
 def install():
     pisitools.dobin("qcad/qcad")
+
+    for qm in ["qcadlib", "qcadcmd", "qcadactions", "qcadguiqt", "qcad"]:
+        pisitools.insinto("/usr/share/qcad/qm", "%s/src/ts/*.qm" % qm)
+
+    pisitools.insinto("/usr/share/qcad/fonts", "qcad/fonts/*.cxf")
+
+    pisitools.dohtml("qcad/doc/*")
