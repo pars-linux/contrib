3f1d9f03b0aa87c748be5956b465f823b7f92c12
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-02-06 13:16:39 +0000

    bump

--- applications/network/getmail/actions.py
+++ applications/network/getmail/actions.py
@@ -9,9 +9,10 @@ from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import get
 
 def install():
+    pisitools.dosed("setup.py", "'./getmail.spec'", "#'./getmail.spec'")
     pythonmodules.install()
 
     pisitools.dodoc("docs/*.txt", "docs/getmailrc-examples", "docs/CHANGELOG", "docs/COPYING", "README")
-    pisitools.dohtml("docs/configuration.html", "docs/documentation.html", "docs/troubleshooting.html")
+    pisitools.dohtml("docs/*")
 
     pisitools.removeDir("/usr/share/doc/getmail-%s/" % get.srcVERSION())
