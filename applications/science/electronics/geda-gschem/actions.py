086d4d3d4f11f1e481b75e2eb68ad36677a99d44
Ayan Öztürk <ayanozturk@gmail.com>
2008-02-06 18:17:20 +0000

    geda-* bump [6 of 9]

--- applications/science/electronics/geda-gschem/actions.py
+++ applications/science/electronics/geda-gschem/actions.py
@@ -9,14 +9,13 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-update-desktop-database")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.domo("po/tr.po", "tr", "geda-gschem.mo")
     pisitools.dosym("/usr/bin/gschem", "/usr/bin/geda")
 
     pisitools.dodoc("AUTHORS","ChangeLog","NEWS","README","TODO","VOCABULARY")
