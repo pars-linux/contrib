f80880edb16e59b162cd3b2003e41d99a6fdadbf
Ayan Öztürk <ayanozturk@gmail.com>
2008-02-09 20:47:11 +0000

    bump, bugfix release

--- applications/science/electronics/gerbv/actions.py
+++ applications/science/electronics/gerbv/actions.py
@@ -8,12 +8,13 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-unit-mm")
+    autotools.configure("--enable-unit-mm \
+                         --disable-update-desktop-database")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-    
+
     pisitools.dodoc("AUTHORS","ChangeLog","NEWS","README")
