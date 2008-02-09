6378ca18afc6f3572ed5838532a03f6079c9f1aa
cenk <cenk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-02-09 16:18:06 +0000

    BUG:FIXED:6932
    Thanks to Jerome Schneider

---
+++ programming/languages/python/elixir/actions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="Elixir-%s" % get.srcVERSION()
+
+def install():
+    pythonmodules.install()
+
+    pisitools.insinto("/usr/share/doc/%s" % get.srcTAG() ,"examples")
+    pisitools.dodoc("CHANGES", "LICENSE", "README")
