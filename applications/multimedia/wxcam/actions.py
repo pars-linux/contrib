f892f92d2084725dfcb626dc13087afcfc61eebb
cenk <cenk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-02-09 02:23:45 +0000

    stable update

--- applications/multimedia/wxcam/actions.py
+++ applications/multimedia/wxcam/actions.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007 TUBITAK/UEKAE
+# Copyright 2007,2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
+from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--with-wx-config=/usr/bin/wx-config-2.8 \
-                         --enable-static=no")
+                         --disable-static")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
-
-    pisitools.insinto("/usr/share/pixmaps", "src/appicon.xpm", "wxcam.xpm")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.removeDir("/usr/doc")
 
