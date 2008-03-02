cb08cbc8d4200d332b8dce4494d926cff42d100d
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-03-02 13:40:54 +0000

    stable release

--- desktop/kde/keepassx/actions.py
+++ desktop/kde/keepassx/actions.py
@@ -7,17 +7,20 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "KeePassX-%s" % get.srcVERSION()
 
 def setup():
-    shelltools.system("/usr/bin/qmake-qt4 prefix=/usr")
+    shelltools.system("qmake-qt4 prefix=/usr")
 
 def build():
     autotools.make()
 
 def install():
-    pisitools.dobin("bin/keepass")
+    pisitools.dobin("bin/keepassx")
     pisitools.insinto("/usr/share/", "share/*")
 
     # Turkish translation
     shelltools.system("lrelease-qt4 src/translations/keepass-tr_TR.ts -qm src/translations/keepass-tr_TR.qm")
-    pisitools.insinto("/usr/share/keepass/i18n/", "src/translations/keepass-tr_TR.qm")
+    pisitools.insinto("/usr/share/keepassx/i18n/", "src/translations/keepass-tr_TR.qm")
