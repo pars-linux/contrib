dcd26dc5c6ab92d86ca3f568c7b50b8feeb0b159
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-03-08 21:57:51 +0000

    Stable release

--- desktop/opencompositing/compiz/compizconfig-backend-kconfig/actions.py
+++ desktop/opencompositing/compiz/compizconfig-backend-kconfig/actions.py
@@ -4,19 +4,16 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
+from pisi.actionsapi import kde
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("./autogen.sh")
-    autotools.configure("--disable-static")
+    kde.configure("--disable-static")
 
 def build():
-    autotools.make()
+    kde.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    kde.install()
 
     pisitools.dodoc("COPYING*")
