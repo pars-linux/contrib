f1a76056779d543d6eeb12bfbc466d7fcdf3a4bb
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-03-14 15:02:16 +0000

    version bump

--- applications/science/extcalc/actions.py
+++ applications/science/extcalc/actions.py
@@ -4,11 +4,13 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.export("QTDIR", "/usr/qt/3")
     autotools.configure()
 
 def build():
@@ -17,4 +19,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
