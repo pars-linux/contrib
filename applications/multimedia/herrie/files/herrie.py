4a39f916967b03c77d596cbac011a5b2278f3d72
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-03-16 16:52:34 +0000

    * version bump and make HERRIE_DIR not executable

--- applications/multimedia/herrie/files/herrie.py
+++ applications/multimedia/herrie/files/herrie.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # A wrapper script for herrie. Herrie automatically saves playlists to ~/.herrie/playlist.xspf but ~/.herrie 
-# doesn't exist by default and it can't make it. Control whether ~/.herrie exits and create it if doesn't exist
+# doesn't exist by default and herrie doesn't create that directory. Control whether ~/.herrie exits and create it if doesn't exist
 
 import os
 import sys
@@ -10,7 +10,7 @@ import sys
 HERRIE_DIR = "%s/.herrie" % os.environ['HOME']
 
 if not os.path.exists(HERRIE_DIR):
-    os.mkdir(HERRIE_DIR, 0755)
+    os.mkdir(HERRIE_DIR, 0744)
 
 args = ''
 for arg in sys.argv[1:]:
