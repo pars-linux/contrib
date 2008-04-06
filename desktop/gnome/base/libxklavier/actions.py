5ef70f6ee69b90c24daf85894aaea723be89a2fc
Eren Türkay <turkay.eren@gmail.com>
2008-04-06 17:25:59 +0000

    * version bump

--- desktop/gnome/base/libxklavier/actions.py
+++ desktop/gnome/base/libxklavier/actions.py
@@ -8,7 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-xkb-support --enable-static=no")
+    autotools.configure("--disable-static \
+                         --enable-xkb-support")
 
 def build():
     autotools.make()
