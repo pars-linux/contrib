8bfb3e8581f8da64ee5b7ea38891c70620c67d81
Tuxedup <Tuxedup@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-07 23:17:51 +0000

    first release

--- desktop/kde/kuftp/actions.py
+++ desktop/kde/kuftp/actions.py
@@ -4,13 +4,16 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import kde
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 def setup():
-    kde.configure()
+    autotools.configure()
 
 def build():
-    kde.make()
+    autotools.make()
 
 def install():
-    kde.install()
+    autotools.install()
+    
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
