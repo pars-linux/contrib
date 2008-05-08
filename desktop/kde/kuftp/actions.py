54e8b53942cbd5b0bae2736a59f43b311cbca393
Eren Türkay <turkay.eren@gmail.com>
2008-05-08 12:33:45 +0000

    * revert. Homebank was accidentaly merged to kuftp, instead of bumping it :)

--- desktop/kde/kuftp/actions.py
+++ desktop/kde/kuftp/actions.py
@@ -4,16 +4,13 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
+from pisi.actionsapi import kde
 
 def setup():
-    autotools.configure()
+    kde.configure()
 
 def build():
-    autotools.make()
+    kde.make()
 
 def install():
-    autotools.install()
-    
-    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
+    kde.install()
