99570e0f3e7e4c50f1a906ebdcc4f232a0cb27fb
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-12 17:26:51 +0000

    merge

---
+++ 2008/applications/admin/conky/actions.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    pisitools.dosed("src/linux.c", "#include <linux/route.h>", "#include <net/route.h>")
+    autotools.configure("--disable-static \
+                         --disable-xmms2 \
+                         --disable-bmpx \
+                         --disable-audacious \
+                         --disable-mpd \
+                         --disable-debug \
+                         --enable-smapi \
+                         --enable-wlan \
+                         --enable-rss \
+                         --enable-own-window \
+                         --enable-proc-uptime \
+                         --enable-hddtemp \
+                         --enable-imlib2 \
+                         --enable-portmon \
+                         --enable-x11 \
+                         --enable-double-buffer \
+                         --enable-xdamage \
+                         --enable-wlan \
+                         --enable-xft")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    for data in ["doc/*.sample","extras/vim","extras/nano"]:
+        pisitools.insinto("/usr/share/doc/%s/examples/" % get.srcTAG(), data)
+
+    pisitools.dohtml("doc/*.html")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
