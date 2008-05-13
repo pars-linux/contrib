945f8e6672573198bd247f5552a2a055145c4668
Eren Türkay <turkay.eren@gmail.com>
2008-05-13 10:37:47 +0000

    merge: xfprint

---
+++ 2008/desktop/xfce4/base/xfprint/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-cups \
+                         --disable-bsdlpr")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    # conflict
+    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
+
+    pisitools.dodoc("README", "TODO", "NEWS", "AUTHORS", "ChangeLog")
