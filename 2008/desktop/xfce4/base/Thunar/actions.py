03a8043ddd695b5216885bda6cf369ef35559af8
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-13 09:10:26 +0000

    merge: Thunar

---
+++ 2008/desktop/xfce4/base/Thunar/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-gnome-thumbnailers \
+                         --with-volume-manager=hal")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    # conflict
+    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
+
+    pisitools.dodoc("TODO", "THANKS", "README", "HACKING", "FAQ", "AUTHORS", "ChangeLog")
