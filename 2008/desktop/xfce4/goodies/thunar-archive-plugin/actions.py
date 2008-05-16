39a347c83b6049782168bebf42c26a7b372caa85
Eren Türkay <turkay.eren@gmail.com>
2008-05-16 19:07:39 +0000

    merge: avoid sanbox violation

--- 2008/desktop/xfce4/goodies/thunar-archive-plugin/actions.py
+++ 2008/desktop/xfce4/goodies/thunar-archive-plugin/actions.py
@@ -4,9 +4,9 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
     autotools.configure()
@@ -15,6 +15,6 @@ def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
