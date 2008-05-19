3ac657e066313eca5f0bf45dcfc8478a78ec4ef9
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-19 10:13:46 +0000

    merge: soundkonverter

---
+++ 2008/applications/multimedia/soundkonverter/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+
+def setup():
+    kde.configure("--with-lame \
+                   --with-vorbis \
+                   --with-faad \
+                   --with-flac \
+                   --with-ffmpeg \
+                   --without-mp4v2")
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
