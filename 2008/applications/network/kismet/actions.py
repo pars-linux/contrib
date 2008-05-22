24d32ecc0592e8df0c1085b8cff1a1cb7ddd6ef8
MeW <MeW@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-22 21:26:36 +0000

    merge:kismet

---
+++ 2008/applications/network/kismet/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="kismet-2007-10-R1"
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make("dep")
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("CHANGELOG", "README", "TODO", "docs/*")
