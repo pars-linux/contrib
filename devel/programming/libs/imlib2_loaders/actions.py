ec02ffe0dfe9dc9d45aee62d19b999e2d845f9d8
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-29 18:30:28 +0000

    bump

--- devel/programming/libs/imlib2_loaders/actions.py
+++ devel/programming/libs/imlib2_loaders/actions.py
@@ -1,28 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
-
-WorkDir="imlib2_loaders-1.2.2.001"
-
-def setup():
-    shelltools.export("IMLIB2_CONFIG", "")
-    shelltools.export("EDB_CONFIG", "")
-    shelltools.export("EET_CONFIG", "")
-    autotools.configure("--disable-static \
-                         --enable-edb \
-                         --enable-eet \
-                         --enable-xcf")
-
-def build():
-    autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README*")
+    pisitools.dobin("*")
