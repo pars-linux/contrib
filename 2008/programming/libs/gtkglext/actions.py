ae5ba8425c7f804d448b6b81aa9111d4bc290e76
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-29 17:28:23 +0000

    merge: gtkglext

---
+++ 2008/programming/libs/gtkglext/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-glibtest \
+                         --disable-gtktest \
+                         --with-gdktarget=x11 \
+                         --with-x \
+                         --with-gl=/usr/lib/opengl/xorg-x11/lib \
+                         --with-gl-includedir=/usr/lib/opengl/include")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("README", "AUTHORS", "ChangeLog*", "COPYING*", "TODO")
