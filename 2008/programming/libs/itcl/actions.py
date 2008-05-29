21817f4ea51250c866989b34bfa85dd23e3f8491
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-29 18:24:15 +0000

    merge: itcl

---
+++ 2008/programming/libs/itcl/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--with-tcl=/usr/lib \
+                         --enable-shared \
+                         --enable-symbols")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    # needed symlink http://bugs.gentoo.org/show_bug.cgi?id=93307
+    pisitools.dosym("/usr/lib/itcl3.4/libitcl3.4.so","/usr/lib/libitcl3.4.so")
+
+    pisitools.dodoc("license.terms")
