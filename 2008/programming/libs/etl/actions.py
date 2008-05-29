100361a7170b1b7412e658302ca37933d0f3d027
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-29 15:19:12 +0000

    merge: etl

---
+++ 2008/programming/libs/etl/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "etl-%s_20071011" % get.srcVERSION()
+
+def setup():
+    shelltools.system("./bootstrap")
+
+    autotools.configure()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "README", "NEWS")
