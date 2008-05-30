003a949b84cb46018f6ca319fbe954cb13dcf594
MeW <MeW@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-30 13:08:49 +0000

    merge:veusz

---
+++ 2008/applications/science/veusz/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def install():
+    pythonmodules.compile()
+
+def install():
+    pythonmodules.install()
+
+    pisitools.insinto("/usr/share/veusz/examples", "examples/*")
+    pisitools.insinto("/usr/share/pixmaps", "windows/icons/veusz.png")
+    pisitools.insinto("usr/share/applications", "veusz.desktop")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "Documents/*.txt", "Documents/*.pdf")
+    pisitools.dohtml("Documents/*")
+    pisitools.domove("/usr/share/doc/%s/html/*.png" % get.srcTAG(), "/usr/share/doc/%s/html/manimages" % get.srcTAG())
