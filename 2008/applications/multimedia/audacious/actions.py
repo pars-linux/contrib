722c41245ba2babb3d89a626e60385e6aeae780b
Murat Senel <msenel@gmail.com>
2008-05-31 13:35:29 +0000

    merge

---
+++ 2008/applications/multimedia/audacious/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--enable-nls \
+                         --enable-ipv6 \
+                         --enable-chardet \
+                         --enable-dbus \
+                         --enable-samplerate \
+                         --enable-xspf \
+                         --enable-sm \
+                         --disable-xmltest")
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ABOUT-NLS", "AUTHORS", "COPYING", "HACKING", "NEWS", "README", "doc/NEW_FORMATS")
