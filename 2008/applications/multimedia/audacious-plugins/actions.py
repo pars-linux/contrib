9f5d3b9da271e9f81fc2765a100911a94631a693
murat <murat@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-05-31 13:35:34 +0000

    merge

---
+++ 2008/applications/multimedia/audacious-plugins/actions.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--enable-nls \
+                         --disable-rpath \
+                         --enable-ipv6 \
+                         --enable-chardet \
+                         --disable-esd \
+                         --enable-pulse \
+                         --enable-arts \
+                         --disable-coreaudio \
+                         --disable-gconf \
+                         --disable-gnomeshortcuts \
+                         --enable-amidiplug \
+                         --enable-amidiplug-alsa \
+                         --disable-amidiplug-flysn \
+                         --enable-amidiplug-dummy")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS")
