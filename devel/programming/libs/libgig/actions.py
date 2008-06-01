0c320e0a9f10d9a418fdf921d70adff8de3118b2
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-01 07:17:01 +0000

    version bump and add gcc43.patch

--- devel/programming/libs/libgig/actions.py
+++ devel/programming/libs/libgig/actions.py
@@ -8,10 +8,10 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-static=no")
+    autotools.configure("--disable-static")
 
 def build():
-    autotools.make()
+    autotools.make("-j1")
 
 def install():
     autotools.install()
