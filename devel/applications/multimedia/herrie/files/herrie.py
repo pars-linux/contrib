26c75e259a33860a328b8c8bbe5800303a43fb17
Eren Türkay <turkay.eren@gmail.com>
2008-06-02 14:36:51 +0000

    fix herrie script, if pulseaudio is not running, start it

--- devel/applications/multimedia/herrie/files/herrie.py
+++ devel/applications/multimedia/herrie/files/herrie.py
@@ -4,16 +4,20 @@
 # A wrapper script for herrie. Herrie automatically saves playlists to ~/.herrie/playlist.xspf but ~/.herrie 
 # doesn't exist by default and herrie doesn't create that directory. Control whether ~/.herrie exits and create it if doesn't exist
 
+# Also, start pulseaudio if it isn't running. On Pardus 2008, pulseaudio is default and ALSA libs use it.
+
 import os
+import subprocess
 import sys
 
 HERRIE_DIR = "%s/.herrie" % os.environ['HOME']
+PULSE_DIR = "/tmp/pulse-%s/pid" % os.environ['USER']
 
 if not os.path.exists(HERRIE_DIR):
     os.mkdir(HERRIE_DIR, 0744)
 
-args = ''
-for arg in sys.argv[1:]:
-    args += "%s " % arg
+if not os.path.exists(PULSE_DIR):
+    subprocess.call(["pulseaudio", "-D"])
 
-os.system("/usr/bin/herrie-bin %s" % args)
+args = " ".join(sys.argv[1:])
+subprocess.call(["/usr/bin/herrie-bin", args])
