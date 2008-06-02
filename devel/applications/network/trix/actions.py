7ec713ec8949cac340564c050cb4dce03cfb8767
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-02 17:26:55 +0000

    bump

--- devel/applications/network/trix/actions.py
+++ devel/applications/network/trix/actions.py
@@ -8,12 +8,12 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-rpath")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install() 
+    autotools.install()
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
