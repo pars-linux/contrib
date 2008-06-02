6547d9d3142cf0d57dc3528880c7ed751907c4d9
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-02 16:15:45 +0000

    merge: postgis

---
+++ 2008/programming/libs/postgis/actions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+def setup():
+    autotools.autoconf()
+    autotools.rawConfigure("--with-pgsql \
+                            --with-geos \
+                            --with-geos-libdir=/usr/lib \
+                            --with-proj \
+                            --with-proj-libdir=/usr/lib \
+                            --with-docdir=/usr/share/doc/%s \
+                            --datadir=/usr/share/postgresql/contrib \
+                            --libdir=/usr/lib/postgresql" % get.srcTAG())
+
+def build():
+    autotools.make()
+    shelltools.cd("topology")
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("topology")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    shelltools.cd("..")
+
+    pisitools.dodoc("CHANGES", "COPYING", "CREDITS", "TODO", "README.*", "doc/README")
+    pisitools.dohtml("doc/html/*")
+    pisitools.doman("doc/man/*")
