6819ae9b4bff59122d8dd3eae538cf17c18faaed
Eren Türkay <turkay.eren@gmail.com>
2008-06-02 15:50:28 +0000

    * version bump and get rid of unneded patch..

--- devel/programming/libs/newt/actions.py
+++ devel/programming/libs/newt/actions.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2005  TUBITAK/UEKAE
+# Copyright © 2005-2008  TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-#
-# Fahri Tuğrul Gürkaynak <ftugrul@gmail.com>
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
-    pisitools.dosed("Makefile.in", "-lslang", "-lslang -lncurses")
-
     autotools.configure()
 
 def build():
@@ -23,8 +17,7 @@ def build():
 def install():
     autotools.install()
 
-    pisitools.dosym("/usr/lib/libnewt.so.0.52.1", "/usr/lib/libnewt.so.0.52")
-
+    # remove static lib
     pisitools.remove("/usr/lib/libnewt.a")
 
     pisitools.dodoc("ChangeLog", "AUTHORS", "NEWS", "README*")
