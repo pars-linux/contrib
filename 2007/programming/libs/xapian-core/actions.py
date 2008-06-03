1b91767f6cdfa56b3eed0e77bf252bcef567cdb0
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-03 17:21:51 +0000

    merge: version bump

--- 2007/programming/libs/xapian-core/actions.py
+++ 2007/programming/libs/xapian-core/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-shared")
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
