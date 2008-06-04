68a58c3f65dfbd33b749cd9b456655a7c02b9dd2
MeW <MeW@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-04 18:02:13 +0000

    merge:scrollkeeper

---
+++ 2008/applications/doc/scrollkeeper/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import libtools
+
+def setup():
+    libtools.libtoolize("--force")
+    autotools.configure("--localstatedir=/var \
+                         --enable-static=no \
+                         --with-xml-catalog=/etc/xml/docbook \
+                         --with-partial-db-dir=scrollkeeper \
+                         --enable-nls")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    # It should be deleted because "pisi check scrollkeeper" gives error.
+    pisitools.remove("/var/log/scrollkeeper.log")
+
+    pisitools.dodoc("README", "AUTHORS", "ChangeLog", "NEWS")
