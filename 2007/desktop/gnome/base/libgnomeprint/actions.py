010c9499956bcfcaf095a7d04e6bc470d0d0f57c
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-05 17:25:23 +0000

    merge: version bump

--- 2007/desktop/gnome/base/libgnomeprint/actions.py
+++ 2007/desktop/gnome/base/libgnomeprint/actions.py
@@ -8,7 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-static=no --with-cups")
+    autotools.configure("--disable-static \
+                         --with-cups")
 
 def build():
     autotools.make()
