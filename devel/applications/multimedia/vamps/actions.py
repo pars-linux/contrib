6f4407e503f0cf0715dfb86b59a2a7cbe575b304
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-07 11:00:23 +0000

    ready

--- devel/applications/multimedia/vamps/actions.py
+++ devel/applications/multimedia/vamps/actions.py
@@ -5,9 +5,7 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def build():
     autotools.make()
