72349507566b2fe00f239645d0d9e6bd6c364114
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-07 11:47:21 +0000

    version bump

--- devel/desktop/kde/kdesvn/actions.py
+++ devel/desktop/kde/kdesvn/actions.py
@@ -4,19 +4,15 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 from pisi.actionsapi import cmaketools
-
-
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
 
 def setup():
     cmaketools.configure(installPrefix = "%s" % (get.kdeDIR()))
 
-
 def build():
     cmaketools.make()
-
 
 def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
