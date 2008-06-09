5bd7429c8cff5632be185e35923e698a7402452c
Eren Türkay <turkay.eren@gmail.com>
2008-06-09 15:07:34 +0000

    version bump

--- devel/desktop/gnome/base/libglademm/actions.py
+++ devel/desktop/gnome/base/libglademm/actions.py
@@ -8,13 +8,16 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-examples --disable-demos")
+    autotools.configure("--disable-examples \
+                         --disable-demos")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS")
+
     pisitools.removeDir("/usr/share/devhelp")
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS")
 
