fc1fde56514ea356bb84867cf5ea08629e2d1d35
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-10 14:51:50 +0000

    merge: version bump

---
+++ 2008/applications/network/dynagui/actions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+
+def install():
+    for data in ["dynagui.py", "libs", "img"]:
+        pisitools.insinto("/usr/share/dynagui", data)
+
+    pisitools.dodoc("ChangeLog", "COPYING")
