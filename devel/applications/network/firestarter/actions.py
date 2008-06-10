caf0cca4e71d108487707fb96e5aa5f7cb316a95
erenturkay <erenturkay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-10 15:54:22 +0000

    edit dependencies, don't update gconf schemas while installing

--- devel/applications/network/firestarter/actions.py
+++ devel/applications/network/firestarter/actions.py
@@ -8,7 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-static=no")
+    autotools.configure("--disable-static \
+                         --disable-schemas-install")
 
 def build():
     autotools.make()
