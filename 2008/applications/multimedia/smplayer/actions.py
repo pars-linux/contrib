a91a215696edf1e327250cbd115f70248825efcd
Tuxedup <Tuxedup@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-19 23:13:00 +0000

    merge for 2008

---
+++ 2008/applications/multimedia/smplayer/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+
+WorkDir = "smplayer-0.6.1"
+
+def build():
+    autotools.make("QMAKE=qmake-qt4 PREFIX=/usr")
+
+def install():
+    autotools.rawInstall("PREFIX=/usr DESTDIR=%s DOC_PATH=/usr/share/doc/%s" % (get.installDIR(),get.srcTAG()))
