69e21faad9bbfd5baddf9a12a636362cfd4e19c9
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-19 15:36:59 +0000

    version bump

--- devel/applications/multimedia/gimmix/actions.py
+++ devel/applications/multimedia/gimmix/actions.py
@@ -4,10 +4,13 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import libtools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
+    libtools.libtoolize("--copy --force")
+    autotools.autoreconf()
     autotools.configure("--disable-static")
 
 def build():
