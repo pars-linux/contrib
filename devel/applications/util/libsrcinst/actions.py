92b1f96d5b80408851da216cda58d06d21ea262a
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-06-26 15:34:18 +0000

    bump

--- devel/applications/util/libsrcinst/actions.py
+++ devel/applications/util/libsrcinst/actions.py
@@ -4,8 +4,11 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import get
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+
+WorkDir="libsrcinst-2.5"
 
 def setup():
     autotools.configure("--disable-static")
