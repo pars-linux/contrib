19f4fe0e99b38483746315a25c857ce536291bf6
Uğur Çetin <ugur@ugurcetin.com.tr>
2008-06-28 05:56:46 +0000

    merge ksmotthdock

---
+++ 2008/desktop/kde/ksmoothdock/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+
+WorkDir="ksmoothdock"
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+
