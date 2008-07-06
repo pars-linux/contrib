a69b51e114faf8494de3e1d44d3d32439615d035
Gokmen Gorgen <gkmngrgn@gmail.com>
2008-07-06 16:20:22 +0000

    merge from review

---
+++ devel/applications/util/hardinfo/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dosym("/usr/share/hardinfo/pixmaps/logo.png", "/usr/share/pixmaps/hardinfo.png")
+
+    pisitools.dodoc("LICENSE")
