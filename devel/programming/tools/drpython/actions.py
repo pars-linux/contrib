0d75220c8be27dcf7e4e3ec3b89e1c48f078392f
Fatih Aşıcı <fatih-asici@users.noreply.github.com>
2008-07-12 20:34:29 +0000

    bump

--- devel/programming/tools/drpython/actions.py
+++ devel/programming/tools/drpython/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007 TUBITAK/UEKAE
+# Copyright 2007,2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -13,18 +13,27 @@ from pisi.actionsapi import get
 WorkDir = "drpython"
 drpython_dir = "/usr/lib/%s/site-packages/drpython" % get.curPYTHON()
 
+def fixfiles(d):
+    import os
+    for root, dirs, files in os.walk(d):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
+            if name == ".cvsignore":
+                try:
+                    os.unlink(os.path.join(root,name))
+                except:
+                    pass
+
 def setup():
+    fixfiles(".")
     pisitools.dosed("drpython.lin", r"\${0\%/\*}", drpython_dir)
 
 def install():
     pythonmodules.install()
 
-    pisitools.dosym("%s/drpython.lin" % drpython_dir, "/usr/bin/drpython")
+    pisitools.rename("/usr/bin/drpython.lin", "drpython")
     pisitools.dosym("%s/bitmaps/drpython.png" % drpython_dir, "/usr/share/pixmaps/drpython.png")
 
-    shelltools.chmod("%s%s/drpython.lin" % (get.installDIR(), drpython_dir))
-
     pisitools.dodoc("*.txt")
-
-    pisitools.remove("/usr/bin/postinst.py")
-    pisitools.remove("%s/postinst.py" % drpython_dir)
