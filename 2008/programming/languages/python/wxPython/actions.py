1d436012b234ad7865a01459a75a336a4af622d5
Jérôme Schneider <mail@jeromeschneider.fr>
2008-07-12 13:47:23 +0000

    merge wxPython

---
+++ 2008/programming/languages/python/wxPython/actions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
+
+WorkDir = "wxPython-src-%s/wxPython" % get.srcVERSION()
+
+def install():
+    pythonmodules.fixCompiledPy()
+
+    pisitools.dodoc("docs/*", "docs/screenshots")
