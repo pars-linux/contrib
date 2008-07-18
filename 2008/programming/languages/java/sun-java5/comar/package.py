3272d858dfe5b061b0488bf3a00b744a714de600
havan <havan@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-07-18 12:12:44 +0000

    merge sun-java5 to 2008

---
+++ 2008/programming/languages/java/sun-java5/comar/package.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import os
+
+def postInstall():
+
+        # Create files used as storage for system preferences.
+        PREFS_LOCATION = "/opt/sun-java5-jdk/jre"
+
+        try:
+            os.makedirs("%s/.systemPrefs" % PREFS_LOCATION)
+        except OSError:
+            pass
+
+        if not os.path.exists("%s/.systemPrefs/.system.lock" % PREFS_LOCATION):
+                os.system("/usr/bin/touch %s/.systemPrefs/.system.lock" % PREFS_LOCATION)
+                os.system("/usr/bin/chmod 644 %s/.systemPrefs/.system.lock" % PREFS_LOCATION)
+
+        if not os.path.exists("%s/.systemPrefs/.systemRootModFile" % PREFS_LOCATION):
+                os.system("/usr/bin/touch %s/.systemPrefs/.systemRootModFile" % PREFS_LOCATION)
+                os.system("/usr/bin/chmod 644 %s/.systemPrefs/.systemRootModFile" % PREFS_LOCATION)
