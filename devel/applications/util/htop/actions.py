660c9d4daa465b7fc8cb5ceb084a4d711f1c2472
fatih <fatih@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-07-25 17:52:42 +0000

    bump

--- devel/applications/util/htop/actions.py
+++ devel/applications/util/htop/actions.py
@@ -10,7 +10,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--enable-taskstats \
+                         --enable-unicode")
 
 def build():
     autotools.make("-j1")
@@ -18,5 +19,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.doman("htop.1")
     pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README")
