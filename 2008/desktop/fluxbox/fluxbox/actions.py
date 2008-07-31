628d0a746b3c50246b2052342bcdbd6e88c71126
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-07-31 18:46:11 +0000

    merge

--- 2008/desktop/fluxbox/fluxbox/actions.py
+++ 2008/desktop/fluxbox/fluxbox/actions.py
@@ -4,15 +4,16 @@
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.system("sh autogen.sh")
     autotools.configure("--disable-static \
                          --enable-imlib2 \
                          --enable-xinerama \
-                         --enable-trutype \
                          --enable-xft \
                          --enable-nls")
 
@@ -20,10 +21,9 @@ def build():
     autotools.make()
 
 def install():
+    pisitools.dosed("data/init", "bloe", "Solaris")
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    # Default configurations
-    for conf in ["data/keys", "data/init", "data/menu"]:
-        pisitools.insinto("/etc/fluxbox", conf)
+    pisitools.insinto("/usr/share/fluxbox", "data/pixmaps")
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
