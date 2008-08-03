7b11abe45c7e4a69b0eae6d81834cd90ead0a9e8
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-08-03 16:31:05 +0000

    new hugin dep

---
+++ 2008/programming/languages/perl/perl-Image-ExifTool/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
+
+def setup():
+    perlmodules.configure()
+
+def install():
+    perlmodules.install()
+
+    # add html and doc's
+    pisitools.dohtml("html/*")
+    pisitools.dodoc("README","Changes")
