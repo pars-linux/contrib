d896b51c17629abedd591debe575298b245cb454
nihat ciddi <nak0f@users.noreply.github.com>
2008-08-18 18:31:44 +0000

    added ifplugd

---
+++ devel/applications/network/ifplugd/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-xmltoman --disable-subversion")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    # remove init script
+    pisitools.remove("/etc/init.d/ifplugd")
+
+    pisitools.dodir("/etc/ifplugd")
+    pisitools.dodoc("doc/README", "doc/SUPPORTED_DRIVERS")
+    pisitools.dohtml("*.html")
+    pisitools.dohtml("*.css")
