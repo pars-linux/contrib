98b4fedb66c651d2e4e3f11d2367c0833689059b
Gokmen Gorgen <gkmngrgn@gmail.com>
2008-09-01 23:20:25 +0000

    merged from contrib-devel

---
+++ 2008/applications/multimedia/rawstudio/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+WorkDir = "rawstudio-1.0rc1"
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README", "TODO")
