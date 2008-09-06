f736131e38d925190c5ab82fb80455f431bb6fd2
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-09-06 10:28:29 +0000

    merged from contrib-devel

---
+++ 2008/programming/libs/webkit-gtk/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import libtools
+from pisi.actionsapi import get
+
+WorkDir = "WebKit-%s" % get.srcVERSION().replace('0.0_', 'r')
+
+def setup():
+    libtools.libtoolize("--force")
+    autotools.autoreconf("-fi")
+
+    autotools.configure("--with-http-backend=curl \
+                         --with-font-backend=pango")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("WebKit/LICENSE", "WebCore/LICENSE-*", "SunSpider/TODO", "JavaScriptCore/AUTHORS")
