c30d852993214dde65308b3b4512bb48692b5f0d
sdalgic <sdalgic@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-09-10 01:31:56 +0000

    Version bump, remove system.base dependencies and add documentation.

--- devel/programming/languages/python/pyfits/actions.py
+++ devel/programming/languages/python/pyfits/actions.py
@@ -5,9 +5,10 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-
-WorkDir = get.srcNAME()
 
 def install():
     pythonmodules.install()
+
+    pisitools.dodoc("docs/*")
