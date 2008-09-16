744936144db6633348159065ea930736eb99d1c9
Serdar Dalgıç <sd@serdardalgic.org>
2008-09-16 23:27:00 +0000

    merge to contrib-2008

---
+++ 2008/applications/office/abiword/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--with-x \
+                                  --with-ImageMagick \
+                                  --with-libxml2 \
+                                  --with-zlib \
+                                  --with-libpng \
+                                  --with-popt \
+                                  --with-fribidi \
+                                  --enable-scripting \
+                                  --enable-printing")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    
+    pisitools.dodoc("docs/Abi*")
