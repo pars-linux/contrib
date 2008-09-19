92434ef35cb1d814927b6d98481e575af3df66c3
sdalgic <sdalgic@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-09-19 10:15:20 +0000

    Version bump, remove system.base dependency, add examples and package takeover.

--- devel/programming/languages/python/pyserial/actions.py
+++ devel/programming/languages/python/pyserial/actions.py
@@ -5,6 +5,17 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+examples = "%s/%s/examples" % (get.docDIR(), get.srcTAG())
+
+def setup():
+    shelltools.chmod("examples/*", 0644)
 
 def install():
     pythonmodules.install()
+    pisitools.insinto(examples, "examples/*")
+
+    pisitools.dodoc("CHANGES.txt", "LICENSE.txt", "README.txt")
