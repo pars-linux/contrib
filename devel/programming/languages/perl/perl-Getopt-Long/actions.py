8f6df7e4820c051f94f5360fe70847f3116a443d
Serdar Dalgıç <sd@serdardalgic.org>
2008-09-20 16:50:59 +0000

    remove <Path fileType=data>/usr/share/perl</Path>, add examples and package takeover

--- devel/programming/languages/perl/perl-Getopt-Long/actions.py
+++ devel/programming/languages/perl/perl-Getopt-Long/actions.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import perlmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
 
 WorkDir = "Getopt-Long-%s" % get.srcVERSION()
+examples = "%s/%s/examples" % (get.docDIR(), get.srcTAG())
 
 def setup():
     perlmodules.configure()
+    shelltools.chmod("examples/*", 0644)
 
 def build():
     perlmodules.make()
@@ -20,4 +22,5 @@ def build():
 def install():
     perlmodules.install()
 
+    pisitools.insinto(examples, "examples/*")
     pisitools.dodoc("CHANGES", "README")
