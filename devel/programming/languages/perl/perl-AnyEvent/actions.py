b818958902e28ba00e89334d66180720cc735e9f
Serdar Dalgıç <sd@serdardalgic.org>
2008-09-20 14:06:39 +0000

    Version bump and package takeover.

--- devel/programming/languages/perl/perl-AnyEvent/actions.py
+++ devel/programming/languages/perl/perl-AnyEvent/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -20,4 +19,4 @@ def build():
 def install():
     perlmodules.install()
 
-    pisitools.dodoc("Changes", "README")
+    pisitools.dodoc("Changes", "COPYING", "README")
