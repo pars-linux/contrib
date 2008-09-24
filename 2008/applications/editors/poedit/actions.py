b2360cde4e3069e2ca943c0c2c689ad05e295600
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-09-24 21:44:14 +0000

    merged from contrib-devel

--- 2008/applications/editors/poedit/actions.py
+++ 2008/applications/editors/poedit/actions.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-#from pisi.actionsapi import get
 
 def setup():
     autotools.configure()
-#    autotools.rawConfigure("--with-wx-config=/usr/bin/wx-config-2.8 --prefix %s/usr" % get.installDIR())
 
 def build():
     autotools.make()
