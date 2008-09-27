8480f75f24900a659c074106560f7f4c373138f7
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-09-27 14:09:52 +0000

    lineakd: gcc4 patch, disable static, add docs and version bump

--- devel/applications/hardware/lineakd/actions.py
+++ devel/applications/hardware/lineakd/actions.py
@@ -5,13 +5,14 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README", "TODO")
