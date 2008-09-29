1f008c10a01b843e4e118146f3d1c6f483e17aa5
mehmet nur olcay <mnurolcay_at_gmail.com>
2008-09-29 02:05:28 +0000

    merge from contrib-devel

---
+++ 2008/applications/editors/texmaker/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def setup():
+    shelltools.system("qmake-qt4 PREFIX=/usr")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
+
+    pisitools.remove("/usr/share/texmaker/AUTHORS")
+    pisitools.remove("/usr/share/texmaker/CHANGELOG.txt")
+    pisitools.remove("/usr/share/texmaker/COPYING")
+    pisitools.remove("/usr/share/pixmaps/texmaker.ico")
+
+    pisitools.dodoc("utilities/AUTHORS","utilities/COPYING","utilities/CHANGELOG.txt")
+
+    pisitools.domove("/usr/share/texmaker/texmaker.desktop","/usr/share/applications")
