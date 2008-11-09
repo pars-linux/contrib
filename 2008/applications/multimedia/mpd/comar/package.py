97458b5e223dac2f3b12ad99df53e6200075b7df
Gokmen Gorgen <gkmngrgn@gmail.com>
2008-11-09 15:13:59 +0000

    merge from contrib-devel

--- 2008/applications/multimedia/mpd/comar/package.py
+++ 2008/applications/multimedia/mpd/comar/package.py
@@ -4,7 +4,10 @@
 import os
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    for d in ["/var/lib/mpd", "/var/lib/mpd/playlists", "/var/log/mpd"]:
+    for d in ["/var/lib/mpd", "/var/lib/mpd/playlists", "/var/log/mpd", "/var/run/mpd"]:
         if not os.path.exists(d):
             os.mkdir(d)
+            # $ id nobody
+            # uid=65534(nobody) gid=65534(nobody) gruplar=65534(nobody)
+            os.chown(d, 65534, 65534)
 
