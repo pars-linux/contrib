a92e21545d1a74dc2630fd7f2aa572c82db3c2af
igungor <igungor@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-11-15 20:43:58 +0000

    copy from devel: character based handwriting input panel

---
+++ 2008/applications/util/cellwriter/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+
+def setup():
+    autotools.configure("--with-x \
+                         --without-gnome")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README", "TODO")
