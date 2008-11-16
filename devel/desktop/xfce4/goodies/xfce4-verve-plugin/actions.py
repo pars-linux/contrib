9b104c39601d890ca0f2572c2a75cf4a8fc1a58c
Gokmen Gorgen <gkmngrgn@gmail.com>
2008-11-16 16:30:05 +0000

    add workdir and fix paths

--- devel/desktop/xfce4/goodies/xfce4-verve-plugin/actions.py
+++ devel/desktop/xfce4/goodies/xfce4-verve-plugin/actions.py
@@ -8,6 +8,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir = "verve-plugin-%s" % get.srcVERSION()
+
 def setup():
     autotools.configure()
 
