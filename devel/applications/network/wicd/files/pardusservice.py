4cef5b0dd531365718084731b890a177e8982480
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-12-10 03:32:20 +0000

    take lang variable from os.getenv and edit message

--- devel/applications/network/wicd/files/pardusservice.py
+++ devel/applications/network/wicd/files/pardusservice.py
@@ -4,15 +4,14 @@
 # Author: Gökmen Görgen, <gkmngrgn@gmail.com>
 
 import pygtk, gtk
+import os
 
 def main(widget = None):
-    lang = file("/etc/mudur/language").read().rstrip("\n")
-
-    if lang == "tr":
+    if os.getenv("LANG").startswith("tr"):
         msg = "Wicd servisi şuan etkin değil! Servis Yöneticisi'nden\nWicd ağ yöneticisini çalıştırın ve tekrar deneyin."
 
     else:
-        msg = "Wicd daemon isn't active! Please run Wicd\nconnection manager from Service Manager and try again."
+        msg = "Service Wicd is not started! Please run Wicd connection\nmanager from Service Manager and try again."
 
     warning = gtk.MessageDialog(None,
                                 gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
