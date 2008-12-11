b6ecbadc17aa2d740f8feaa3c09ed295a5e1b14e
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2008-12-11 18:06:18 +0000

    Version bump, many new features, including i18n support and Turkish translation.

--- 2007/applications/games/tuxmath/actions.py
+++ 2007/applications/games/tuxmath/actions.py
@@ -7,24 +7,16 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-import os
 
-WorkDir="tuxmath"
+WorkDir='tuxmath_w_fonts-%s' % get.srcVERSION()
 
 def setup():
-    pisitools.dosed("Makefile", "INSTALL_DIR=", "INSTALL_DIR=%s" % get.installDIR())
+    autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
-    pisitools.dodir("/usr/bin")
-
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("docs/*.txt")
-
-    # Remove CVS dirs in installDir
-    for root, dirs, files in os.walk(get.installDIR()):
-        if "CVS" in root:
-            pisitools.removeDir(root.split("install")[1])
+    pisitools.dodoc("docs/COPYING.txt", "docs/TODO.txt", "docs/README*", "docs/OFL.txt", "docs/changelog")
