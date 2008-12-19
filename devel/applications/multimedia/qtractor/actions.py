f5787fa6c4dfffc176f7a3b407ba380760f3b9b7
mnurolcay <mnurolcay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-12-19 18:19:40 +0000

    An audio/MIDI multi-track sequencer application

---
+++ devel/applications/multimedia/qtractor/actions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+flags="%s \
+      -msse \
+      -mfpmath=sse \
+      -ffast-math" % get.CXXFLAGS()
+
+def setup():
+    autotools.configure("--with-qt=/usr/qt/4 \
+                         --disable-librubberband \
+                         --disable-vst \
+                         --enable-ladspa \
+                         --enable-dssi ")
+
+def build():
+    autotools.make('CXXFLAGS="%s" \
+                    CXX="%s"' % (flags, get.CXX()))
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "TODO", "README", "README.VST")
