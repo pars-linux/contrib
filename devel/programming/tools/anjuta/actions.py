52845b542cd712b6fff3d8fc3bc89eceb207dbad
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-12-21 19:51:11 +0000

    version bump and add scintilla & devhelp supports.

--- devel/programming/tools/anjuta/actions.py
+++ devel/programming/tools/anjuta/actions.py
@@ -6,16 +6,17 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-plugin-devhelp \
+    autotools.configure("--enable-plugin-scintilla \
                          --disable-scrollkeeper ")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("-j1 GCONF_DISABLE_MAKEFILE_SCHEMA_INSTALL=1 DESTDIR=%s" % get.installDIR())
 
     # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
