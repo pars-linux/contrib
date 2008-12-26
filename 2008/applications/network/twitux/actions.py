94876228a08c8e206f4a13324e5740f257c8b759
mnurolcay <mnurolcay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2008-12-26 16:48:50 +0000

    merge from devel (new version with translations.xml, desktop patch and minor fixes)

--- 2008/applications/network/twitux/actions.py
+++ 2008/applications/network/twitux/actions.py
@@ -4,17 +4,24 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    shelltools.export("CC", get.CC())
+
+    autotools.configure("--disable-static \
+                         --disable-spell \
+                         --enable-nls")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dosym("/usr/share/icons/hicolor/48x48/apps/twitux.png", "/usr/share/pixmaps/twitux.png")
 
     pisitools.dodoc("AUTHORS", "NEWS", "README", "TODO", "ChangeLog", "COPYING")
