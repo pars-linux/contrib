466b4e1401f0239f501ef890ae612a25cc922ceb
Uğur Çetin <ugur@ugurcetin.com.tr>
2009-01-04 06:46:29 +0000

    boncuk: merge to 2007

--- 2007/applications/dictionaries/boncuk/actions.py
+++ 2007/applications/dictionaries/boncuk/actions.py
@@ -10,18 +10,18 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 
 def setup():
-    shelltools.system("qmake-qt4")
+    shelltools.system("lrelease-qt4 boncuk.pro")
+    shelltools.system("qmake-qt4 boncuk.pro")
 
+def build():
     # Change C/XXFLAGS
     pisitools.dosed("Makefile", "CFLAGS        = -pipe -O2", "CFLAGS        = %s" % get.CFLAGS())
     pisitools.dosed("Makefile", "CXXFLAGS      = -pipe -O2", "CXXFLAGS      = %s" % get.CXXFLAGS())
 
-def build():
-    shelltools.system("lrelease-qt4 boncuk.pro")
     autotools.make()
 
 def install():
     pisitools.dobin("bin/boncuk")
     pisitools.insinto("/usr/share/pixmaps", "resources/boncuk.png")
 
-    pisitools.dodoc("AUTHORS", "COPYING", "README", "TODO")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README", "TODO")
