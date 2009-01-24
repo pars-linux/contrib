2e0630612b6358730ee4a2ad947c8a13d31525a9
inanc <inanc@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-01-24 18:34:27 +0000

    Version bump

--- devel/applications/hardware/libsynaptics/actions.py
+++ devel/applications/hardware/libsynaptics/actions.py
@@ -6,13 +6,16 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("ChangeLog", "COPYING", "README", "TODO")
