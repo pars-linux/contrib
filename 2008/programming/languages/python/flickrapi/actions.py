33ed1dcdb637f71ee28a08ba8af8f6404ec27523
MeW <MeW@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-02-03 00:52:16 +0000

    merge sandbox fix

--- 2008/programming/languages/python/flickrapi/actions.py
+++ 2008/programming/languages/python/flickrapi/actions.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007 TUBITAK/UEKAE
+# Copyright 2007-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def build():
+    pythonmodules.compile()
 
 def install():
     pythonmodules.install()
 
     shelltools.system('doxygen doc/GNUmakefile')
-    pisitools.dohtml('doc/html/*')
-    pisitools.dodoc('documentation.rst', 'LICENSE', 'README', 'UPGRADING')
+    pisitools.dohtml('html/*')
+    pisitools.dodoc('LICENSE', 'README', 'UPGRADING')
 
+    pisitools.removeDir("%s/flickrapi-%s" % (get.docDIR(), get.srcVERSION()))
