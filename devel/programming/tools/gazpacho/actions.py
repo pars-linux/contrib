8c13bffbdf3843d64c99a2cb26193a2cd5a51d68
Serdar Dalgıç <sd@serdardalgic.org>
2009-02-06 20:18:31 +0000

    fix typo, add and reorganize documentation parts.

--- devel/programming/tools/gazpacho/actions.py
+++ devel/programming/tools/gazpacho/actions.py
@@ -5,7 +5,12 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import pisitools
 
 def install():
     pythonmodules.install()
-#   pisitools.rename("/usr/share/doc/gazpacho", get.srcTAG())
+
+    pisitools.domove("/usr/share/doc/gazpacho/*","/usr/share/doc/gazpacho-0.7.2-4")
+    #gazpacho setup.py automatically installs docs into this folder so removeDir..
+    pisitools.removeDir("/usr/share/doc/gazpacho")
+    pisitools.dodoc("doc/*")
