53cafe8f90011505da307c2e396c4fbdee4ecad9
kenp <kenp@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-02-11 00:20:10 +0000

    Update to 2.0.0-beta2

--- devel/desktop/kde4/krusader4/actions.py
+++ devel/desktop/kde4/krusader4/actions.py
@@ -19,4 +19,4 @@ def build():
 def install():
     cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("ChangeLog", "COPYING", "NEWS", "FAQ")
+    pisitools.dodoc("ChangeLog", "COPYING", "README", "FAQ")
