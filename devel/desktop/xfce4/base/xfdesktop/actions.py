cb132bddba7a9455589accef0f451803f567718f
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-02-15 01:28:46 +0000

    version bump for new xfce4.6

--- devel/desktop/xfce4/base/xfdesktop/actions.py
+++ devel/desktop/xfce4/base/xfdesktop/actions.py
@@ -9,15 +9,19 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --enable-desktop-icons \
+                         --enable-thunarx \
+                         --enable-file-cons \
+                         --enable-exo \
+                         --enable-desktop-menu \
+                         --enable-desktop-menu-dir-monitor \
+                         --enable-panel-plugin")
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    # conflict
-    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
 
     pisitools.dodoc("TODO", "README", "NEWS", "ChangeLog", "AUTHORS")
