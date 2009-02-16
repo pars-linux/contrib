10a1b51bf8703b67ff23e8a1d235f3472303b8a3
Ertuğrul Erata <ertugrulerata@gmail.com>
2009-02-16 06:21:14 +0000

    fix doc path.

--- devel/applications/admin/fish/actions.py
+++ devel/applications/admin/fish/actions.py
@@ -6,15 +6,13 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
-from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--without-xsel")
+    autotools.configure("--without-xsel \
+                        docdir=/usr/share/doc/%s" % get.srcTAG())
 
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    
-    pisitools.domove("/usr/share/doc/fish/*", "/usr/share/doc/%s/html/" % get.srcTAG())
