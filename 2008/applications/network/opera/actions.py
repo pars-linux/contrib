0c2957ea0baef2ce3ae4ec1bf680f44518ab32dc
Serdar Dalgıç <sd@serdardalgic.org>
2009-03-03 19:01:32 +0000

    merge changes to contrib-2008 repository ;)

--- 2008/applications/network/opera/actions.py
+++ 2008/applications/network/opera/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2008 TUBITAK/UEKAE
+# Copyright 2006-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="opera-9.63-2474.gcc4-shared-qt3.i386"
+WorkDir="opera-9.64-2480.gcc4-shared-qt3.i386"
 
 def install():
     shelltools.system("./install.sh DESTDIR=%s" % get.installDIR())
