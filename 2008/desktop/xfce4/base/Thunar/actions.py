c33f0415d2856ff2d55f4be0fd6630c922974ae7
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-03-12 18:22:27 +0000

    stable version bump.

--- 2008/desktop/xfce4/base/Thunar/actions.py
+++ 2008/desktop/xfce4/base/Thunar/actions.py
@@ -18,7 +18,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    # conflict
-    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
-
     pisitools.dodoc("TODO", "THANKS", "README", "HACKING", "FAQ", "AUTHORS", "ChangeLog")
