5471bb538b0858eaf93ed20b22b32af2649aeed9
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-03-13 10:56:18 +0000

    reverted. pyxdg is required for this new version.

--- 2008/applications/multimedia/jokosher/actions.py
+++ 2008/applications/multimedia/jokosher/actions.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# Copyright 2008 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
-
-def setup():
-    pythonmodules.compile()
+from pisi.actionsapi import get
 
 def install():
     pythonmodules.install()
 
     pisitools.removeDir("/usr/share/gnome")
 
-    pisitools.dodoc("AUTHORS", "COPYING", "README")
+    pisitools.dodoc("AUTHORS", "COPYING", "PKG-INFO", "README")
