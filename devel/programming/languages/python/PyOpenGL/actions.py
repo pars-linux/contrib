0fe57dd7456fc5e4c3d11c8917c7289246441ba5
Serdar Dalgıç <sd@serdardalgic.org>
2009-03-15 12:47:50 +0000

    * Package takeover. * Reorganize the dependencies: gle and numpy added, glut is removed as it's gle's dependency and setuptools is only needed for building the package. * Version bump before the final release.

--- devel/programming/languages/python/PyOpenGL/actions.py
+++ devel/programming/languages/python/PyOpenGL/actions.py
@@ -8,9 +8,8 @@ from pisi.actionsapi import get
 from pisi.actionsapi import pythonmodules
 from pisi.actionsapi import pisitools
 
-WorkDir = "%s-3.0.0b6" % get.srcNAME()
+WorkDir = "%s-3.0.0c1" % get.srcNAME()
 
 def install():
     pythonmodules.install()
 
-    pisitools.dohtml("documentation/*")
