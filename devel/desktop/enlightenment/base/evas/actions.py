16d347724ec9693d532e0af2ae35a3904ac5bdfb
igungor <igungor@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-03-20 16:43:00 +0000

    new snapshot, remove non-existing or unnecessary parameters

--- devel/desktop/enlightenment/base/evas/actions.py
+++ devel/desktop/enlightenment/base/evas/actions.py
@@ -6,64 +6,22 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+WorkDir = "evas"
+
 def setup():
-    autotools.configure("--enable-fontconfig \
-                         --enable-software-x11 \
-                         --enable-software-xcb \
-                         --enable-directfb \
-                         --enable-fb \
+    shelltools.system("./autogen.sh")
+    autotools.configure("--enable-gl-x11 \
                          --enable-buffer \
-                         --enable-software-qtopia \
-                         --enable-gl-x11 \
+                         --enable-software-x11 \
                          --enable-xrender-x11 \
-                         --enable-xrender-xcb \
-                         --enable-image-loader-gif \
-                         --enable-image-loader-png \
-                         --enable-image-loader-jpeg \
-                         --enable-image-loader-eet \
-                         --enable-font-loader-eet \
-                         --enable-image-loader-edb \
-                         --enable-image-loader-tiff \
-                         --enable-image-loader-xpm \
-                         --enable-image-loader-svg \
-                         --enable-cpu-mmx \
-                         --enable-cpu-sse \
-                         --enable-cpu-altivec \
-                         --enable-cpu-c \
-                         --enable-scale-sample \
-                         --enable-scale-smooth \
-                         --enable-convert-yuv \
-                         --enable-small-dither-mask \
-                         --enable-convert-8-rgb-332 \
-                         --enable-convert-8-rgb-666 \
-                         --enable-convert-8-rgb-232 \
-                         --enable-convert-8-rgb-222 \
-                         --enable-convert-8-rgb-221 \
-                         --enable-convert-8-rgb-121 \
-                         --enable-convert-8-rgb-111 \
-                         --enable-convert-16-rgb-565 \
-                         --enable-convert-16-rgb-555 \
-                         --enable-convert-16-rgb-444 \
-                         --enable-convert-16-rgb-ipq \
-                         --enable-convert-16-rgb-rot-0 \
-                         --enable-convert-16-rgb-rot-270 \
-                         --enable-convert-16-rgb-rot-90 \
-                         --enable-convert-24-rgb-888 \
-                         --enable-convert-24-bgr-888 \
-                         --enable-convert-32-rgb-8888 \
-                         --enable-convert-32-rgbx-8888 \
-                         --enable-convert-32-bgr-8888 \
-                         --enable-convert-32-bgrx-8888 \
-                         --enable-convert-32-rgb-rot-0 \
-                         --enable-convert-32-rgb-rot-270 \
-                         --enable-convert-32-rgb-rot-90 \
-                         --with-x \
-                         --with-eet-config=/usr/bin/eet-config \
-                         --with-edb-config=/usr/bin/edb-config \
+                         --enable-software-sdl \
+                         --enable-fb \
+                         --enable-directfb \
                          --with-qtdir=%s \
-                         --with-freetype \
+                         --with-x \
                          --disable-static" % get.qtDIR())
 
 def build():
@@ -71,5 +29,5 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    pisitools.dodoc("AUTHORS", "Changelog", "COPYING*", "README*")
 
+    pisitools.dodoc("AUTHORS", "COPYING*", "README")
