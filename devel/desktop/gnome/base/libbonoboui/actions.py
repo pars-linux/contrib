7cdec7b80c80cc11cf99a999439e587f2551a697
burak <burak@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-03-24 20:18:44 +0000

    - Version bump                 - Split into documents                 - Samples are removed

--- devel/desktop/gnome/base/libbonoboui/actions.py
+++ devel/desktop/gnome/base/libbonoboui/actions.py
@@ -10,11 +10,19 @@ from pisi.actionsapi import pisitools
 def setup():
     autotools.configure("--disable-static \
                          --with-x")
+    pisitools.dosed("libtool"," -shared "," -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
+
+    samples = ["/usr/lib/bonobo-2.0/samples/*",
+               "/usr/lib/bonobo/servers/*.server",
+               "/usr/share/gnome-2.0/ui/Bonobo_Sample_Hello.xml",
+               "/usr/share/gnome-2.0/ui/Bonobo_Sample_Container-ui.xml"]
+    for sample in samples:
+        pisitools.remove(sample)
 
     pisitools.dodoc("NEWS", "README", "ChangeLog", "INSTALL")
