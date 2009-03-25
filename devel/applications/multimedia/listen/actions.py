f141efa0195c9fab7790e8fbad0086d8d16a455b
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-03-25 17:44:39 +0000

    version bump.

--- devel/applications/multimedia/listen/actions.py
+++ devel/applications/multimedia/listen/actions.py
@@ -8,13 +8,15 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
+WorkDir="%s-%s" % (get.srcNAME(), get.srcVERSION().replace('_', ''))
+
 def build():
     autotools.make()
 
 def install():
     autotools.install("DESTDIR=%s" % get.installDIR())
 
-    pisitools.remove("/usr/bin/listen")
+#    pisitools.remove("/usr/bin/listen")
     pisitools.remove("/usr/share/man/man1/listen.1.gz")
 
     pisitools.dodoc("README", "TODO", "copy", "gpl.txt")
