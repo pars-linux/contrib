e65dfcfc5502eda61e70757194f485fca671ff32
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-03-26 11:30:58 +0000

    version bump.

--- devel/desktop/gnome/gtk2-engines-murrine/actions.py
+++ devel/desktop/gnome/gtk2-engines-murrine/actions.py
@@ -6,6 +6,9 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "murrine-%s" % get.srcVERSION()
 
 def setup():
     autotools.configure("--disable-static \
