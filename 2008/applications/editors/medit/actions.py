159d7d056115b6298d263af0ef484c3f721965b8
ceyhun.asilkazanci <ceyhun.asilkazanci@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-03-26 20:44:01 +0000

    adding medit to contrib

---
+++ 2008/applications/editors/medit/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.rawConfigure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("README" , "COPYING" , "NEWS" , "AUTHORS" , "LICENSE")
+    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
+    pisitools.remove("/usr/share/icons/hicolor/48x48/apps/medit.png")
