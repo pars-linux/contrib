f6aa7cea01859f86da08618cd6f1bc36ab6e507e
ceyhun.asilkazanci <ceyhun.asilkazanci@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-03-29 00:03:04 +0000

    change configure and remove directory

--- 2008/applications/editors/medit/actions.py
+++ 2008/applications/editors/medit/actions.py
@@ -6,16 +6,18 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
-    autotools.rawConfigure()
+    autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("README" , "COPYING" , "NEWS" , "AUTHORS" , "LICENSE")
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
     pisitools.remove("/usr/share/icons/hicolor/48x48/apps/medit.png")
+    pisitools.removeDir("/usr/lib/python2.5/")
