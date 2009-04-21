a85d581ab0d10bc52997068de31c713e41f11905
erenoglu <erenoglu@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-04-21 23:13:10 +0000

    crypto++ version bump to new contrib devel repo

--- 2008/devel/programming/libs/crypto++/actions.py
+++ 2008/devel/programming/libs/crypto++/actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-·
 #
+# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 3.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 #
@@ -26,9 +27,12 @@ def setup():
     #delete GNUmakefile as it's not necessary anymore and messing things up if it's there
     shelltools.unlink("%s/GNUmakefile" % get.workDIR())
 
+    #delete patended code
+    shelltools.unlink("%s/mars.*" % get.workDIR())
+    shelltools.unlink("%s/marss.*" % get.workDIR())
+
     #convert the DOS line endings of data files to unix type
     shelltools.system("dos2unix %s/TestVectors/*" % get.workDIR())
-    shelltools.system("dos2unix %s/*.dat" % get.workDIR())
 
     #create the configure script and configure to create only dynamic library
     for f in ["NEWS", "README", "AUTHORS", "ChangeLog"]:
@@ -41,29 +45,27 @@ def build():
     autotools.make()
 
 def check():
-    shelltools.system("./cryptestcwd v")
+    shelltools.system("./cryptest v")
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     #GNU/Linux distributions prefer crypto++ instead of cryptopp, so we do the same by renaming and moving
     #rename and symlink for the .la file
-    pisitools.dosym("libcrypto++.la", "/usr/lib/libcryptopp.la")
+    pisitools.dosym("libcryptopp.la", "/usr/lib/libcrypto++.la")
 
     #rename and symlink for the .so file
-    for file in ["libcryptopp.so", "libcryptopp.so.6", "libcryptopp.so.6.0.0"]:
-        pisitools.dosym("libcrypto++.so.6.0.0", "/usr/lib/%s" % file)
+    for file in ["libcrypto++.so", "libcrypto++.so.6", "libcrypto++.so.6.0.0"]:
+        pisitools.dosym("libcryptopp.so.6.0.0", "/usr/lib/%s" % file)
 
     #rename the include and data directories
     pisitools.rename("/usr/include/cryptopp", "crypto++")
-    pisitools.rename("/usr/share/cryptopp", "crypto++")
+#    pisitools.rename("/usr/share/cryptopp", "crypto++")
 
     #We include the TestVectors directory in package as examples for future coders
     pisitools.insinto(datadir, "TestVectors")
 
     #fix directory and file permissions so that they are readable by users
     fixperms("%s/%s" % (get.installDIR(),datadir))
-
-    pisitools.remove("/usr/bin/cryptestcwd")
 
     pisitools.dodoc("Readme*", "License*")
