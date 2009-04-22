0eff9de58cbd87b504a140e598fb6a6f29d43929
ert976 <ert976@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-04-22 14:08:38 +0000

    Version bump and modify pspec.xml, actions.py

--- 2008/devel/applications/multimedia/lives/actions.py
+++ 2008/devel/applications/multimedia/lives/actions.py
@@ -6,17 +6,16 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static\
+                                     --disable-rpath \
+                                     --enable-nls")
 
 def build():
     autotools.make()
 
 def install():
-    pisitools.dodir("/usr/share/pixmaps")
-    pisitools.dodir("/usr/share/applications")
 
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
