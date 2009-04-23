930ba0876622c32a94c04ad14960ce08bc12c55e
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-04-23 21:33:30 +0000

    copied from contrib/2008/devel

---
+++ 2008/stable/desktop/xfce4/base/xfprint/comar/package.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+def postInstall(fromVersion, fromRelease, toVersion, toRelease):
+    os.system("gtk-update-icon-cache -q -t -f /usr/share/icons/hicolor")
+
+def postRemove(fromVersion, fromRelease, toVersion, toRelease):
+    os.system("gtk-update-icon-cache -q -t -f /usr/share/icons/hicolor")
