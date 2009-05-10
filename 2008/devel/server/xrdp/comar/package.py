1b2698c3de1fb34106cea715255a1177fa1d8764
erenoglu <erenoglu@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-05-10 16:01:57 +0000

    correct location of xrdp

--- 2008/devel/server/xrdp/comar/package.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    if not os.path.exists("/etc/xrdp/rsakeys.ini"):
-        os.system("/usr/bin/xrdp-keygen xrdp /etc/xrdp/rsakeys.ini")
