49103a691323a85c231bab0ff52cde38ea5e2c3d
Onur Küçük <zaburt@users.noreply.github.com>
2009-06-03 19:10:12 +0000

    component programming/language/perl/ is ready

---
+++ 2009/devel/programming/language/perl/perl-XML-LibXML-Common/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import perlmodules
+from pisi.actionsapi import pisitools
+
+WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
+
+def setup():
+    perlmodules.configure()
+
+def build():
+    perlmodules.make()
+
+def install():
+    perlmodules.install()
+
+    pisitools.removeDir("/usr/lib/perl5/5.10.0")
+
+    pisitools.dodoc("README", "LICENSE", "Changes")
