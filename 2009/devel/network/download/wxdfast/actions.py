8f82b097c0a66457905896d52443e36b78a7c4c7
Onur Küçük <zaburt@users.noreply.github.com>
2009-06-03 19:10:07 +0000

    component network/download/ is ready

---
+++ 2009/devel/network/download/wxdfast/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007,2008 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    shelltools.export("WXRC", "/usr/bin/wxrc-2.8")
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog*", "COPYING", "README*", "TODO")
