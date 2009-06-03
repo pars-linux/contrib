f4560e8645a74e669a50fa46d9c641a23baf33a5
onur <onur@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-03 19:08:23 +0000

    component desktop/enlightenment/base/ is ready

---
+++ 2009/devel/desktop/enlightenment/base/enlightenment/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "e_20090404"
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --enable-nls \
+                         --disable-rpath \
+                         --with-x")
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "BUGS", "COPYING", "README")
