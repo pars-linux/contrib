3117ff9ce8c17e2ea0ad7f42a8b7d16f7c940583
Onur Küçük <zaburt@users.noreply.github.com>
2009-06-03 19:08:23 +0000

    component desktop/enlightenment/base/ is ready

---
+++ 2009/devel/desktop/enlightenment/base/evas/actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+WorkDir = "evas_20090404"
+
+def setup():
+    shelltools.touch("README")
+    autotools.autoreconf("-fi")
+    autotools.configure("--enable-gl-x11 \
+                         --enable-buffer \
+                         --enable-software-x11 \
+                         --enable-xrender-x11 \
+                         --enable-software-sdl \
+                         --enable-fb \
+                         --enable-directfb \
+                         --with-qtdir=%s \
+                         --with-x \
+                         --disable-static" % get.qtDIR())
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING*", "README")
