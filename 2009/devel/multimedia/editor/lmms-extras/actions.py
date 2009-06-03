ea5e5868ed3817f59a5901f40161927fbeadc231
Onur Küçük <zaburt@users.noreply.github.com>
2009-06-03 19:07:54 +0000

    component multimedia/editor/ is ready

---
+++ 2009/devel/multimedia/editor/lmms-extras/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def setup():
+    cmaketools.configure("-DCMAKE_INSTALL_PREFIX=/usr")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall('DESTDIR="%s"' % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "TODO", "README")
---
+++ 2009/devel/multimedia/editor/prokyon3/actions.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--with-x \
+                         --with-qtdir=%s \
+                         --with-qt-includes=%s/include \
+                         --with-qt-libs=%s \
+                         --with-taglib \
+                         --with-musicextras \
+                         --with-mixxx \
+                         --with-musicbrainz \
+                         --with-ogg \
+                         --with-mad \
+                         --with-id3 \
+                         --with-mysql --with-mysql-lib --with-mysql-include" % (get.qtDIR(), get.qtDIR(), get.qtLIBDIR()))
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto("/usr/share/applications", "prokyon3.desktop")
+    pisitools.insinto("/usr/share/pixmaps", "images/prokyon_logo.png", "prokyon.png")
+
+    pisitools.domove("usr/share/prokyon3/html", "%s/%s" % (get.docDIR(), get.srcTAG()))
+    pisitools.dodoc("ChangeLog", "AUTHORS", "COPYING", "NEWS", "README")
+
