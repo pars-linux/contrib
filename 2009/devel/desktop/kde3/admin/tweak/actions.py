4bd047eadab410a625f4438e4a287ec993ca3e1e
Onur Küçük <zaburt@users.noreply.github.com>
2009-06-03 19:08:01 +0000

    component desktop/kde3/admin/ is ready

---
+++ 2009/devel/desktop/kde3/admin/tweak/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import kde
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="tweaK-%s" % get.srcVERSION()
+
+def setup():
+    kde.configure()
+
+def build():
+    kde.make()
+
+def install():
+    kde.install()
+
+    pisitools.dodoc("COPYING", "AUTHORS", "ChangeLog")
