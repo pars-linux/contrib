12a75aaeaf2eea48b232838dc7e251fd83bb2fc8
Onur Küçük <zaburt@users.noreply.github.com>
2009-06-03 19:08:25 +0000

    component desktop/lookandfeel/ is ready

---
+++ 2009/devel/desktop/lookandfeel/dmz-xcursor/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Copyright 2007 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+WorkDir = "."
+
+def install():
+    pisitools.dodir("/usr/share/cursors/xorg-x11/DMZ/")
+    shelltools.copytree("Vanilla-DMZ/cursors/","%s/usr/share/cursors/xorg-x11/DMZ/cursors" % get.installDIR(), True)
