97f78f0d5d5fecfe7f7d0f83476666f4368bf33a
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-06-04 19:43:46 +0000

    fixed

--- 2009/devel/desktop/xfce/base/exo/actions.py
+++ 2009/devel/desktop/xfce/base/exo/actions.py
@@ -6,11 +6,17 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    shelltools.unlink("py-compile" )
+    shelltools.sym("/bin/true", "%s/py-compile" % get.curDIR())
+
     autotools.configure("--disable-static \
                          --enable-python")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
