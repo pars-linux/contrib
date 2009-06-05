1249fc8424267b3151bef1c80f94d8869fdbed11
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-05 00:11:02 +0000

    fixed

--- 2009/devel/desktop/xfce/base/libxfcegui4/comar/package.py
+++ 2009/devel/desktop/xfce/base/libxfcegui4/comar/package.py
@@ -5,3 +5,6 @@ from os import system as run
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
     run("gtk-update-icon-cache -q -t -f /usr/share/icons/hicolor")
+
+def preRemove():
+    run("gtk-update-icon-cache -q -t -f /usr/share/icons/hicolor")
