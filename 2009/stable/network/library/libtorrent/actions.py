3276d9c36e7bf219b13a5f2b8c643da0dcbbfc82
igungor <igungor@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-05 14:56:51 +0000

    ready for 2009

---
+++ 2009/stable/network/library/libtorrent/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "libtorrent-rasterbar-%s" % get.srcVERSION()
+
+def setup():
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static")
+
+def build():
+    autotools.make("CXX=%s" % get.CXX())
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto('/usr/include/libtorrent/asio/', 'include/libtorrent/asio/*')
+
+    pisitools.dohtml("docs/*")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
