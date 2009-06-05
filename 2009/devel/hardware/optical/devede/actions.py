4e6aa69504f192987bb6a4134b95944fa284f66a
ert976 <ert976@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-05 11:54:35 +0000

    devede for pardus 2009

--- 2009/devel/hardware/optical/devede/actions.py
+++ 2009/devel/hardware/optical/devede/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008 TUBITAK/UEKAE
+# Copyright 2008-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,4 +11,4 @@ from pisi.actionsapi import get
 WorkDir = "devede-%s" % get.srcVERSION()
 
 def install():
-    shelltools.system("sh install.sh prefix=/usr uninstall=no DESTDIR=%s" % get.installDIR())
+    shelltools.system("sh install.sh prefix=/usr uninstall=no targeted=yes DESTDIR=%s" % get.installDIR())
