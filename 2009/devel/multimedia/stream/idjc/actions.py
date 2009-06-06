4b81be2e1848216adf4de86148726792c126edaf
mnurolcay <mnurolcay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-06 16:15:24 +0000

    Version bump, fix docdir for 2009, add all direct dependencies Ready for 2009, will wait python-eyeD3

--- 2009/devel/multimedia/stream/idjc/actions.py
+++ 2009/devel/multimedia/stream/idjc/actions.py
@@ -4,19 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
+    #Fix docdir
+    pisitools.dosed("Makefile.in", "{PACKAGE_NAME}-\${PACKAGE_VERSION}", "{PACKAGE_NAME}")
+
     autotools.configure("--disable-static \
                          --enable-speex \
                          --enable-lame \
                          --enable-tooltips \
                          --enable-mad \
-                         --enable-ffmpeg \
-                         --docdir=/usr/share/doc/%s" % get.srcTAG())
+                         --enable-ffmpeg")
 
 def build():
     autotools.make()
