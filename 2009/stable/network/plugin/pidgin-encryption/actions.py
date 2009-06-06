17d7bbae36ea6c705cc1cdf9951beff790cc2e4c
mnurolcay <mnurolcay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-06 21:23:54 +0000

    merge

---
+++ 2009/stable/network/plugin/pidgin-encryption/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-static \
+                         --disable-rpath \
+                         --enable-nls")
+
+    # Put flags in front of the libs. Needed for --as-needed.
+    replace = (r"(\\\$deplibs) (\\\$compiler_flags)", r"\2 \1")
+    pisitools.dosed("libtool", *replace)
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("TODO", "README", "NOTES", "CHANGELOG", "COPYING","VERSION", "WISHLIST")
