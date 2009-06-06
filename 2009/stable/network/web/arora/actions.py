0675630d65dd6ef91bef3eda23ad75e66ce6dcb1
mehmet nur olcay <mnurolcay_at_gmail.com>
2009-06-06 17:00:47 +0000

    merge to 2009

---
+++ 2009/stable/network/web/arora/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.system("qmake-qt4 PREFIX=/usr")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
+
+    # Don't know, why package manager can't show the icon when there is a .svg
+    #pisitools.remove("/usr/share/icons/hicolor/scalable/apps/arora.svg")
+
+    pisitools.dodoc("AUTHORS", "README", "ChangeLog", "LICENSE*")
