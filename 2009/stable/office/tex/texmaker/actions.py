00e3c3f2147d9d9a33fbc6c7fa435cfddf265bf0
mehmet nur olcay <mnurolcay_at_gmail.com>
2009-06-06 22:03:13 +0000

    Merge to stable A pretty nice GUI and some cool changes: http://www.xm1math.net/texmaker/CHANGELOG.txt

---
+++ 2009/stable/office/tex/texmaker/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def setup():
+    shelltools.system("qmake-qt4 PREFIX=/usr")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
+
+    pisitools.remove("/usr/share/texmaker/AUTHORS")
+    pisitools.remove("/usr/share/texmaker/CHANGELOG.txt")
+    pisitools.remove("/usr/share/texmaker/COPYING")
+
+    pisitools.dodoc("utilities/AUTHORS", "utilities/COPYING", "utilities/CHANGELOG.txt")
