ec2674b47915ddc2a9c1ce9f1e39e86a51ab330a
mehmet nur olcay <mnurolcay_at_gmail.com>
2009-06-06 15:53:24 +0000

    Version bump, fix docdir for 2009

--- 2009/devel/multimedia/editor/python-eyeD3/actions.py
+++ 2009/devel/multimedia/editor/python-eyeD3/actions.py
@@ -4,6 +4,7 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pythonmodules
@@ -11,6 +12,9 @@ from pisi.actionsapi import pythonmodules
 WorkDir = "eyeD3-%s" % get.srcVERSION()
 
 def setup():
+    #Fix docdir
+    pisitools.dosed("Makefile.in", "doc/\${DIST_NAME}", "doc/python-eyeD3")
+
     autotools.configure()
 
 def build():
@@ -18,5 +22,3 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pythonmodules.fixCompiledPy()
