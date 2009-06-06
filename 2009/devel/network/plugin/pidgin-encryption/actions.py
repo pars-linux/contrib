cd7916514ead20761b7cfd90682e89bfdc86ea78
mehmet nur olcay <mnurolcay_at_gmail.com>
2009-06-06 21:13:46 +0000

    Fix unused direct dependencies, add nspr dependency

--- 2009/devel/network/plugin/pidgin-encryption/actions.py
+++ 2009/devel/network/plugin/pidgin-encryption/actions.py
@@ -4,17 +4,18 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 
 def setup():
-    shelltools.export("CC", get.CC())
-
     autotools.configure("--disable-static \
                          --disable-rpath \
                          --enable-nls")
+
+    # Put flags in front of the libs. Needed for --as-needed.
+    replace = (r"(\\\$deplibs) (\\\$compiler_flags)", r"\2 \1")
+    pisitools.dosed("libtool", *replace)
 
 def build():
     autotools.make()
