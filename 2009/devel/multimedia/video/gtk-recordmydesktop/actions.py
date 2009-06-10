80af3162b2ddad8b77c57f62496b844103cb269a
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-10 23:30:03 +0000

    fix for pardus 2009.

--- 2009/devel/multimedia/video/gtk-recordmydesktop/actions.py
+++ 2009/devel/multimedia/video/gtk-recordmydesktop/actions.py
@@ -6,8 +6,13 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
+    shelltools.unlink("py-compile" )
+    shelltools.sym("/bin/true", "%s/py-compile" % get.curDIR())
+
     autotools.configure()
 
 def build():
