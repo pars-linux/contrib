e0898e367d505a9601426b17d0c868ef72525f24
Tuxedup <Tuxedup@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-12 13:19:30 +0000

    version bump

--- 2009/devel/office/misc/homebank/actions.py
+++ 2009/devel/office/misc/homebank/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure()
+    autotools.configure("--without-ofx")
 
 def build():
     autotools.make()
