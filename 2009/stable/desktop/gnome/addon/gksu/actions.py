5ed7c723f3e4fa94f89b5c6585c424ada64234d5
burak <burak@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-13 21:43:32 +0000

    ready for 2009

---
+++ 2009/stable/desktop/gnome/addon/gksu/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2005  TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.autoreconf("-fiv")
+    autotools.configure("--disable-static \
+                         --disable-gtk-doc \
+                         --enable-nautilus-extension=no")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "README", "NEWS", "ChangeLog")
