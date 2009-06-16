5aa79feb1586461ddfaf8fafd75eb3cbb363c54e
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-16 17:28:21 +0000

    it works but not ready for Pardus 2009. There are undefined symbols in LibXML2, so it must wait to this error's solution.

--- 2009/devel/programming/language/python/gst-python/actions.py
+++ 2009/devel/programming/language/python/gst-python/actions.py
@@ -6,16 +6,18 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
 
 def setup():
+    shelltools.unlink("py-compile" )
+    shelltools.sym("/bin/true", "%s/py-compile" % get.curDIR())
+
     autotools.configure()
 
-def build():
-    autotools.make()
-
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=\"%s\"" % get.installDIR())
     pythonmodules.fixCompiledPy()
 
     pisitools.dodoc("AUTHORS","ChangeLog","COPYING","README","RELASE","TODO")
