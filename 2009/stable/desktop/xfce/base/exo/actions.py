e20945b62c8216a3d48d5070616cf6ffc4e81f8a
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-06-17 11:49:28 +0000

    is ready for Pardus 2009.

---
+++ 2009/stable/desktop/xfce/base/exo/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    shelltools.unlink("py-compile" )
+    shelltools.sym("/bin/true", "%s/py-compile" % get.curDIR())
+
+    autotools.configure("--disable-static \
+                         --enable-gtk-doc \
+                         --enable-python")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "HACKING", "NEWS", "README", "THANKS", "TODO")
