61e467cc66c24ee1a6656775d848fbed743c44a8
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-06-18 03:00:34 +0000

    is ready.

---
+++ 2009/stable/desktop/xfce/base/xfce4-session/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-static \
+                         --enable-gnome \
+                         --enable-libgnome-keyring \
+                         --enable-final \
+                         --enable-session-screenshots \
+                         --disable-legacy-sm")
+
+    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "BUGS", "ChangeLog*", "COPYING", "NEWS", "README", "TODO")
