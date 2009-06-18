6df521644757bf554977841c8f3e711616bceb73
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-06-18 19:59:57 +0000

    it will be later..

--- 2009/devel/desktop/xfce/base/xfmedia/actions.py
+++ 2009/devel/desktop/xfce/base/xfmedia/actions.py
@@ -8,8 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--enable-static=no \
-                         --with-taglib")
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
@@ -17,7 +16,6 @@ def build():
 def install():
     autotools.install()
 
-    # conflict
     pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
 
-    pisitools.dodoc("TODO", "README*", "NEWS", "ChangeLog", "AUTHORS")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README*", "TODO")
