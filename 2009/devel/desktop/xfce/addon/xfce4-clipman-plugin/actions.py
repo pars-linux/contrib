2f5daf31fe699848d5cc10695c3163a3a3f52782
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-19 12:27:21 +0000

    fix package.

--- 2009/devel/desktop/xfce/addon/xfce4-clipman-plugin/actions.py
+++ 2009/devel/desktop/xfce/addon/xfce4-clipman-plugin/actions.py
@@ -9,7 +9,9 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --enable-xsltproc \
+                         --enable-xml2po")
 
 def build():
     autotools.make()
@@ -17,4 +19,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "NEWS", "README", "ChangeLog")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
