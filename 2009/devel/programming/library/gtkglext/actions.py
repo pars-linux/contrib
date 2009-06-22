6bfc2603c61be0722ff5d8b5419e7f751b08bf58
mnurolcay <mnurolcay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-22 00:57:14 +0000

    Add pango support, fix linkage, add all direct dependencies

--- 2009/devel/programming/library/gtkglext/actions.py
+++ 2009/devel/programming/library/gtkglext/actions.py
@@ -8,7 +8,9 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--disable-glibtest \
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-static \
+                         --disable-glibtest \
                          --disable-gtktest \
                          --with-gdktarget=x11 \
                          --with-x \
