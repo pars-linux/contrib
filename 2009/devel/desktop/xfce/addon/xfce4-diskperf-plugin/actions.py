1302ad95a316c3a108d97c2846b1a202f11497cb
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-25 16:31:49 +0000

    fix package for pardus 2009.

--- 2009/devel/desktop/xfce/addon/xfce4-diskperf-plugin/actions.py
+++ 2009/devel/desktop/xfce/addon/xfce4-diskperf-plugin/actions.py
@@ -17,4 +17,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("README", "AUTHORS", "ChangeLog", "COPYING")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
