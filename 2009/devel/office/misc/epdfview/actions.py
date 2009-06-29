a96852bb0a14fee0e67f9d8e3260fcd2fe9faad4
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-06-29 14:18:04 +0000

    * Rewrite summary and description. * Add translation.xml. * Add missing docs. * Fix dependencies. * Remove default configuration parameters.

--- 2009/devel/office/misc/epdfview/actions.py
+++ 2009/devel/office/misc/epdfview/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2005-2007 TUBITAK/UEKAE
+# Copyright 2005-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -10,8 +10,11 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-rpath \
-                         --with-cups")
+    pisitools.dosed("data/epdfview.desktop",
+                    "Icon=icon_epdfview-48",
+                    "Icon=epdfview")
+
+    autotools.configure("--disable-rpath")
 
 def build():
     autotools.make()
@@ -19,7 +22,7 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dosym("/usr/share/epdfview/pixmaps/icon_epdfview-48.png", "/usr/share/pixmaps/epdfview.png")
+    pisitools.dosym("/usr/share/epdfview/pixmaps/icon_epdfview-48.png",
+                    "/usr/share/pixmaps/epdfview.png")
 
-    pisitools.dodoc("AUTHORS", "COPYING", "NEWS*", "README*")
-
+    pisitools.dodoc("ABOUT-NLS", "AUTHORS", "COPYING", "NEWS", "README", "THANKS")
