3af90a8b17e959964ea01470b2c3721b8259cb13
burak <burak@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-06-29 18:37:47 +0000

    ready for 2009

---
+++ 2009/stable/desktop/gnome/base/gdm/actions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fiv")
+    autotools.configure("--disable-scrollkeeper\
+                         --disable-static")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodir("/var/lib/gdm")
+
+    pisitools.domove("/usr/share/gdm/applications/","/usr/share")
+
+    remove = ["/usr/sbin/gdm","/usr/sbin/gdm-restart","/usr/sbin/gdm-safe-restart","/usr/sbin/gdm-stop",
+              "/usr/share/gdm/BuiltInSessions/*","/usr/share/xsessions/gnome.desktop",
+              "/usr/share/applications/gdmflexiserver.desktop"]
+    for r in remove:
+        pisitools.remove(r)
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
