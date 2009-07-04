b6cf3120a3d0d64e8b9c29873f55ba235e13ac27
mehmet nur olcay <mnurolcay_at_gmail.com>
2009-07-04 19:11:44 +0000

    *Bump to first KDE4 release, now using CMake *Fix dependencies, paths, change homepage *Add Description, translations.xml and desktop.patch  Ready for 2009

--- 2009/devel/util/archive/kbackup/actions.py
+++ 2009/devel/util/archive/kbackup/actions.py
@@ -4,13 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import kde
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
-    kde.configure()
+    cmaketools.configure(installPrefix="/usr/kde/4", sourceDir=".")
 
 def build():
-    kde.make("-j1")
+    cmaketools.make()
 
 def install():
-    kde.install()
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README", "TODO")
