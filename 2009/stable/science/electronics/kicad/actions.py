51a70fa133500ad46a629fb9f3d0782f11bc1895
mnurolcay <mnurolcay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-07-06 22:26:53 +0000

    KiCad is ready for 2009 but not fully functional for now, till kicad library/docs hit repos will be better BUG:COMMENT:6762

---
+++ 2009/stable/science/electronics/kicad/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+
+WorkDir = "kicad-%s" % get.srcVERSION()[4:]
+
+def setup():
+    cmaketools.configure(sourceDir=".")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.insinto("/usr/share/pixmaps", "bitmaps/icon_eeschema.xpm", "eeschema.xpm")
+
+    pisitools.dodoc("AUTHORS.txt", "CHANGELOG*", "TODO.txt", "version.txt")
