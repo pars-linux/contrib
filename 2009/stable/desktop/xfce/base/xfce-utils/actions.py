bed72ffddd4f5f999417b10173e9868003b366a2
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-07-13 10:08:42 +0000

    merge from contrib.2009.devel.

--- 2009/stable/desktop/xfce/base/xfce-utils/actions.py
+++ 2009/stable/desktop/xfce/base/xfce-utils/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--disable-static \
-                         --with-browser=firefox \
+                         --with-browser=midori \
                          --with-terminal=Terminal \
                          --with-vendor-info=Pardus")
 
