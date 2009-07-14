1c3274bdc429ba1f0476fddde3304b790c681a36
mehmet nur olcay <mnurolcay_at_gmail.com>
2009-07-14 09:23:47 +0000

    Kicad Docs and Libraries from review

---
+++ 2009/devel/science/electronics/kicad-library/actions.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import get
+from pisi.actionsapi import cmaketools
+
+WorkDir = "kicad-library-%s" % get.srcVERSION()[4:]
+
+def setup():
+    cmaketools.configure(sourceDir=".")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
