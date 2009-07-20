cc921965597aeb69c37efddac26e50f300d7f480
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-07-20 12:06:09 +0000

    knemo: bump to KDE4 version

--- 2009/devel/network/monitor/knemo/actions.py
+++ 2009/devel/network/monitor/knemo/actions.py
@@ -4,14 +4,14 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import kde
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
 
 def setup():
-    kde.configure()
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release -DCMAKE_INSTALL_PREFIX=%s" % get.kdeDIR())
 
 def build():
-    kde.make()
+    cmaketools.make()
 
 def install():
-    kde.install()
-
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
