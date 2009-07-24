84ac8ed93699fbdd8bdd598da4c9b589782e791f
igungor <igungor@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-07-24 13:00:26 +0000

    mpc is ready for 2009

---
+++ 2009/stable/multimedia/sound/ncmpc/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--enable-artist-screen \
+                         --enable-colors \
+                         --enable-lyrics-screen")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.removeDir("/usr/share/doc")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README", "doc/*.sample", "doc/ncmpc.lirc")
---
+++ 2009/stable/multimedia/sound/ncmpcpp/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--enable-unicode=yes \
+                         --enable-clock=yes \
+                         --with-taglib=yes \
+                         --with-curl=yes")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.removeDir("usr/share/doc")
+    pisitools.doman("doc/ncmpcpp.1")
+
+    pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "NEWS", "doc/config", "doc/keys")
