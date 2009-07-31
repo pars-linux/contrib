e5bbe7d389b5f54bbf76122da715f4897220aa49
jnmbk <jnmbk@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-07-31 13:09:54 +0000

    kmess: version bump to KDE4 port, waiting for qca2 fix to merge\nBUG:COMMENT:10730

--- 2009/devel/network/chat/kmess/actions.py
+++ 2009/devel/network/chat/kmess/actions.py
@@ -4,13 +4,14 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import kde
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
 
 def setup():
-    kde.configure()
+    cmaketools.configure(installPrefix=get.kdeDIR())
 
 def build():
-    kde.make()
+    cmaketools.make()
 
 def install():
-    kde.install()
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
