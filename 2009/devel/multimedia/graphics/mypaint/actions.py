0c375a9408a89f96dce23b7680d6206e989421bb
sdalgic <sdalgic@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-08-01 02:04:20 +0000

    nearly ready except a desktop file miscellaneous problem :@

--- 2009/devel/multimedia/graphics/mypaint/actions.py
+++ 2009/devel/multimedia/graphics/mypaint/actions.py
@@ -4,21 +4,19 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import scons
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-def setup():
-    autotools.configure()
-
 def build():
-    autotools.make()
+    scons.make()
+    #autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    scons.install("prefix=%s/usr install" % get.installDIR())
 
-    pisitools.domove("/usr/share/pixmaps/mypaint_48.png" , "/usr/share/pixmaps/" , "mypaint.png")
+    pisitools.domove("/usr/share/mypaint/desktop/mypaint_48.png" , "/usr/share/pixmaps/" , "mypaint.png")
+    pisitools.domove("/usr/share/mypaint/desktop/mypaint.desktop" , "/usr/share/applications/")
 
-    pisitools.dohtml("html/*")
-    pisitools.dodoc("COPYING", "README", "PLAN", "html/tutorial.txt")
+    pisitools.dodoc("changelog", "COPYING", "LICENSE", "README", "doc/*" )
 
