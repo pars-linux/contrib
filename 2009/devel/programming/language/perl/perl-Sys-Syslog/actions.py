c41eaf166436ebf0b84db492d23a4205b9183c69
Serdar Dalgıç <sd@serdardalgic.org>
2009-08-02 00:17:31 +0000

    Changes before transferring the package to Pardus repository: Version bump, package takeover and make the package ready for Pardus 2009.

--- 2009/devel/programming/language/perl/perl-Sys-Syslog/actions.py
+++ 2009/devel/programming/language/perl/perl-Sys-Syslog/actions.py
@@ -20,4 +20,6 @@ def build():
 def install():
     perlmodules.install()
 
+    pisitools.removeDir("/usr/lib/perl5/%s"  % get.curPERL())
+    pisitools.remove("/usr/lib/perl5/vendor_perl/%s/i686-linux-thread-multi/auto/Sys/Syslog/.packlist" % get.curPERL())
     pisitools.dodoc("Changes", "README")
