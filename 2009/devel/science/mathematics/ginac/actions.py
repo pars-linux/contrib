de98ef2eb6d037ee5a1f701c3f870c7c92561685
sdalgic <sdalgic@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-08-15 16:15:09 +0000

    Make the package ready for 2009:
        * Version bump.
        * Create documentation and make a seperate package for reference documentation.

--- 2009/devel/science/mathematics/ginac/actions.py
+++ 2009/devel/science/mathematics/ginac/actions.py
@@ -7,14 +7,21 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --disable-dependency-tracking")
 
 def build():
-    autotools.make()
+    autotools.make("html")
 
 def install():
     autotools.install()
+
+    pisitools.dohtml("doc/reference/html_files/*")
+
+    for i in ["doc/examples/*.cpp", "doc/examples/ginac-examples.html/index.html"]:
+        pisitools.insinto("/usr/share/doc/%s/examples" % get.srcNAME(), i)
 
     pisitools.dodoc("ChangeLog","NEWS","README","COPYING","AUTHORS")
