9973e112a09fa338cc2c0110f825055e9d1e85e0
Serdar Dalgıç <sd@serdardalgic.org>
2009-08-15 11:29:46 +0000

    Ready for 2009

---
+++ 2009/stable/util/misc/ack/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import perlmodules
+
+def setup():
+    perlmodules.configure()
+
+def build():
+    perlmodules.make()
+
+def install():
+    perlmodules.install()
+
+    pisitools.removeDir("/usr/lib/perl5/%s" % get.curPERL())
+    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/i686-linux-thread-multi" % get.curPERL())
+
+    pisitools.dodoc("ack-help*.txt", "README", "Changes", "TODO")
