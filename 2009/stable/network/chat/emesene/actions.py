63b38ee88b506147a11dad93ded9cde5960d2bd2
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-08-16 18:02:57 +0000

    merge from contrib.2009.devel. It will close #10825
    BUG:FIXED:10825

---
+++ 2009/stable/network/chat/emesene/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+
+def install():
+
+    # .desktop entry
+    pisitools.insinto("/usr/share/applications", "misc/emesene.desktop")
+
+    # icon
+    pisitools.insinto("/usr/share/pixmaps", "emesene-logo.png", "emesene.png")
+
+    # emesene script
+    pisitools.insinto("/usr/share/emesene", "*")
+
+    # license & translators
+    pisitools.dodoc("COPYING", "docs/TRANSLATORS")
+
+    # no need for setup.py
+    pisitools.remove("/usr/share/emesene/setup.py")
