8fac4c8829254421dc84c4329f3597ad458ece5f
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-08-16 20:54:01 +0000

    * Package takeover. * Version bump. * Add translations.xml. * Fix dependencies for lddtree.sh output. * Fix underlinking. * Rewrite file paths in pspec.xml.

--- 2009/devel/multimedia/editor/subtitlecomposer/actions.py
+++ 2009/devel/multimedia/editor/subtitlecomposer/actions.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008 TUBITAK/UEKAE
+# Copyright 2008 - 2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import kde
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    kde.configure()
+    cmaketools.configure(installPrefix="/usr/kde/4")
 
 def build():
-    kde.make()
+    cmaketools.make()
 
 def install():
     kde.install()
@@ -22,4 +23,4 @@ def install():
     shelltools.system("msgfmt -v po/tr.po -o po/tr.mo")
     pisitools.insinto("%s/share/locale/tr/LC_MESSAGES" % get.kdeDIR(), "po/tr.mo", "subtitlecomposer.mo")
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "TODO")
