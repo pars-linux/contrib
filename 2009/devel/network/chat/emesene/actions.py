b0f15fbe525493e92c4542fbd669d321e69fd03a
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-08-16 18:01:05 +0000

    clear whitespaces.

--- 2009/devel/network/chat/emesene/actions.py
+++ 2009/devel/network/chat/emesene/actions.py
@@ -7,21 +7,18 @@
 from pisi.actionsapi import pisitools
 
 def install():
-    
+
     # .desktop entry
     pisitools.insinto("/usr/share/applications", "misc/emesene.desktop")
-    
+
     # icon
     pisitools.insinto("/usr/share/pixmaps", "emesene-logo.png", "emesene.png")
 
     # emesene script
     pisitools.insinto("/usr/share/emesene", "*")
-    
+
     # license & translators
     pisitools.dodoc("COPYING", "docs/TRANSLATORS")
 
     # no need for setup.py
     pisitools.remove("/usr/share/emesene/setup.py")
-
-
-
