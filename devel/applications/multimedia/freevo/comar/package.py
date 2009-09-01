dc883d38c83ab83ae6ba0a8a352a5d04cd05dd0c
faik <faik@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-09-01 07:57:08 +0000

    contrib devel moved to 2009/devel and is continuing there...

--- devel/applications/multimedia/freevo/comar/package.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    os.system("/usr/bin/chmod 1777 /var/cache/freevo")
-    os.system("/usr/bin/chmod 1777 /var/log/freevo")
-    os.system("freevo setup")
--- devel/applications/office/tekir/comar/package.py
+++
@@ -1,9 +0,0 @@
-import re
-import os
-import shutil
-
-def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    if not os.path.exists("/var/db/tekir/tekirDB.log"):
-        shutil.copyfile("/usr/share/tekir/tekirDB.log", "/var/db/tekir/tekirDB.log")
-
-
