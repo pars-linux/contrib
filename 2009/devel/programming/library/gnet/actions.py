1e52778868b1df5f1591140c9f7f11861dd92ed3
mnurolcay <mnurolcay@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-09-05 11:12:14 +0000

    Bump to stable: - Fix homepage and license add translations.xml - Split docs - Fix license and copyright date - Fix unused direct deps
    Ready for 2009. Needed for gcompris

--- 2009/devel/programming/library/gnet/actions.py
+++ 2009/devel/programming/library/gnet/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007 TUBITAK/UEKAE
+# Copyright 2007-2009 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,6 +9,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
+    autotools.autoreconf("-vfi")
     autotools.configure("--disable-static")
 
 def build():
