f9f6c37e6b0f31a88e7487bd574d8d6ed7272899
mehmet nur olcay <mnurolcay_at_gmail.com>
2009-09-10 22:11:39 +0000

    Stable bump: - Ported to KDE4 - Turkish translation is in now - Add icon tag and desktop patch, fix home page, dependencies and some cosmetics
    But it's broken, can not connect to gmail for now...

--- 2009/devel/network/mail/kgmailnotifier/actions.py
+++ 2009/devel/network/mail/kgmailnotifier/actions.py
@@ -4,18 +4,17 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import kde
-
-WorkDir = "KGmailNotifier"
 
 def setup():
-    kde.configure()
+    cmaketools.configure(installPrefix="/usr/kde/4", sourceDir=".")
 
 def build():
-    kde.make()
+    cmaketools.make()
 
 def install():
-    kde.install()
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS","ChangeLog","NEWS","README","TODO")
+    pisitools.dodoc("ChangeLog", "COPYING")
