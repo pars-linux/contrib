daaa51ebdafe99e19d697c9bc9a6ad0e21eab0e3
Gokmen Gorgen <gkmngrgn@gmail.com>
2009-09-28 10:22:18 +0000

    changed package name and version bump. Now it's supporting git version control system.

--- 2009/devel/desktop/xfce/addon/thunar-vcs-plugin/actions.py
+++ 2009/devel/desktop/xfce/addon/thunar-vcs-plugin/actions.py
@@ -9,7 +9,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--enable-subversion \
+                         --enable-git")
 
 def build():
     autotools.make()
