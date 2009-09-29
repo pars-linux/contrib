282c2ba8c80630e491a0208c4b3d1fa458564a85
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2009-09-29 19:52:09 +0000

    merge: qlandkarte since its dependency proj is in repos

---
+++ 2009/stable/science/gis/qlandkarte/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+WorkDir = 'QLandkarte_%s' % get.srcVERSION()
+
+def setup():
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall('INSTALL_ROOT="%s"' % get.installDIR())
+    pisitools.remove("/usr/share/menu/qlandkarte.menu")
+
+    pisitools.dodoc("skin/README.SKIN", "skin/skin.txt")
