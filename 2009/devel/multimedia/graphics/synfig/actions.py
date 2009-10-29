32e02a95872737e2034de60ffd386738f4f0b3cf
Cihan Okyay <okyaycihan@gmail.com>
2009-10-29 21:57:18 +0000

    Version bump Purged unused direct dependencies Changed dependency name (libxmlpp) Take over

--- 2009/devel/multimedia/graphics/synfig/actions.py
+++ 2009/devel/multimedia/graphics/synfig/actions.py
@@ -6,14 +6,17 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
     autotools.configure()
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING",  "TODO", "NEWS", "README")
