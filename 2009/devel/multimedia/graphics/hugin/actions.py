98ae1072eaefcf083623bf910844c79ba36237a0
Ertuğrul Erata <ertugrulerata@gmail.com>
2009-10-30 16:01:31 +0000

    hugin new release wxGTK release 14 e uyumlu.

--- 2009/devel/multimedia/graphics/hugin/actions.py
+++ 2009/devel/multimedia/graphics/hugin/actions.py
@@ -8,17 +8,6 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
 
-# since upstream did not release a tarball, we made it by hand
-# to get the same tarball checkout the svn revision 3197. There is a rumour
-# that revision 3098 will be released as 0.7.0 but since it is not released
-# and it is not tagged, we are now going for latest a.k.a. rev 3197
-#
-# svn co -r3197 https://svn.sourceforge.net/svnroot/hugin/hugin/trunk hugin-svn
-#
-# and make a source tarball with
-#
-# cmake .
-# make package_source
 
 def setup():
     cmaketools.configure("--enable-shared \
