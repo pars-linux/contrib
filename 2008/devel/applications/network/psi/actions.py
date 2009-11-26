dadeb59881988cbf4926cfdbe024718685254983
dfisek <dfisek@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2009-11-26 05:18:10 +0000

    Version bump to new RC for various bugfixes

--- 2008/devel/applications/network/psi/actions.py
+++ 2008/devel/applications/network/psi/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir='psi-0.14-rc2'
+WorkDir='psi-0.14-rc3'
 
 docdir="%s/%s" % (get.docDIR(), get.srcTAG())
 
