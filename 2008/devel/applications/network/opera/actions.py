dee5f188bce9530762d3fffd37b4abbed838f6a3
Serdar Dalgıç <sd@serdardalgic.org>
2009-11-26 01:43:07 +0000

    Version bump to 10.10 release for Pardus 2008. See http://www.opera.com/docs/changelogs/linux/1010/ for details.
    BUG:COMMENT:11544

--- 2008/devel/applications/network/opera/actions.py
+++ 2008/devel/applications/network/opera/actions.py
@@ -9,7 +9,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir="opera-10.01-4682.gcc4-shared-qt3.i386"
+WorkDir="%s-%s-4742.gcc4-shared-qt3.i386" %(get.srcNAME(), get.srcVERSION())
 
 def install():
     shelltools.system("./install.sh DESTDIR=%s" % get.installDIR())
