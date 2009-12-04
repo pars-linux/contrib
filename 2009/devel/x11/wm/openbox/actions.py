a37e8b2835403d11d9c3e5d09a16f4cece9b5980
Cihan Okyay <okyaycihan@gmail.com>
2009-12-04 16:48:13 +0000

    add openbox and new component

---
+++ 2009/devel/x11/wm/openbox/actions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+
+def setup():
+    shelltools.export("AUTOPOINT", "true")
+    autotools.autoreconf("-fi")
+    autotools.configure("--disable-static \
+                         --docdir=/%s/%s" % (get.docDIR(), get.srcDIR()))
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "CHANGELOG", "COPYING", "README")
