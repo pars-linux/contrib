1e1fbeb4da1887d1acdc184951390c8deaa0f71b
dfisek <dfisek@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-01-03 17:58:25 +0000

    Fix sandbox violations

--- 2008/devel/desktop/kde/moodin-themes/actions.py
+++ 2008/devel/desktop/kde/moodin-themes/actions.py
@@ -6,6 +6,10 @@
 
 from pisi.actionsapi import kde
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
 
 WorkDir='moodin'
 
