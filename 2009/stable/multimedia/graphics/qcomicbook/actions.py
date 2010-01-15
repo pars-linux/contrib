b5df66fb95fd8731b1d5b94b0524234976e3556a
Doruk Fisek <dfisek@ozguryazilim.com.tr>
2010-01-15 19:07:06 +0000

    Merge from devel/multimedia/graphics/qcomicbook:
    rev. 12435, by dfisek on 2010-01-15
    Version bump

--- 2009/stable/multimedia/graphics/qcomicbook/actions.py
+++ 2009/stable/multimedia/graphics/qcomicbook/actions.py
@@ -5,18 +5,17 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import autotools
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    cmaketools.configure(installPrefix="/usr", sourceDir=".")
 
 def build():
-    autotools.make()
+    cmaketools.make()
 
 def install():
-    autotools.install()
-
-    pisitools.insinto("/usr/share/applications","fedora/qcomicbook.desktop")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("ChangeLog", "COPYING", "README", "AUTHORS", "TODO", "THANKS")
