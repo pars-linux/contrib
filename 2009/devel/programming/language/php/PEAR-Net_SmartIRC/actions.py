70915b72567e95da037551a33afc0aeaa1aa2248
Necdet Yucel <necdetyucel@gmail.com>
2010-01-31 21:16:03 +0000

    version bump. package takeover.

--- 2009/devel/programming/language/php/PEAR-Net_SmartIRC/actions.py
+++ 2009/devel/programming/language/php/PEAR-Net_SmartIRC/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Net_SmartIRC-1.0.0"
+WorkDir="Net_SmartIRC-%s"  % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Net", "SmartIRC*")
