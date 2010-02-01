94851c5c0c62ed5cf0d62ffc1665a3eb922e20a3
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-02-01 13:51:05 +0000

    version bump. package takeover.

--- 2009/devel/programming/language/php/PEAR-HTTP_Session/actions.py
+++ 2009/devel/programming/language/php/PEAR-HTTP_Session/actions.py
@@ -12,3 +12,4 @@ WorkDir="HTTP_Session-%s" % get.srcVERSION()
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTTP", "Session*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/HTTP_Session", "docs/*")
+    pisitools.insinto("/usr/share/php5/PEAR/tests/HTTP_Session", "tests/*")
