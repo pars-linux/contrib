76119bcb1960a3c883ad08c174dc02811d3b8990
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-02-01 00:59:06 +0000

    version bump. package takeover.

--- 2009/devel/programming/language/php/PEAR-Net_FTP/actions.py
+++ 2009/devel/programming/language/php/PEAR-Net_FTP/actions.py
@@ -12,3 +12,4 @@ WorkDir="Net_FTP-%s" % get.srcVERSION()
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Net", "Net/*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/Net_FTP", "example/*")
+    pisitools.insinto("/usr/share/php5/PEAR/tests/Net_FTP", "tests/*")
