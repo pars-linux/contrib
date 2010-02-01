31998abc2f15ed0e7f53f260ca73aaa7f9c514a1
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-02-01 12:56:43 +0000

    version bump. package takeover.

--- 2009/devel/programming/language/php/PEAR-Services_W3C_HTMLValidator/actions.py
+++ 2009/devel/programming/language/php/PEAR-Services_W3C_HTMLValidator/actions.py
@@ -7,8 +7,9 @@
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir="Services_W3C_HTMLValidator-%s" % get.srcVERSION()
+WorkDir="Services_W3C_HTMLValidator-1.0.0RC2"
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Services/W3C", "HTMLValidator*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/Services_W3C_HTMLValidator", "docs/*")
+    pisitools.insinto("/usr/share/php5/PEAR/tests/Services_W3C_HTMLValidator", "tests/*")
