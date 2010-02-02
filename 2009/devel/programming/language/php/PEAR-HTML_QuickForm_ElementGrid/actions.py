e97ec71f4dec61b322121fe582c216c266288e4c
Necdet Yucel <necdetyucel@gmail.com>
2010-02-02 10:48:26 +0000

    version bump. package takeover.

--- 2009/devel/programming/language/php/PEAR-HTML_QuickForm_ElementGrid/actions.py
+++ 2009/devel/programming/language/php/PEAR-HTML_QuickForm_ElementGrid/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="HTML_QuickForm_ElementGrid-0.1.0"
+WorkDir="HTML_QuickForm_ElementGrid-%s"  % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTML/QuickForm", "HTML/QuickForm/ElementGrid.php")
