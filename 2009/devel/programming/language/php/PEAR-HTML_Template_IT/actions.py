8db3a9cd10ebeae59178ab614ac9750d091d94cb
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-02-03 08:32:51 +0000

    version bump. package takeover.

--- 2009/devel/programming/language/php/PEAR-HTML_Template_IT/actions.py
+++ 2009/devel/programming/language/php/PEAR-HTML_Template_IT/actions.py
@@ -5,9 +5,8 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
-WorkDir="HTML_Template_IT-%s" % get.srcVERSION()
+WorkDir="HTML_Template_IT-1.3.0a1"
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/HTML/Template", "IT*")
