a38619467016437cbbb215a8e16fff72338dd681
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-02-12 14:04:17 +0000

    tiny fixes

--- 2009/devel/hardware/graphics/cellwriter/actions.py
+++ 2009/devel/hardware/graphics/cellwriter/actions.py
@@ -6,7 +6,7 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-
+from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--with-x \
@@ -16,8 +16,5 @@ def build():
     autotools.make()
 
 def install():
-    autotools.install()
-
-    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
-
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     pisitools.dodoc("AUTHORS", "COPYING", "ChangeLog", "README", "TODO")
