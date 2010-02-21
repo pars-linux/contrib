477036fb0186ee0600738857127252d6f70647ff
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-02-21 22:11:23 +0000

    * Update dependencies. * Remove empty doc files. * Split docs.

--- 2009/devel/programming/language/dotnet/webkit-sharp/actions.py
+++ 2009/devel/programming/language/dotnet/webkit-sharp/actions.py
@@ -7,16 +7,18 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
-shelltools.export("MONO_SHARED_DIR", ".")
+shelltools.export("MONO_SHARED_DIR", get.workDIR())
 
 def setup():
     autotools.configure()
 
 def build():
-    autotools.make()
+    autotools.make("-j1")
 
 def install():
     autotools.install()
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
+    # Empty files: NEWS, README
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING")
