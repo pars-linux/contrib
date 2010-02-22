a792cf489b1af0e2fb355efbb69a843a7c23cef1
Serdar Dalgıç <sd@serdardalgic.org>
2010-02-22 09:07:48 +0000

    make changes @devel, waiting for protobuf package to be reviewed.

--- 2009/devel/multimedia/graphics/mypaint/actions.py
+++ 2009/devel/multimedia/graphics/mypaint/actions.py
@@ -10,13 +10,9 @@ from pisi.actionsapi import get
 
 def build():
     scons.make()
-    #autotools.make()
 
 def install():
     scons.install("prefix=%s/usr install" % get.installDIR())
-
-    pisitools.domove("/usr/share/mypaint/desktop/mypaint_48.png" , "/usr/share/pixmaps/" , "mypaint.png")
-    pisitools.domove("/usr/share/mypaint/desktop/mypaint.desktop" , "/usr/share/applications/")
 
     pisitools.dodoc("changelog", "COPYING", "LICENSE", "README", "doc/*" )
 
