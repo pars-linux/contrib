ff7b148dd7930caaec4d064ce069e5e4b2d8f40d
Necdet Yucel <necdetyucel@gmail.com>
2010-02-25 08:01:16 +0000

    Fixed unit tests, version bump.

--- 2009/devel/programming/language/php/PEAR-HTML_Template_IT/actions.py
+++ 2009/devel/programming/language/php/PEAR-HTML_Template_IT/actions.py
@@ -6,9 +6,9 @@
 
 from pisi.actionsapi import pisitools
 
-WorkDir="HTML_Template_IT-1.3.0a1"
+WorkDir="HTML_Template_IT-1.3.0a2"
 
 def install():
-    pisitools.insinto("/usr/share/php5/PEAR/HTML/Template", "IT*")
+    pisitools.insinto("/usr/share/php5/PEAR/HTML/Template", "HTML/Template/*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/HTML_Template_IT", "examples/*")
     pisitools.insinto("/usr/share/php5/PEAR/tests/HTML_Template_IT", "tests/*")
