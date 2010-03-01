a4c91e85ecf953ad42519574dbac4df12653cfba
mehmet nur olcay <mnurolcay_at_gmail.com>
2010-03-01 00:08:01 +0000

    Sandbox fix

--- 2009/devel/network/mail/kgmailnotifier/actions.py
+++ 2009/devel/network/mail/kgmailnotifier/actions.py
@@ -4,9 +4,12 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import cmaketools
 from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     cmaketools.configure(installPrefix="/usr/kde/4", sourceDir=".")
