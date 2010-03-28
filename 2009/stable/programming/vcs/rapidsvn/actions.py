e08c881706e8d9d30ace39236b9c2ffc774b079c
mehmet nur olcay <mnurolcay_at_gmail.com>
2010-03-28 18:31:30 +0000

    Ready for stable

---
+++ 2009/stable/programming/vcs/rapidsvn/actions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import autotools
+
+WorkDir="rapidsvn-%s-1" % get.srcVERSION()
+
+def setup():
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-static \
+                         --with-apr-config=/usr/bin/apr-1-config \
+                         --with-apu-config=/usr/bin/apu-1-config")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.insinto("/usr/share/pixmaps", "src/res/bitmaps/rapidsvn_32x32.xpm", "rapidsvn.xpm")
+
+    pisitools.dodoc("AUTHORS", "CHANGES", "COPYING", "README")
