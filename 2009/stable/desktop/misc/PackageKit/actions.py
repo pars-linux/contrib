59a9e4bc81a7040318502195a278b8bd1587c228
Aşkın Yollu <askin@askin.ws>
2010-03-29 13:14:12 +0000

    Ready for stable

---
+++ 2009/stable/desktop/misc/PackageKit/actions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf('-vif')
+    autotools.configure("--with-dbus-sys=/etc/dbus-1 \
+                         --with-dbus-services=/usr/share/dbus-1/system-services \
+                         --with-default-backend=pisi \
+                         --enable-pisi \
+                         --with-security-framework=polkit \
+                         --localstatedir=/var \
+                         --disable-static \
+                         --disable-ruck \
+                         --disable-dummy \
+                         --disable-tests \
+                         --enable-browser-plugin \
+                         --with-mozilla-plugin-dir=/usr/lib/nsbrowser/plugins")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("README", "TODO", "MAINTAINERS", "HACKING", "AUTHORS", "NEWS")
+    pisitools.doman("man/*.1")
+    pisitools.dohtml("docs/html/*")
