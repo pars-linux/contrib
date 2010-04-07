8839dc120ac2651e7ff9ddb828facc878c69e5c6
igungor <igungor@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-04-07 18:37:50 +0000

    Version bump, remove lyx dependency, extend fileTypes.
    BUG:COMMENT:12561

--- 2009/devel/programming/build/sdcc/actions.py
+++ 2009/devel/programming/build/sdcc/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2005-2009 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,12 +9,15 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 WorkDir = "sdcc"
+NoStrip = ["/"]
 
 def setup():
-    autotools.configure("--enable-libgc") # --enable-doc")
+    autotools.configure("--enable-libgc")
 
 def build():
     autotools.make("-j1")
 
 def install():
     autotools.install()
+
+    pisitools.dodoc("ChangeLog", "COPYING", "README")
