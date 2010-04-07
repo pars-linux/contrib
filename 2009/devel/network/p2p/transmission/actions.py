76e221edf0236acfee46848232aaf01c5f033a18
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-04-07 22:19:47 +0000

    security update: * Version bump:   All Platforms:     - Fix possible data corruption issue caused by data sent by bad peers during endgame     - Fix potential buffer overflow when adding maliciously-crafted magnet links     - Fix announces to IPv6 trackers     - Fix DNS problems on some platforms, including Debian     - Fix issues with the incomplete directory functionality     - Fix port forwarding error on some routers by updating libnatpmp and miniupnp
      Mac:
        - Fix bug where setting low priority in the add window resulted in high priority
    
      GTK+:
        - Fix directory selection error in GTK+ 2.19
        - Small GUI improvements: HIG correctness, remove deprecated GTK+ calls, etc.
    
      Daemon:
        - Fix 1.91 build error on Mac and FreeBSD
        - Standardize the daemon's watchdir feature to behave like the other clients'
    
      Web Client:
        - Statistics dialog
        - Fix error in "trash data and remove from list" that didn't trash all data
        - Fix display of ratios and time
        - Update to jQuery 1.4.2
    * Remove desktop.patch. Not required. It fixed on this version.
    * Fix actions.py for pep8.
    * Split transmission-web.
    * Update translations.xml.

--- 2009/devel/network/p2p/transmission/actions.py
+++ 2009/devel/network/p2p/transmission/actions.py
@@ -4,10 +4,14 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+import glob
+import os
+
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+
 
 def setup():
     autotools.configure("--disable-static \
@@ -17,7 +21,8 @@ def setup():
                          --enable-daemon")
 
     # For daemon config files.
-    pisitools.dodir("%s/etc/transmission-daemon")
+    pisitools.dodir("/etc/transmission-daemon")
+
 
 def build():
     autotools.make()
@@ -26,11 +31,33 @@ def build():
     shelltools.system("qmake-qt4 qtr.pro")
     autotools.make()
 
+
 def install():
     autotools.install()
 
     pisitools.dodoc("COPYING", "AUTHORS", "README", "ChangeLog", "NEWS")
 
+    ## install transmission-web manually. it's giving an error in 1.92.
+    web_dir = '%s/usr/share/%s/web' % (get.installDIR(), get.srcNAME())
+    web_subdirs = ['images', 'javascript', 'stylesheets']
+
+    def _remove_unrequired_files(dir_path):
+        unrequired_files = glob.glob(os.path.join(dir_path, 'Makefile*'))
+
+        for unrequired_file in unrequired_files:
+            shelltools.unlink(unrequired_file)
+
+    # copy
+    shelltools.copytree('web/', web_dir)
+
+    # remove unrequired files
+    _remove_unrequired_files(web_dir)
+    shelltools.unlink(os.path.join(web_dir, 'LICENSE'))
+    for root, dirs, files in os.walk(web_dir):
+        for name in dirs:
+            _remove_unrequired_files(os.path.join(root, name))
+
+    ## and install transmission-qt..
     shelltools.cd("qt/")
     autotools.rawInstall("INSTALL_ROOT=%s/usr" % get.installDIR())
     pisitools.dosym("/usr/bin/qtr", "/usr/bin/transmission-qt")
