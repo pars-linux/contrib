6b6b2e60143c9a3cba21c60540383ac3059faea3
Serdar Dalgıç <sd@serdardalgic.org>
2010-04-08 20:58:03 +0000

    Version bump: Updated ip adress control added.
    BUG:COMMENT:11707

--- 2009/devel/network/connection/pog/actions.py
+++ 2009/devel/network/connection/pog/actions.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 TUBITAK/UEKAE
+# Copyright 2009-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pythonmodules
-from pisi.actionsapi import get
-
-WorkDir = "%s" % get.srcNAME()
 
 def build():
     pythonmodules.compile()
