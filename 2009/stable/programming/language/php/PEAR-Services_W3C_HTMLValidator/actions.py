0e2e133350eeabae2df880d5ca6fc98416874ffa
Necdet Yucel <necdetyucel@gmail.com>
2010-04-26 21:12:44 +0000

    merge from devel

---
+++ 2009/stable/programming/language/php/PEAR-Services_W3C_HTMLValidator/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="Services_W3C_HTMLValidator-%s" % get.srcVERSION()
+
+def install():
+    pisitools.insinto("/usr/share/php5/PEAR/Services/W3C", "Services/W3C/HTMLValidator*")
+    pisitools.insinto("/usr/share/php5/PEAR/doc/Services_W3C_HTMLValidator", "docs/*")
+    pisitools.insinto("/usr/share/php5/PEAR/tests/Services_W3C_HTMLValidator", "tests/*")
