3574c88322056ddeb7dab0ba05e7800a884f458a
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-04-26 20:55:27 +0000

    merge from devel

---
+++ 2009/stable/programming/language/php/PEAR-HTML_Javascript/actions.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir="HTML_Javascript-%s" % get.srcVERSION()
+
+def install():
+    pisitools.insinto("/usr/share/php5/PEAR/HTML", "Javascript*")
+    pisitools.insinto("/usr/share/php5/PEAR/doc/HTML_Javascript", "examples/*")
+    pisitools.insinto("/usr/share/php5/PEAR/tests/HTML_Javascript", "tests/*")
