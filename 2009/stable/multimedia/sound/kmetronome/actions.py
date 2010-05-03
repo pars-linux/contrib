187e6015e604f0baf0a7a0a3b1da5f68a5b5a369
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-03 14:10:51 +0000

    ready for contrib-stable

---
+++ 2009/stable/multimedia/sound/kmetronome/actions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import cmaketools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release", installPrefix="/usr/kde/4")
+
+def build():
+    cmaketools.make()
+
+def install():
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dosym("/usr/share/icons/hicolor/128x128/apps/kmetronome.png", "/usr/share/pixmaps/kmetronome.png")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
