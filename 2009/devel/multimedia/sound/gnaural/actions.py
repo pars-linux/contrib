660ce4cd7eff18f319a9c0d7267a37ac3e1d2760
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-05 16:11:25 +0000

    takeover, vesion bump. not ready yet for contrib stable: translation and test, unnecesseay dirs, why not desktop in menu

--- 2009/devel/multimedia/sound/gnaural/actions.py
+++ 2009/devel/multimedia/sound/gnaural/actions.py
@@ -4,22 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-
-WorkDir="gnaural-0.4.20070301"
+from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    shelltools.system("./autogen.sh")
+    autotools.configure("--disable-rpath")
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
-
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     pisitools.domo("po/gnaural.tr.po", "tr", "gnaural.mo")
-
     pisitools.insinto("/usr/share/pixmaps", "pixmaps/*")
-
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
