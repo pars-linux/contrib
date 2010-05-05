84a623ba6b928aebb4f8c241e9c26186f6e02dae
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-05 22:11:53 +0000

    take over package, version bump, start use (after testing add contrib 2009 stable)

--- 2009/devel/util/misc/klogshow/actions.py
+++ 2009/devel/util/misc/klogshow/actions.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2006 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import cmaketools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 def setup():
-    cmaketools.configure()
+    shelltools.system("qmake-qt4 PREFIX=/usr")
 
 def build():
-    cmaketools.make()
+    autotools.make()
 
 def install():
-    pisitools.dobin("src/klogshow")
-
-    pisitools.insinto("/usr/share/applications","src/klogshow.desktop")
-    pisitools.insinto("/usr/share/pixmaps","src/images/klogshow.png")
-
+    autotools.rawInstall("INSTALL_ROOT=%s" % get.installDIR())
     pisitools.dodoc("CHANGELOG", "COPYING")
