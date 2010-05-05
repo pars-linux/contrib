754f17239916a8020f6122c386c4a0578fec8ee9
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-05 17:16:47 +0000

    take over dmz* cursor theme package, they are ready for contrib 2009 stable

--- 2009/devel/desktop/lookandfeel/dmz-xcursor/actions.py
+++ 2009/devel/desktop/lookandfeel/dmz-xcursor/actions.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 #
-# Copyright 2007 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
-WorkDir = "."
+WorkDir = "Vanilla-DMZ"
 
 def install():
-    pisitools.dodir("/usr/share/cursors/xorg-x11/DMZ/")
-    shelltools.copytree("Vanilla-DMZ/cursors/","%s/usr/share/cursors/xorg-x11/DMZ/cursors" % get.installDIR(), True)
+    pisitools.insinto("/usr/share/icons/DMZ", "cursors")
+    pisitools.insinto("/usr/share/icons/DMZ", "index.theme")
+    pisitools.dodoc("COPYING")
