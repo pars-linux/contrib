90b1ac4f2bc76a7f28fb3020396e9082038b7d2b
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-06 10:39:52 +0000

    take over, version bump, test system

--- 2009/devel/hardware/info/systester/actions.py
+++ 2009/devel/hardware/info/systester/actions.py
@@ -5,22 +5,20 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
 WorkDir = "systester-%s-src" % get.srcVERSION()
 
 def setup():
-    autotools.rawConfigure()
+    autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
+    # autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     pisitools.dobin("systester")
-
     pisitools.insinto("/usr/share/systester/data", "images/*.png")
     pisitools.insinto("/usr/share/pixmaps", "images/generic.png", "systester.png")
-
-    pisitools.dodoc("gpl.txt")
+    pisitools.dodoc("Authors", "Changelog", "COPYING", "gpl.txt")
