3a7a0615fe8f6bf49f0ad41ad50e9a353cba2f26
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-06 22:40:42 +0000

    not a take over, version bump only

--- 2009/devel/programming/tool/gquilt/actions.py
+++ 2009/devel/programming/tool/gquilt/actions.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2007 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def build():
@@ -13,6 +14,8 @@ def build():
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-#   Requires SVN PiSi
+    pisitools.dodoc("ChangeLog", "COPYING")
+
+#    Requires SVN PiSi
 #    from pisi.actionsapi import pythonmodules
 #    pythonmodules.fixCompiledPy("/usr/share/gquilt/")
