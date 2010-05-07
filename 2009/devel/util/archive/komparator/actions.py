977f899c983daf6458ef3351e5a276210e8b2722
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-07 13:17:35 +0000

    not a take over, bumped to kde4 version, it must be renamed (imo), it needs also more sympathy

--- 2009/devel/util/archive/komparator/actions.py
+++ 2009/devel/util/archive/komparator/actions.py
@@ -4,16 +4,19 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import kde
+from pisi.actionsapi import cmaketools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+# sormadan paketin ismini değiştirmek istemedim, komparator4 olacak sonra
+WorkDir = "komparator4-0.1b"
 
 def setup():
-    kde.configure()
+    cmaketools.configure("-DCMAKE_BUILD_TYPE=release", installPrefix="/usr/kde/4")
 
 def build():
-    kde.make()
+    cmaketools.make()
 
 def install():
-    kde.install()
-
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
+    cmaketools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README", "TODO")
