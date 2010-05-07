81667c5cdb6947b4c84282a65b1a5a60c997d741
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-07 21:21:50 +0000

    bu oyun maidis's list'teydi ama simdi hatirladim, gelistiricisi baska bir firma tarafindan satin alindi, sonra bunlar niyeyse arsivleri filan cektiler galiba, tam detaylari hatirlamiyorum, pakette not biraktim, bakicisi olacak kisi bakar

--- 2009/devel/game/board/hangman/actions.py
+++ 2009/devel/game/board/hangman/actions.py
@@ -6,15 +6,15 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-
+from pisi.actionsapi import get
 
 def setup():
+    pisitools.dosed("src/hangman.c", "inline void SE_CheckEvents", "void SE_CheckEvents")
     autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
-
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
