f89c38e46e54626b473746c130b84575b309c6a0
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-07 23:47:08 +0000

    find a new live archive

--- 2009/devel/game/arcade/smw/actions.py
+++ 2009/devel/game/arcade/smw/actions.py
@@ -1,31 +1,33 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright © 2006-2008  TUBITAK/UEKAE
+# Copyright © 2006-2010  TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "smw-%s" % get.srcVERSION()
+WorkDir = "smw1.7afe"
 
 def setup():
+    shelltools.chmod(get.workDIR() + "/smw1.7afe/configure", 0755)
     autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
+    # veri dosyalarını kurduramadım
+    # insinto veya copytree ile bir şeyler yapılabilir
+    # ayrıca dosya izinlerinin de değişmesi gerekiyor
+    # yoksa ancak sudo smw
+    # autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
     pisitools.dobin("smw")
     pisitools.dobin("leveledit")
-
-    pisitools.insinto("/usr/share/smw/gfx","gfx/*")
-    pisitools.insinto("/usr/share/smw/sfx","sfx/*")
-    pisitools.insinto("/usr/share/smw/maps","maps/*.map")
-    pisitools.insinto("/usr/share/smw/music","music/*")
-    pisitools.insinto("/usr/share/smw/tours","tours/*")
 
     pisitools.dohtml("README.html")
     pisitools.dodoc("WHATSNEW.txt", "README.txt")
