14844c827311b4d4ecf275eb20dda15c5a1071c7
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-08 14:22:22 +0000

    take over milkytracker and milkytracker-xi

--- 2009/devel/multimedia/editor/milkytracker/actions.py
+++ 2009/devel/multimedia/editor/milkytracker/actions.py
@@ -4,10 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = ""
+def setup():
+    autotools.configure("--prefix=/usr \
+                         --with-jack \
+                         --with-alsa")
+
+def build():
+    autotools.make()
 
 def install():
-    pisitools.insinto("/opt/MilkyTracker",  "*")
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.insinto("/usr/share/milkytracker", "resources")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "docs/*.rtf", "docs/readme_rtaudio", "docs/readme_unix", "docs/TiTAN.nfo")
+    pisitools.dohtml("docs/*.html")
