7a83e027bdc7a431fa79984d6fa23b53ff3d097e
maidis <maidis@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-05-20 20:58:52 +0000

    two cursor themes, ready for contrib stable imlec temalari katkici deposu icin hazir yakinda depoya eklenir, paket yoneticisi araciligiyla yukleyebilirsiniz
    BUG:FIXED:12873

---
+++ 2009/stable/desktop/lookandfeel/dmz-xcursor/actions.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2007-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+WorkDir = "Vanilla-DMZ"
+
+def install():
+    pisitools.insinto("/usr/share/icons/DMZ", "cursors")
+    pisitools.insinto("/usr/share/icons/DMZ", "index.theme")
+    pisitools.dodoc("COPYING")
