2056e515efd5371ad23f41962b641ff82534babe
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-05-31 12:07:38 +0000

    security update: add patch for fixing resolv.conf file permissions.

--- 2009/stable/network/connection/wicd/files/startservice.py
+++ 2009/stable/network/connection/wicd/files/startservice.py
@@ -14,23 +14,5 @@ def startService(service):
     link.System.Service[service].start()
 
 def infoService(service):
-    # return 'on' or 'stopped'.
     return link.System.Service[service].info()[2]
 
-def warningWidget():
-    import gtk
-    import os
-
-    if os.getenv("LANG").startswith("tr"):
-        msg = "Wicd servisi şuan etkin değil! Servis Yöneticisi'nden\nWicd ağ yöneticisini çalıştırın ve tekrar deneyin."
-
-    else:
-        msg = "Service Wicd is not started! Please run Wicd connection\nmanager from Service Manager and try again."
-
-    warning = gtk.MessageDialog(None,
-                                gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                                gtk.MESSAGE_INFO,
-                                gtk.BUTTONS_OK,
-                                str(msg))
-    warning.run()
-    warning.destroy()
