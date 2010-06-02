2172b81f75368677f224937fa157cb89217e24ff
gkmngrgn <gkmngrgn@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-06-02 10:22:58 +0000

    * Update copyright year. * Not required to COPYING file. * Build documentation with gtk-doc. * Specify package name and url. * Add missing license information. * Fix doc files that will be include to package.

--- 2009/devel/multimedia/library/gnonlin/actions.py
+++ 2009/devel/multimedia/library/gnonlin/actions.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008 TUBITAK/UEKAE
+# Copyright 2008 - 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
 
 def setup():
+    autotools.autoreconf("-vfi")
     autotools.configure("--disable-static \
-                         --disable-valgrind")
-                        # disabled gtkdoc. it still doesn't on repositories..
+                         --enable-gtk-doc \
+                         --disable-valgrind \
+                         --with-package-name='Pardus %s package' \
+                         --with-package-origin='http://www.pardus.org.tr/' \
+                         " % get.srcNAME())
 
-    pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
 
 def build():
     autotools.make()
 
+
 def install():
     autotools.install()
 
-    pisitools.dodoc("COPYING", "README", "AUTHORS", "NEWS", "ChangeLog")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "RELEASE")
