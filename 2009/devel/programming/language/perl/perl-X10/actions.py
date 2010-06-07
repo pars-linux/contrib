4731684ff65a5ce5282e3f9e73ea270a6b5c0747
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-06-07 22:07:21 +0000

    some fixes, add translations.xml, package take over

--- 2009/devel/programming/language/perl/perl-X10/actions.py
+++ 2009/devel/programming/language/perl/perl-X10/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,7 +9,7 @@ from pisi.actionsapi import perlmodules
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "X10-%s" % get.srcVERSION()
+WorkDir = "%s-%s" % (get.srcNAME()[5:], get.srcVERSION())
 
 def setup():
     perlmodules.configure()
@@ -19,5 +19,5 @@ def build():
 
 def install():
     perlmodules.install()
-
-    pisitools.dodoc("README", "TODO")
+    
+    pisitools.removeDir("/usr/lib/perl5/vendor_perl/%s/%s-linux-thread-multi/" % (get.curPERL(), get.ARCH()))
