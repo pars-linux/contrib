db67a64f6f81523659afec76869ecdcd98166be8
igungor <igungor@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-06-07 13:13:31 +0000

    New snapshot. remove mainlines patches

--- 2009/devel/desktop/enlightenment/base/enlightenment/actions.py
+++ 2009/devel/desktop/enlightenment/base/enlightenment/actions.py
@@ -9,29 +9,22 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-WorkDir = "e-%s" % get.srcVERSION()
-
 def setup():
     shelltools.export("AUTOPOINT", "/bin/true")
-    #autotools.autoreconf("-fi")
 
-    shelltools.system("./autogen.sh \
-                         --prefix=/usr \
+    autotools.autoreconf("-fi")
+    autotools.configure("--prefix=/usr \
                          --disable-static \
+                         --enable-shared \
                          --enable-nls \
                          --enable-pam \
                          --disable-rpath \
-                         --disable-illume \
-                         --disable-illume2 \
                          --with-x")
+
 def build():
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-
-    pisitools.removeDir("/usr/etc")
-
-    pisitools.domo("po/tr.po", "tr", "enlightenment.mo")
 
     pisitools.dodoc("AUTHORS", "COPYING", "README")
