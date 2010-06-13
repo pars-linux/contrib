d84165a048945fdf50c4595e10156feb9f95f3f5
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-06-13 21:44:09 +0000

    Merge from devel/hardware/info/hardinfo:
    rev. 13226, by necdet.yucel on 2010-06-13
    Remove empty /usr/local directory

--- 2009/stable/hardware/info/hardinfo/actions.py
+++ 2009/stable/hardware/info/hardinfo/actions.py
@@ -20,3 +20,4 @@ def install():
     pisitools.dosym("/usr/share/hardinfo/pixmaps/logo.png", "/usr/share/pixmaps/hardinfo.png")
 
     pisitools.dodoc("LICENSE", "TODO")
+    pisitools.removeDir("/usr/local")
