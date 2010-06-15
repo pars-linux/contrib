7e9bcc8f1a1817a83e24dcb209f38f0962e137f0
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-06-15 19:24:55 +0000

    Merge from devel/game/kids/tuxtype:
    rev. 12431, by dfisek on 2010-01-15
    add missing librsvg dep
    
    rev. 13015, by dfisek on 2010-05-01
    direct homepage address
    
    rev. 13253, by necdet.yucel on 2010-06-15
    Remove empty /etc directory

--- 2009/stable/game/kids/tuxtype/actions.py
+++ 2009/stable/game/kids/tuxtype/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2007 TUBITAK/UEKAE
+# Copyright 2007-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -25,3 +25,4 @@ def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("doc/AUTHORS", "doc/ChangeLog", "doc/TODO", "COPYING", "README", "doc/en/howtotheme.html")
+    pisitools.removeDir("/etc")
