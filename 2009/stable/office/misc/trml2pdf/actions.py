0cb78a7a7c9e3ac9a095142b1e6fa70b0b2f9211
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-06-26 10:13:02 +0000

    trml2pdf reportlab rml dosyalarını pdf ye çeviren python kütüphanesi ve konsol uygulaması 2009.2 için hazır

---
+++ 2009/stable/office/misc/trml2pdf/actions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import pythonmodules
+
+def install():
+    pisitools.insinto("/usr/lib/%s/site-packages/trml2pdf" % get.curPYTHON(),"trml2pdf/*.py")
+
+    pythonmodules.fixCompiledPy()
+
+    # create needed symlink
+    pisitools.dosym("/usr/lib/%s/site-packages/trml2pdf/trml2pdf.py" % get.curPYTHON(),"/usr/bin/trml2pdf")
+
+    # add examples and docs
+    pisitools.insinto("/usr/share/doc/trml2pdf/examples" , "rmls/*.rml")
+
+    pisitools.dodoc("README.txt","COPYRIGHT.txt")
