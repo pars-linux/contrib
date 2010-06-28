d6c2f6061787af1690d9c58f464be6221f6e13bb
ertugrul <ertugrul@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-06-28 05:09:14 +0000

    new release not ready

--- 2009/devel/programming/language/python/soya/actions.py
+++ 2009/devel/programming/language/python/soya/actions.py
@@ -4,15 +4,20 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import get
 
-WorkDir="Soya-0.13"
 
+WorkDir="Soya-0.15rc1"
+
+def build():
+    pythonmodules.compile()  
+      
 def install():
     pythonmodules.install()
-    pythonmodules.fixCompiledPy()
 
     #remove soya_editor
-    pisitools.remove("/usr/bin/soya_editor")
-    pisitools.removeDir("/usr/lib/python2.4/site-packages/soya/editor")
+    #pisitools.removeDir("/usr/bin")
+    #pisitools.removeDir("/usr/lib/%s/site-packages/soya/editor" % get.curPYTHON())
