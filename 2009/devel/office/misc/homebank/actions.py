4ce7bca5abfcf79c6e553b99ba9598fa8a429560
Serdar Dalgıç <sd@serdardalgic.org>
2010-07-13 06:55:40 +0000

    Version bump and add missing dependencies: pango, cairo and libofx.
    BUG:COMMENT:12389

--- 2009/devel/office/misc/homebank/actions.py
+++ 2009/devel/office/misc/homebank/actions.py
@@ -8,7 +8,7 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 
 def setup():
-    autotools.configure("--without-ofx")
+    autotools.configure()
 
 def build():
     autotools.make()
