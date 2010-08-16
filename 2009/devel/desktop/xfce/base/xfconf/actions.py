6751af2a87bdd6fa44174aecebc0985bc3e1c772
Gokmen Gorgen <gkmngrgn@gmail.com>
2010-08-16 18:14:43 +0000

    version bump

--- 2009/devel/desktop/xfce/base/xfconf/actions.py
+++ 2009/devel/desktop/xfce/base/xfconf/actions.py
@@ -6,13 +6,12 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static \
-                         --enable-gtk-doc")
-
-    pisitools.dosed("libtool"," -shared ", " -Wl,--as-needed -shared ")
+    shelltools.system('xdt-autogen')
+    autotools.configure('--disable-static')
 
 def build():
     autotools.make()
@@ -20,4 +19,4 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "NEWS", "README", "TODO")
