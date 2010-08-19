84ba3f3048f1638020a0731b4fdd6240f35c0ac4
Necdet Yucel <necdetyucel@gmail.com>
2010-08-19 21:07:27 +0000

    add Turkish Description

--- 2009/devel/programming/language/php/PEAR-Calendar/actions.py
+++ 2009/devel/programming/language/php/PEAR-Calendar/actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
@@ -10,10 +11,7 @@ from pisi.actionsapi import get
 WorkDir="Calendar-%s" % get.srcVERSION()
 
 def install():
-    items = ['Calendar.php', 'Minute.php', 'Week.php', 'Day.php', 'Year.php', 'Factory.php', 'Month.php', 'Decorator.php', 'Hour.php', 'Second.php', 'Validator.php']
-    for item in items:
-        pisitools.insinto("/usr/share/php5/PEAR/Calendar", item)
-    
+    pisitools.insinto("/usr/share/php5/PEAR/Calendar", "*.php")
     pisitools.insinto("/usr/share/php5/PEAR/Calendar/Decorator", "Decorator/*")
     pisitools.insinto("/usr/share/php5/PEAR/Calendar/Engine", "Engine/*")
     pisitools.insinto("/usr/share/php5/PEAR/Calendar/Month", "Month/*")
