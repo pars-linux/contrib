53e955eb3b2b09412d064dc455d700e963db74a5
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-08-19 14:25:33 +0000

    add Turkish Description

--- 2009/devel/programming/language/php/PEAR-Auth/actions.py
+++ 2009/devel/programming/language/php/PEAR-Auth/actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
@@ -13,8 +14,8 @@ def install():
     pisitools.insinto("/usr/share/php5/PEAR", "Auth.php")
     pisitools.insinto("/usr/share/php5/PEAR/Auth", "Container.php")
     pisitools.insinto("/usr/share/php5/PEAR/Auth", "Auth/*")
-    pisitools.insinto("/usr/share/php5/PEAR/Auth/Container", "Container/*")
-    pisitools.insinto("/usr/share/php5/PEAR/Auth/Frontend", "Frontend/*")
+    pisitools.insinto("/usr/share/php5/PEAR/Auth", "Container")
+    pisitools.insinto("/usr/share/php5/PEAR/Auth", "Frontend")
     pisitools.insinto("/usr/share/php5/PEAR/tests/Auth", "tests/*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/Auth", "README*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/Auth/examples", "examples/*")
