4a51302c83ffebbdcb2c498486f2ce53e5614e47
ert976 <ert976@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-08-28 06:12:49 +0000

    takeover, version bump

--- 2009/devel/multimedia/graphics/AutoQ3D/actions.py
+++ 2009/devel/multimedia/graphics/AutoQ3D/actions.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006 TUBITAK/UEKAE
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
-WorkDir = "AutoQ3Dsource"
+import os
+
+datadir = "/usr/share/AutoQ3D-Community"
+
+def fixperms(d):
+    for root, dirs, files in os.walk(d):
+        for name in dirs:
+            shelltools.chmod(os.path.join(root, name), 0755)
+        for name in files:
+            shelltools.chmod(os.path.join(root, name), 0644)
+
+WorkDir = "AutoQ3DCommunity1-40qt4source"
 
 def setup():
     shelltools.system("qmake-qt4")
@@ -19,13 +31,14 @@ def build():
 
 def install():
     pisitools.dobin("AutoQ3D")
-
-    shelltools.chmod("*.qm", 0644)
-    shelltools.chmod("*.png", 0644)
-    shelltools.chmod("images/*", 0644)
-
-    pisitools.insinto("/usr/share/AutoQ3D", "images")
-    pisitools.insinto("/usr/share/AutoQ3D", "*.qm")
-
     pisitools.insinto("/usr/share/pixmaps", "AutoQ3D.png")
-    pisitools.dodoc("docs/*")
+    pisitools.insinto(datadir, "images")
+    pisitools.insinto(datadir, "*.qm")
+    pisitools.insinto(datadir, "*.htm")
+
+    #fix directory and file permissions
+    fixperms("%s/%s" % (get.installDIR(),datadir))
+
+    pisitools.dodoc("docs/*.txt")
+
+
