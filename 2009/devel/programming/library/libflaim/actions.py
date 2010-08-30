8431de8e23e45a0e2ff1991c2b7d335ffc396c15
Necdet Yucel <necdetyucel@gmail.com>
2010-08-30 22:54:37 +0000

    add -docs, take over

--- 2009/devel/programming/library/libflaim/actions.py
+++ 2009/devel/programming/library/libflaim/actions.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
+from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import get
 
@@ -13,3 +14,5 @@ def build():
 
 def install():
     autotools.rawInstall("rpm_build_root=%s OSTYPE=linux HOSTTYPE=x86" % get.installDIR())
+
+    pisitools.dohtml("docs/docs/html/*")
