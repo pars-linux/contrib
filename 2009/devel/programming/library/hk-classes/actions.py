96d2302a45e770da800093fe09e8c4a76f5b06ea
Necdet Yucel <necdetyucel@gmail.com>
2010-09-03 20:04:23 +0000

    split headers, package take over

--- 2009/devel/programming/library/hk-classes/actions.py
+++ 2009/devel/programming/library/hk-classes/actions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*- 
 #
-# Copyright 2005 TUBITAK/UEKAE
+# Copyright 2005-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,7 +9,8 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir ="hk_classes-0.8.3"
+WorkDir ="hk_classes-%s" % get.srcVERSION()
+
 def setup():
     autotools.configure("--with-mysql \
                          --with-postgres \
