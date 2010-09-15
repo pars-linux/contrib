7c1c4cc84fef00f3959c5ad619dffee9d1670f1a
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-09-15 23:05:52 +0000

    Version bump, add gentoo patches, split headers, package take over

--- 2009/devel/programming/library/gdal/actions.py
+++ 2009/devel/programming/library/gdal/actions.py
@@ -4,62 +4,68 @@
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import perlmodules
 from pisi.actionsapi import get
 
 def setup():
     pisitools.dosed("GDALmake.opt.in", "@datadir@", "@datadir@/gdal")
 
-    autotools.configure("--with-libz \
-                         --with-grass=no \
-                         --with-libtool \
-                         --with-local=/usr \
-                         --with-ld-shared \
-                         --enable-static=no \
-                         --enable-shared=yes \
-                         --with-pic \
-                         --with-libgrass=no \
-                         --with-cfitsio=yes \
-                         --with-pcraster=internal \
-                         --with-netcdf \
-                         --with-png \
-                         --with-libtiff \
-                         --with-geotiff=/usr/lib \
-                         --with-jpeg \
-                         --with-gif \
-                         --with-ogdi \
-                         --without-hdf5 \
-                         --with-jasper \
-                         --with-mrsid=no \
-                         --with-jp2mrsid=yes \
-                         --without-bsb \
-                         --with-mysql \
-                         --with-xerces \
-                         --with-odbc \
-                         --with-sqlite \
-                         --with-pg \
-                         --with-ogr \
-                         --with-static-proj4 \
-                         --with-geos \
-                         --without-php \
-                         --with-perl \
-                         --with-ruby \
-                         --with-python")
+    autotools.configure("--enable-shared \
+                        --disable-static \
+                        --with-expat \
+                        --without-grass \
+                        --without-hdf4 \
+                        --without-fme \
+                        --without-pcraster \
+                        --without-kakadu \
+                        --without-mrsid \
+                        --without-jp2mrsid \
+                        --without-msg \
+                        --without-bsb \
+                        --without-dods-root \
+                        --without-oci \
+                        --without-ingres \
+                        --without-spatialite \
+                        --without-dwgdirect \
+                        --without-epsilon \
+                        --without-idb \
+                        --without-sde \
+                        --without-libtool \
+                        --with-ogr \
+                        --with-grib \
+                        --with-vfk \
+                        --with-libtiff \
+                        --with-geotiff \
+                        --with-pg \
+                        --with-cfitsio \
+                        --with-netcdf \
+                        --with-png \
+                        --with-jpeg \
+                        --with-pcidsk \
+                        --with-gif \
+                        --with-ogdi \
+                        --with-hdf5 \
+                        --with-jasper \
+                        --with-ecw \
+                        --with-xerces \
+                        --with-odbc \
+                        --with-curl \
+                        --with-sqlite3 \
+                        --with-mysql \
+                        --with-geos \
+                        --with-pam \
+                        --with-perl \
+                        --with-ruby \
+                        --with-python \
+                        --with-threads \
+                        --with-pymoddir=/usr/lib/python2.6/site-packages")
 
 def build():
     autotools.make()
 
-    shelltools.cd("swig/ruby")
-    autotools.make()
-
 def install():
-    pisitools.dodir("/usr/lib/ruby/site_ruby/1.8/i686-linux/gdal")
-
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("COMMITERS", "HOWTO-RELEASE", "NEWS", "PROVENANCE.TXT", "VERSION")
 
-    perlmodules.fixLocalPod()
