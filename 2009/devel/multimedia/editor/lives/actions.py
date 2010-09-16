5ff8a461ccbb324f72c846923f1a370e25e44a4c
Ertan Argüden <ert976@gmail.com>
2010-09-16 17:39:34 +0000

    delete import shelltools

--- 2009/devel/multimedia/editor/lives/actions.py
+++ 2009/devel/multimedia/editor/lives/actions.py
@@ -6,7 +6,6 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
