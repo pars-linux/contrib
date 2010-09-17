2454354140fe079a7dd6549294459f2af09679bc
Ertan Argüden <ert976@gmail.com>
2010-09-17 15:55:32 +0000

    Merge from devel/multimedia/editor/lives:
    rev. 13668, by ert976 on 2010-09-16
    Version bump to development version
    
    rev. 13669, by ert976 on 2010-09-16
    delete patch
    
    rev. 13670, by ert976 on 2010-09-16
    delete import shelltools

--- 2009/stable/multimedia/editor/lives/actions.py
+++ 2009/stable/multimedia/editor/lives/actions.py
@@ -6,12 +6,12 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
-    shelltools.export("AUTOPOINT", "/bin/true")
-    autotools.autoreconf("-fiv")
+    autotools.aclocal("-I mk/autoconf")
+    autotools.automake()
+    autotools.autoconf()
 
     autotools.configure("--disable-static\
                          --disable-rpath \
