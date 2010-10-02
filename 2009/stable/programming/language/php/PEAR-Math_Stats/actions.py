9b60b1631fcf52cac7914dda7409782acc45e225
Necdet Yucel <necdetyucel@gmail.com>
2010-10-02 19:22:38 +0000

    Merge from devel/programming/language/php/PEAR-Math_Stats:
    rev. 13363, by necdet.yucel on 2010-07-14
    add Description
    
    rev. 13768, by necdet.yucel on 2010-10-02
    version bump after 7 years

--- 2009/stable/programming/language/php/PEAR-Math_Stats/actions.py
+++ 2009/stable/programming/language/php/PEAR-Math_Stats/actions.py
@@ -5,10 +5,11 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Math_Stats-0.9.0beta3"
+WorkDir="Math_Stats-%s" % get.srcVERSION()
 
 def install():
-    pisitools.insinto("/usr/share/php5/PEAR/Math", "Stats.php")
-    pisitools.insinto("/usr/share/php5/PEAR/doc/Math_Stats", "examples/*")
-    pisitools.insinto("/usr/share/php5/PEAR/tests/Math_Stats", "test/*")
+    pisitools.insinto("/usr/share/php5/PEAR/Math", "Math*")
+    pisitools.insinto("/usr/share/php5/PEAR/doc/Math_Stats", "docs/*")
+    pisitools.insinto("/usr/share/php5/PEAR/tests/Math_Stats", "tests/*")
