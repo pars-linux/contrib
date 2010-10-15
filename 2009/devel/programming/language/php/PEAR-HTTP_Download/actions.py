c993cb05e41ea2855f233b91759fb16fc6e7510a
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-10-15 22:05:38 +0000

    version bump

--- 2009/devel/programming/language/php/PEAR-HTTP_Download/actions.py
+++ 2009/devel/programming/language/php/PEAR-HTTP_Download/actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
@@ -10,5 +11,6 @@ from pisi.actionsapi import get
 WorkDir="HTTP_Download-%s" % get.srcVERSION()
 
 def install():
-    pisitools.insinto("/usr/share/php5/PEAR/HTTP", "Download*")
+    pisitools.insinto("/usr/share/php5/PEAR/HTTP", "HTTP/Download*")
     pisitools.insinto("/usr/share/php5/PEAR/tests/HTTP_Download", "tests/*")
+    pisitools.dodoc("LICENSE")
