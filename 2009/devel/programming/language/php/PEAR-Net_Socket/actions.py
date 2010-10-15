ab34f38c0260d8bd8b9d44a4428d657626af158b
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-10-15 21:57:37 +0000

    version bump

--- 2009/devel/programming/language/php/PEAR-Net_Socket/actions.py
+++ 2009/devel/programming/language/php/PEAR-Net_Socket/actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
+# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
@@ -10,4 +11,4 @@ from pisi.actionsapi import get
 WorkDir="Net_Socket-%s" % get.srcVERSION()
 
 def install():
-    pisitools.insinto("/usr/share/php5/PEAR/Net","Socket.php")
+    pisitools.insinto("/usr/share/php5/PEAR/Net","Net/Socket.php")
