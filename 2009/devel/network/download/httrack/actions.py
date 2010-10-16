e10fac4e231dfabec7a80c43cef0560b6db61807
Necdet Yucel <necdetyucel@gmail.com>
2010-10-16 10:33:54 +0000

    split headers, package take over

--- 2009/devel/network/download/httrack/actions.py
+++ 2009/devel/network/download/httrack/actions.py
@@ -7,9 +7,6 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
-
-WorkDir = "httrack-%s" % get.srcVERSION()
 
 def setup():
     autotools.configure("--disable-static")
