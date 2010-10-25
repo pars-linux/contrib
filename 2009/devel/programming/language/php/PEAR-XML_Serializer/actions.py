f32a8c31e40d5da07bf8ea24281fc2efb8170ba3
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2010-10-25 20:43:50 +0000

    version bump

--- 2009/devel/programming/language/php/PEAR-XML_Serializer/actions.py
+++ 2009/devel/programming/language/php/PEAR-XML_Serializer/actions.py
@@ -10,8 +10,7 @@ from pisi.actionsapi import get
 WorkDir="XML_Serializer-%s" % get.srcVERSION()
 
 def install():
-    items = ['Serializer.php', 'Unserializer.php']
-    for item in items:
-        pisitools.insinto("/usr/share/php5/PEAR/XML", item)
+    pisitools.insinto("/usr/share/php5/PEAR/XML", "XML/*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/XML_Serializer","doc/*")
     pisitools.insinto("/usr/share/php5/PEAR/doc/XML_Serializer/examples","examples/*")
+    pisitools.insinto("/usr/share/php5/PEAR/tests/XML_Serializer", "tests/*")
