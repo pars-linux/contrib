45aa04df2ffbf4850442be931fe74c445017ebaf
necdet.yucel <necdet.yucel@a748b760-f2fe-475f-8849-a8a11d7a3cd2>
2011-02-07 22:22:26 +0000

    version bump

--- 2009/devel/programming/language/php/PEAR-Net_IPv6/actions.py
+++ 2009/devel/programming/language/php/PEAR-Net_IPv6/actions.py
@@ -5,8 +5,9 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
-WorkDir="Net_IPv6-1.2.1RC1"
+WorkDir="Net_IPv6-%s" % get.srcVERSION()
 
 def install():
     pisitools.insinto("/usr/share/php5/PEAR/Net", "Net/IPv6.php")
